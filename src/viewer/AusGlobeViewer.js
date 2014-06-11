4b644dc23623bf50a4692d8f1e52ec9abe30d3cb
kgrochow <kgrochow@nicta.com.au>
2014-06-11 16:23:38 +1000

    initial printing and share capability for 2d and 3d

--- src/viewer/AusGlobeViewer.js
+++ src/viewer/AusGlobeViewer.js
@@ -53,6 +53,8 @@ var SearchWidget = require('./SearchWidget');
 //use our own bing maps key
 BingMapsApi.defaultKey = 'Aowa32_DmAxInFM948JlflrBYsiqRIm-SqH1-zp8Btp4Bk-9K6gMKkpUNbPnrSsk';
 
+
+
 //Initialize the selected viewer - Cesium or Leaflet
 var AusGlobeViewer = function(geoDataManager) {
 
@@ -80,13 +82,25 @@ var AusGlobeViewer = function(geoDataManager) {
             },
             {
                 label : 'Share',
-                uri : 'http://www.nicta.com.au',
                 callback : function() {
-                    if (that.scene) {
-                        that.geoDataManager.shareRequest = true;
+                    if (that.map) {
+//                        that.geoDataManager.setShareRequest({});
+                        console.log($('cesiumContainer'));
+                        html2canvas( document.getElementById('cesiumContainer'), {
+						    useCORS: true,
+                            onrendered: function(canvas) {
+                                var dataUrl = canvas.toDataURL("image/jpeg");
+                                var bnds = that.map.getBounds()
+                                var rect = Rectangle.fromDegrees(bnds.getWest(), bnds.getSouth(), bnds.getEast(), bnds.getNorth());
+                                that.geoDataManager.setShareRequest({
+                                    image: dataUrl,
+                                    camera: rect
+                                });
+                            }
+                        })
                     }
-                    else {
-                        that.geoDataManager.setShareRequest({});
+                    else if (that.scene) {
+                        that.geoDataManager.shareRequest = true;
                     }
                 }
             }
