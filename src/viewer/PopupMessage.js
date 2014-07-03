ed3de3836e9c4eb4497a28db77f6ed3b57df262c
Kevin Ring <kevin@kotachrome.com>
2014-07-03 19:02:15 -0400

    Add a message on startup if WebGL not supported.

---
+++ src/viewer/PopupMessage.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*global require,Cesium,$*/
+var defaultValue = Cesium.defaultValue;
+var getElement = Cesium.getElement;
+
+var knockout = require('knockout');
+
+var PopupMessage = function(options) {
+    var container = getElement(options.container);
+    var message = options.message;
+    var title = defaultValue(options.title, 'Information');
+
+    var wrapper = document.createElement('div');
+    wrapper.className = 'ausglobe-info-container';
+    wrapper.setAttribute('data-bind', 'click: closeIfClickOnBackground');
+    container.appendChild(wrapper);
+
+    var popup = document.createElement('div');
+    popup.className = 'ausglobe-message';
+    popup.innerHTML = '\
+        <div class="ausglobe-info-header">\
+            <div class="ausglobe-info-close-button" data-bind="click: close">&times;</div>\
+            <h1 data-bind="text: title"></h1>\
+        </div>\
+        <div class="ausglobe-info-content">\
+            <div class="ausglobe-share-label" data-bind="html: message"></div>\
+        </div>\
+    ';
+    wrapper.appendChild(popup);
+
+    var viewModel = {};
+
+    viewModel.close = function() {
+        container.removeChild(wrapper);
+    };
+    viewModel.closeIfClickOnBackground = function(viewModel, e) {
+        if (e.target === wrapper) {
+            viewModel.close();
+        }
+        return true;
+    };
+
+    viewModel.title = knockout.observable(defaultValue(options.title, 'Information'));
+    viewModel.message = knockout.observable(options.message);
+
+    knockout.applyBindings(viewModel, wrapper);
+};
+
+module.exports = PopupMessage;
