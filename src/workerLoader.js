6a9cc65eee0f6463203fb1d12ff22c6f20a3b850
Kevin Ring <kevin@kotachrome.com>
2014-07-31 17:04:26 -0400

    Remove unused file.

--- src/workerLoader.js
+++
@@ -1,8 +0,0 @@
-/*global require*/
-"use strict";
-
-var createTaskProcessorWorker = require('../third_party/cesium/Source/Workers/createTaskProcessorWorker');
-
-module.exports = function(workerName) {
-    return createTaskProcessorWorker.workers[workerName];
-};
