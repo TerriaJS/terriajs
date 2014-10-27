8fa9e487622b5e1d17b1c98ea59dc5457dad4ec7
Kevin Ring <kevin@kotachrome.com>
2014-10-27 12:50:00 +1100

    Add CZML support.

--- src/ViewModels/registerCatalogViewModels.js
+++ src/ViewModels/registerCatalogViewModels.js
@@ -6,6 +6,7 @@ var ArcGisMapServerItemViewModel = require('./ArcGisMapServerItemViewModel');
 var CkanGroupViewModel = require('./CkanGroupViewModel');
 var createCatalogMemberFromType = require('./createCatalogMemberFromType');
 var createCatalogItemFromUrl = require('./createCatalogItemFromUrl');
+var CzmlItemViewModel = require('./CzmlItemViewModel');
 var CatalogGroupViewModel = require('./CatalogGroupViewModel');
 var GeoJsonItemViewModel = require('./GeoJsonItemViewModel');
 var KmlItemViewModel = require('./KmlItemViewModel');
@@ -15,6 +16,7 @@ var CsvItemViewModel = require('./CsvItemViewModel');
 
 var registerCatalogViewModels = function() {
     createCatalogMemberFromType.register('ckan', CkanGroupViewModel);
+    createCatalogMemberFromType.register('czml', CzmlItemViewModel);
     createCatalogMemberFromType.register('esri-mapServer', ArcGisMapServerItemViewModel);
     createCatalogMemberFromType.register('geojson', GeoJsonItemViewModel);
     createCatalogMemberFromType.register('group', CatalogGroupViewModel);
@@ -22,6 +24,8 @@ var registerCatalogViewModels = function() {
     createCatalogMemberFromType.register('wms', WebMapServiceItemViewModel);
     createCatalogMemberFromType.register('wms-getCapabilities', WebMapServiceGroupViewModel);
 
+    createCatalogItemFromUrl.register(matchesExtension('czm'), CzmlItemViewModel);
+    createCatalogItemFromUrl.register(matchesExtension('czml'), CzmlItemViewModel);
     createCatalogItemFromUrl.register(matchesExtension('geojson'), GeoJsonItemViewModel);
     createCatalogItemFromUrl.register(matchesExtension('json'), GeoJsonItemViewModel);
     createCatalogItemFromUrl.register(matchesExtension('topojson'), GeoJsonItemViewModel);
