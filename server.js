e2f42fbcd8f7011dd5116e74c4de59bbadbdd321
kgrochow <kgrochow@nicta.com.au>
2014-11-07 15:33:06 +1100

    handle input_url case and limit copy to server size

--- server.js
+++ server.js
@@ -224,17 +224,50 @@ if (cluster.isMaster) {
     app.post('/convert', function(req, res, next) {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
-            //TODO: handle url case
-            var fname = files.input_file.name;
+            var fname, fpath, inputStream;
+            var maxSize = 1000000;
+
+            if (fields.input_url !== undefined) {
+                fpath = fields.input_url;
+                fname = fpath;
+            } else if (files.input_file !== undefined) {
+                if (files.input_file.size <= maxSize) {
+                    fpath = files.input_file.path;
+                    fname = files.input_file.name;
+                } else {
+                    console.log('Input file is too large', files.input_file.size);
+                }
+            }
+            if (fpath === undefined) {
+                res.status(500).send('Unable to convert data');
+                return;
+            }
             console.log('Converting', fname);
+
             var hint = '';
             //simple hint for now, might need to crack zip files going forward
             if (fname.toLowerCase().indexOf('.zip') === fname.length-4) {
                 hint = 'shp';
             }
 
-            var input = fs.createReadStream(files.input_file.path);
-            var ogr = ogr2ogr(input, hint)
+            if (fpath.indexOf('http') === 0) {
+                 inputStream = request.get({url: fpath}).on('response', function(response) {
+                    var request = this, len = 0;
+                    response.on('data', function (chunk) {
+                        len += chunk.length;
+                        if (len > maxSize) {
+                            request.abort();
+                        }
+                    });
+                    response.on('end', function() {
+                        console.log('Convert download size', len);
+                    });
+                });
+            } else {
+                inputStream = fs.createReadStream(fpath);
+            }
+
+            var ogr = ogr2ogr(inputStream, hint)
                             .skipfailures()
                             .options(['-t_srs', 'EPSG:4326']);
 
