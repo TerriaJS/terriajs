ac0062d29b47775cf272898464de0ba9cb35b1c0
Kevin Ring <kevin@kotachrome.com>
2014-12-18 14:02:30 +1100

    Info buttons, arrows on top-level groups.

--- src/ViewModels/DataCatalogTabViewModel.js
+++ src/ViewModels/DataCatalogTabViewModel.js
@@ -3,6 +3,7 @@
 /*global require*/
 var createFragmentFromTemplate = require('../Core/createFragmentFromTemplate');
 var ExplorerTabViewModel = require('./ExplorerTabViewModel');
+var GeoDataInfoPopup = require('../viewer/GeoDataInfoPopup');
 var inherit = require('../Core/inherit');
 var loadView = require('../Core/loadView');
 
@@ -18,12 +19,21 @@ var DataCatalogTabViewModel = function(catalog) {
     this.svgCheckboxUnchecked = 'M26,27.5H6c-0.829,0-1.5-0.672-1.5-1.5V6c0-0.829,0.671-1.5,1.5-1.5h20c0.828,0,1.5,0.671,1.5,1.5v20C27.5,26.828,26.828,27.5,26,27.5zM7.5,24.5h17v-17h-17V24.5z';
     this.svgArrowDown = 'M8.037,11.166L14.5,22.359c0.825,1.43,2.175,1.43,3,0l6.463-11.194c0.826-1.429,0.15-2.598-1.5-2.598H9.537C7.886,8.568,7.211,9.737,8.037,11.166z';
     this.svgArrowRight = 'M11.166,23.963L22.359,17.5c1.43-0.824,1.43-2.175,0-3L11.166,8.037c-1.429-0.826-2.598-0.15-2.598,1.5v12.926C8.568,24.113,9.737,24.789,11.166,23.963z';
+    this.svgInfo = 'M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z M14.757,8h2.42v2.574h-2.42V8z M18.762,23.622H16.1c-1.034,0-1.475-0.44-1.475-1.496v-6.865c0-0.33-0.176-0.484-0.484-0.484h-0.88V12.4h2.662c1.035,0,1.474,0.462,1.474,1.496v6.887c0,0.309,0.176,0.484,0.484,0.484h0.88V23.622z';
 };
 
 inherit(ExplorerTabViewModel, DataCatalogTabViewModel);
 
 DataCatalogTabViewModel.prototype.show = function(container) {
     loadView(require('fs').readFileSync(__dirname + '/../Views/DataCatalogTab.html', 'utf8'), container, this);
+};
+
+DataCatalogTabViewModel.prototype.showInfo = function(item) {
+    ga('send', 'event', 'dataSource', 'info', item.name);
+    GeoDataInfoPopup.open({
+        container : document.body,
+        dataSource : item
+    });
 };
 
 module.exports = DataCatalogTabViewModel;
