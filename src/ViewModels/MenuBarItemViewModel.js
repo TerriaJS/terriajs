6e77b422cab7ad38f82602a911399e7c9c29c2b4
Kevin Ring <kevin@kotachrome.com>
2014-12-22 14:35:40 +1100

    Knockoutify the menu bar.

--- src/ViewModels/MenuBarItemViewModel.js
+++ src/ViewModels/MenuBarItemViewModel.js
@@ -1,7 +1,9 @@
 'use strict';
 
 /*global require*/
+var defaultValue = require('../../third_party/cesium/Source/Core/defaultValue');
 var defined = require('../../third_party/cesium/Source/Core/defined');
+var knockout = require('../../third_party/cesium/Source/ThirdParty/knockout');
 
 var MenuBarItemViewModel = function(options) {
     this.label = options.label;
@@ -11,6 +13,9 @@ var MenuBarItemViewModel = function(options) {
     this.tooltip = options.tooltip;
     this.callback = options.callback;
     this.href = options.href;
+    this.visible = defaultValue(options.visible, true);
+
+    knockout.track(this, ['label', 'image', 'imageWidth', 'imageHeight', 'tooltip', 'href', 'visible']);
 };
 
 MenuBarItemViewModel.prototype.execute = function() {
