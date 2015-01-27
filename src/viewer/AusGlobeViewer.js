fd1d0fd7515310a3cadd6081008116a5b4ccb9fa
Kevin Ring <kevin@kotachrome.com>
2015-01-27 17:13:35 +1100

    Temporarily disable frame rate monitoring.
    It doesn't interact nicely with the new, more aggressive render loop pausing.

--- src/viewer/AusGlobeViewer.js
+++ src/viewer/AusGlobeViewer.js
@@ -490,27 +490,27 @@ us via email at nationalmap@lists.nicta.com.au.'
         ScreenSpaceEventType.LEFT_DOUBLE_CLICK, KeyboardEventModifier.SHIFT);
 
     //Simple monitor to start up and switch to 2D if seem to be stuck.
-    this.monitor = new FrameRateMonitor({ 
-        scene: scene, 
-        minimumFrameRateDuringWarmup: 2,
-        minimumFrameRateAfterWarmup: 0,
-        samplingWindow: 2
-    });
-    this.monitor.lowFrameRate.addEventListener( function() {
-        if (!defined(that.slow3DPerformanceMessageViewed)) {
-            PopupMessageViewModel.open('ui', {
-                title : 'Unusually Slow Performance Detected',
-                message : '\
-It appears that your system is capable of running National Map in 3D mode, but is having significant performance issues. \
-We are automatically switching to 2D mode to help resolve this issue.  If you want to switch back to 3D mode you can select \
-that option from the Maps button.'
-            });
-            that.slow3DPerformanceMessageViewed = true;
-            runLater(function() { 
-                that.selectViewer(false); 
-            });
-        }
-    });
+//     this.monitor = new FrameRateMonitor({ 
+//         scene: scene, 
+//         minimumFrameRateDuringWarmup: 2,
+//         minimumFrameRateAfterWarmup: 0,
+//         samplingWindow: 2
+//     });
+//     this.monitor.lowFrameRate.addEventListener( function() {
+//         if (!defined(that.slow3DPerformanceMessageViewed)) {
+//             PopupMessageViewModel.open('ui', {
+//                 title : 'Unusually Slow Performance Detected',
+//                 message : '\
+// It appears that your system is capable of running National Map in 3D mode, but is having significant performance issues. \
+// We are automatically switching to 2D mode to help resolve this issue.  If you want to switch back to 3D mode you can select \
+// that option from the Maps button.'
+//             });
+//             that.slow3DPerformanceMessageViewed = true;
+//             runLater(function() { 
+//                 that.selectViewer(false); 
+//             });
+//         }
+//     });
 
     return viewer;
 };
