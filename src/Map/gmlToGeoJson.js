d450611b1ddd3f3551e16fcca3a9c3a69cb862d7
Kevin Ring <kevin@kotachrome.com>
2015-02-25 10:29:05 +1100

    WIP WFS / GML feature info improvements.

--- src/Map/gmlToGeoJson.js
+++ src/Map/gmlToGeoJson.js
@@ -1,7 +1,13 @@
 'use strict';
 
-/*global $*/
+/*global require,$*/
+var clone = require('../../third_party/cesium/Source/Core/clone');
 
+/**
+ * Converts a GML v3.1.1 document to GeoJSON.
+ * @param  {Document|String} xml The GML document.
+ * @return {Object} The GeoJSON object.
+ */
 function gmlToGeoJson(xml) {
     var json = $.xml2json(xml);
 
@@ -20,28 +26,44 @@ function gmlToGeoJson(xml) {
     }
 
     for (var i = 0; i < json.featureMember.length; i++) {
-           //TODO: get feature properties from non-SHAPE properties if present
-        //feature.properties = feature.attributes;
-
         //Recursively find features and add to FeatureCollection
-        filterValue(json.featureMember[i], 'Point', pointFilterFunction);
-        filterValue(json.featureMember[i], 'LineString', lineStringFilterFunction);
-        filterValue(json.featureMember[i], 'Polygon', polygonFilterFunction);
+        filterValue(json.featureMember[i], 'Point', pointFilterFunction, {});
+        filterValue(json.featureMember[i], 'LineString', lineStringFilterFunction, {});
+        filterValue(json.featureMember[i], 'Polygon', polygonFilterFunction, {});
     }
 
     return newObj;
 }
 
 //Utility function to convert esri gml based feature to geojson
-function convertFeature(feature, geom_type) {
-    var newFeature = {type: "Feature"};
+function convertFeature(feature, geom_type, properties) {
     var pts = (geom_type === 'Point') ? gml2coord(feature.pos)[0] : gml2coord(feature.posList);
-    newFeature.geometry = { "type": geom_type, "coordinates": pts };
+
+    var newFeature = {
+        type: 'Feature',
+        geometry: {
+            type: geom_type,
+            coordinates: pts
+        },
+        properties: clone(properties)
+    };
+
     return newFeature;
 }  
 
 // find a member by name in the gml
-function filterValue(obj, prop, func) {
+function filterValue(obj, prop, func, properties) {
+    properties = clone(properties);
+
+    // Find
+    for (var p1 in obj) {
+        if (obj.hasOwnProperty(p1)) {
+            if (p1 !== prop && typeof obj[p1] !== 'object') {
+                properties[p1] = obj[p1];
+            }
+        }
+    }
+
     for (var p in obj) {
         if (obj.hasOwnProperty(p) === false) {
             continue;
@@ -52,7 +74,7 @@ function filterValue(obj, prop, func) {
             }
         }
         else if (typeof obj[p] === 'object') {
-            filterValue(obj[p], prop, func);
+            filterValue(obj[p], prop, func, properties);
         }
     }
 }
