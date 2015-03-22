b36ac23a9b0b315eaa93f22533f84149d98fc702
Kevin Ring <kevin@kotachrome.com>
2015-03-22 17:02:59 +1100

    Update branding.

--- src/main.js
+++ src/main.js
@@ -136,7 +136,7 @@ if (start) {
         BrandBarViewModel.create(ui, {
             elements: [
                 '<a target="_blank" href="http://www.gov.au/"><img src="images/gov-brand.png" height="52" /></a>',
-                '<div class="brand-bar-name"><a target="_blank" href="http://nicta.github.io/nationalmap/public/info.html">NATIONAL<br/><strong>MAP</strong> <small>beta</small></a></div>',
+                '<div class="brand-bar-name">National Environmental<br/> <small>Information Infrastructure (beta)</small></div>',
                 '<a target="_blank" href="http://www.nicta.com.au"><img src="images/nicta.png" height="52" /></a>'
             ]
         });
