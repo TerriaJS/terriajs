f5978b725025eff82c0c376e1d01702e20adf911
chloe <chloeleichen@gmail.com>
2015-10-02 11:44:40 +1000

    add properties

--- test/Core/readTemplateSpec.js
+++ test/Core/readTemplateSpec.js
@@ -7,22 +7,22 @@ describe("readTemplate", function() {
   it("contains spec with an expectation", function() {
     mockData = {
       name: "name lala",
-      value: "value lala",
-      "some more item": "some more item lala"
+      "some more item": "some more item lala",
+      value: "value lala"
     };
-    mockTemplate = "hello {{name}} for {{value}}, {{some more item}}";
+    mockTemplate = "hello {{name}} for {{value}}, {{some more item}} and {{some more item}}";
     output = readTemplate(mockData, mockTemplate);
-    expect(output).toBe("hello name lala for value lala, some more item lala");
+    expect(output).toBe("hello name lala for value lala, some more item lala and some more item lala");
   });
 
   it("contains spec with an expectation", function() {
     mockData = {
-      name123: "name",
+      name123: "&copy;name",
       VALUE: "",
-      "cat's ear": "<em>Hello</em> world! < a href='www'>www </a>"
+      "&copy;cat's ear": "<em>Hello</em> world! < a href='www'>www </a>"
     };
-    mockTemplate = "hello {{name123}} for {{VALUE}}, {{cat's ear}}";
+    mockTemplate = "hello {{name123}} for {{VALUE}}, {{&copy;cat's ear}}";
     output = readTemplate(mockData, mockTemplate);
-    expect(output).toBe("hello name for , <em>Hello</em> world! < a href='www'>www </a>");
+    expect(output).toBe("hello &copy;name for , <em>Hello</em> world! < a href='www'>www </a>");
   });
 });
