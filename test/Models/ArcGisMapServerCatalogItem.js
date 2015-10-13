b6fff0f12aad88f171408b679bfac56e28b626c4
Arthur Street <arthurstreet@yahoo.com>
2015-10-13 15:24:02 +1100

    added tests that load loads the right urls

--- test/Models/ArcGisMapServerCatalogItem.js
+++ test/Models/ArcGisMapServerCatalogItem.js
@@ -17,7 +17,17 @@ beforeEach(function() {
     item = new ArcGisMapServerCatalogItem(terria);
 });
 
+
 describe('ArcGisMapServerCatalogItemViewModel', function() {
+
+    beforeEach(function() {
+        // item.load() ultimately calls loadWithXhr.load(), which just needs to return some json, so fake it
+        spyOn(loadWithXhr, 'load').and.callFake(function(url, responseType, method, data, headers, deferred, overrideMimeType, preferText, timeout) {
+            var json = '{"currentVersion": 1.1, "folders": ["Utilities"], "services": [{"name": "2014-Map", "type": "MapServer"}]}';
+            deferred.resolve(json);
+        });
+    });
+
     it('has sensible type and typeName', function() {
         expect(item.type).toBe('esri-mapServer');
         expect(item.typeName).toBe('Esri ArcGIS MapServer');
@@ -55,23 +65,55 @@ describe('ArcGisMapServerCatalogItemViewModel', function() {
         expect(item.showTilesAfterMessage).toBe(false);
     });
 
-    it('can load JSON', function(done) {
-        item.updateFromJson({
-            url: 'http://my.arcgis.com'
-        });
-        // item.load() ultimately calls loadWithXhr.load(), which just needs to return some json, so fake it
-        spyOn(loadWithXhr, 'load').and.callFake(function(url, responseType, method, data, headers, deferred, overrideMimeType, preferText, timeout) {
-            var json = '{"currentVersion": 1.1, "folders": ["Utilities"], "services": [{"name": "2014-Map", "type": "MapServer"}]}';
-            deferred.resolve(json);
-        });
+    it('can load json', function(done) {
+        var url = 'http://my.arcgis.com/';
+        item.updateFromJson({url: url});
         item.load().then(function() {
-            expect(loadWithXhr.load).toHaveBeenCalled();
+            // with this url, loadJson (and thus loadWithXhr) should have been called twice
+            // once for the serviceUrl, which is the same as the url plus a query param
+            // and once for the layersUrl, which is url/layers?...
+            expect(loadWithXhr.load.calls.count()).toEqual(2);
+            // this reg exp allows for optional / at end of url and after /layers
+            var load1 = (new RegExp(url + '\/?\\?')).test(loadWithXhr.load.calls.argsFor(0)[0]);
+            var load2 = (new RegExp(url + '\/{0,2}layers\/?\\?')).test(loadWithXhr.load.calls.argsFor(1)[0]);
+            expect(load1).toBe(true);
+            expect(load2).toBe(true);
             done();
         });
         // .otherwise(function() {
         //     expect(loadWithXhr.load).toHaveBeenCalledWith('abc');
         //     done();
         // });
+    });
+
+    it('properly loads MapServer/3', function(done) {
+        var url = 'http://my.arcgis.com/MapServer/3'; // do not put a / at the end, number must be one digit
+        item.updateFromJson({url: url});
+        item.load().then(function() {
+            // with this url, loadJson (and thus loadWithXhr) should have been called twice
+            // once for the serviceUrl, which is the same as the url plus a query param
+            // and once for the layersUrl, which is the same url again
+            expect(loadWithXhr.load.calls.count()).toEqual(2);
+            // this reg exp allows for optional / at end of url and after /layers
+            var re = new RegExp(url.substr(0, url.length-1) + '\/?\\?');   // the first url will be missing the number
+            var load1 = re.test(loadWithXhr.load.calls.argsFor(0)[0]);
+            var re2 = new RegExp(url + '\/?\\?');
+            var load2 = re2.test(loadWithXhr.load.calls.argsFor(1)[0]);
+            expect(load1).toBe(true);
+            expect(load2).toBe(true);
+            done();
+        });
+    });
+
+    it('properly loads with numbers in url', function(done) {
+        var url = 'http://my.arcgis3.com';
+        item.updateFromJson({url: url});
+        item.load().then(function() {
+            // this reg exp allows for optional / at end of url and after /layers
+            var load2 = (new RegExp(url + '\/{0,2}layers\/?\\?')).test(loadWithXhr.load.calls.argsFor(1)[0]);
+            expect(load2).toBe(true);
+            done();
+        });
     });
 
 });
