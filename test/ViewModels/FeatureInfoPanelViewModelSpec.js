b39a8340dc08978ba6cbe0899d5d9d69392add04
Arthur Street <arthurstreet@yahoo.com>
2015-11-06 11:02:47 +1100

    revise tests so featureInfoTemplate does not have to be on imageryLayer

--- test/ViewModels/FeatureInfoPanelViewModelSpec.js
+++ test/ViewModels/FeatureInfoPanelViewModelSpec.js
@@ -71,129 +71,6 @@ describe('FeatureInfoPanelViewModel', function() {
         expect(terria.selectedFeature.id).toBe('Pick Location');
     });
 
-    function createTestFeature(options) {
-        var properties = {};
-        properties[options.name || 'Foo'] = options.value || 'bar';
-        var description = {};
-        description.getValue = function() {
-            return options.value || 'bar';
-        };
-
-        return new Entity({
-            name: options.name || 'Foo',
-            properties: properties,
-            description: description,
-            imageryLayer: options.imageryLayer || {}
-        });
-    }
-
-    it('uses and completes a string-form featureInfoTemplate if present on an imagery layer', function(done) {
-        var feature = createTestFeature({
-            value: 'bar',
-            imageryLayer: {featureInfoTemplate : '<div>test {{Foo}}</div>'}
-        });
-        var pickedFeatures = new PickedFeatures();
-        pickedFeatures.features.push(feature);
-        pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
-
-        panel.showFeatures(pickedFeatures).then(function() {
-            expect(panel.sections[0].info).toBe('<div>test bar</div>');
-        }).otherwise(done.fail).then(done);
-    });
-
-    it('uses and completes a string-form featureInfoTemplate if present on geojson', function(done) {
-        createCatalogMemberFromType.register('group', CatalogGroup);
-        createCatalogMemberFromType.register('geojson', GeoJsonCatalogItem);
-        loadJson('test/init/geojson-with-template.json').then(function(json) {
-            var terria = new Terria({
-                baseUrl: './'
-            });
-            var catalog = new Catalog(terria);
-            catalog.updateFromJson(json.catalog).then(function() {
-                var geoJson = catalog.group.items[0].items[0];
-                geoJson.load().then(function() {
-                    expect(geoJson.dataSource.entities.values.length).toBeGreaterThan(0);
-                    panel.terria.nowViewing.add(geoJson);
-                    var feature = geoJson.dataSource.entities.values[0];
-                    var pickedFeatures = new PickedFeatures();
-                    pickedFeatures.features.push(feature);
-                    pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
-
-                    panel.showFeatures(pickedFeatures).then(function() {
-                        expect(panel.sections[0].info).toBe('A Hoop_Big made of Stainless Steel with Capex funding.');
-                    }).otherwise(done.fail).then(done);
-                }).otherwise(done.fail);
-            }).otherwise(done.fail);
-        }).otherwise(done.fail);
-    });
-
-    it('must use triple braces to embed html in template', function(done) {
-        var feature = createTestFeature({
-            value: '<h1>bar</h1>',
-            imageryLayer: {featureInfoTemplate : '<div>{{{Foo}}} Hello {{name}}</div>'}
-        });
-        feature.properties['name'] = 'Jay<br>';
-        var pickedFeatures = new PickedFeatures();
-        pickedFeatures.features.push(feature);
-        pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
-
-        panel.showFeatures(pickedFeatures).then(function() {
-            expect(panel.sections[0].info).toBe('<div><h1>bar</h1> Hello Jay&lt;br&gt;</div>');
-        }).otherwise(done.fail).then(done);
-    });
-
-    it('can use a json featureInfoTemplate with partials', function(done) {
-        var feature = createTestFeature({
-            value: 'bar',
-            imageryLayer: {featureInfoTemplate : {template: '<div>test {{>foobar}}</div>', foobar: '<b>{{Foo}}</b>'}}
-        });
-        var pickedFeatures = new PickedFeatures();
-        pickedFeatures.features.push(feature);
-        pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
-
-        panel.showFeatures(pickedFeatures).then(function() {
-            expect(panel.sections[0].info).toBe('<div>test <b>bar</b></div>');
-        }).otherwise(done.fail).then(done);
-    });
-
-    it('can render a recursive featureInfoTemplate', function(done) {
-        var feature = createTestFeature({
-            value: 'bar',
-            imageryLayer: {
-                featureInfoTemplate : {
-                    template: '<ul>{{>show_children}}</ul>',
-                    show_children: '{{#children}}<li>{{name}}<ul>{{>show_children}}</ul></li>{{/children}}'
-                }
-            }
-        });
-        feature.properties['children'] = [
-            {name: 'Alice', children: [{name: 'Bailey', children: null}, {name: 'Beatrix', children: null}]}, 
-            {name: 'Xavier', children: [{name: 'Yann', children: null}, {name: 'Yvette', children: null}]}
-        ];
-        var pickedFeatures = new PickedFeatures();
-        pickedFeatures.features.push(feature);
-        pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
-
-        var recursedHtml = ''
-            + '<ul>'
-            +   '<li>Alice'
-            +       '<ul>'
-            +           '<li>' + 'Bailey' + '<ul></ul>' + '</li>'
-            +           '<li>' + 'Beatrix' + '<ul></ul>' + '</li>'
-            +       '</ul>'
-            +   '</li>'
-            +   '<li>Xavier'
-            +       '<ul>'
-            +           '<li>' + 'Yann' + '<ul></ul>' + '</li>'
-            +           '<li>' + 'Yvette' + '<ul></ul>' + '</li>'
-            +       '</ul>'
-            +   '</li>'
-            + '</ul>';
-        panel.showFeatures(pickedFeatures).then(function() {
-            expect(panel.sections[0].info).toBe(recursedHtml);
-        }).otherwise(done.fail).then(done);
-    });
-
     it('removes all clock event listeners', function(done) {
         var feature = createTestFeature({});
         var pickedFeatures = new PickedFeatures();
@@ -213,14 +90,167 @@ describe('FeatureInfoPanelViewModel', function() {
         });
     });
 
-
-    function domContainsText(panel, s) {
-        for (var i = 0; i < panel._domNodes.length; ++i) {
-            if (panel._domNodes[i].innerHTML && panel._domNodes[i].innerHTML.indexOf(s) >= 0) {
-                return true;
-            }
-        }
-
-        return false;
+    function createTestFeature(options) {
+        var properties = {};
+        properties[options.name || 'Foo'] = options.value || 'bar';
+        var description = {};
+        description.getValue = function() {
+            return options.value || 'bar';
+        };
+        return new Entity({
+            name: options.name || 'Foo',
+            properties: properties,
+            description: description,
+            imageryLayer: options.imageryLayer || {}
+        });
     }
+
 });
+
+
+describe('FeatureInfoPanelViewModel templating', function() {
+    var terria,
+        panel,
+        catalog,
+        item;
+
+    beforeEach(function(done) {
+        terria = new Terria({
+            baseUrl: './'
+        });
+        panel = new FeatureInfoPanelViewModel({
+            terria: terria
+        });
+        createCatalogMemberFromType.register('group', CatalogGroup);
+        createCatalogMemberFromType.register('geojson', GeoJsonCatalogItem);
+        loadJson('test/init/geojson-with-template.json').then(function(json) {
+            catalog = new Catalog(terria);
+            catalog.updateFromJson(json.catalog).then(function() {
+                item = catalog.group.items[0].items[0];
+                done();
+            }).otherwise(done.fail);
+        }).otherwise(done.fail);
+    });
+
+    afterEach(function() {
+        panel.destroy();
+        panel = undefined;
+    });
+
+    it('has a default template', function(done) {
+        item.featureInfoTemplate = undefined;
+        item.load().then(function() {
+            expect(item.dataSource.entities.values.length).toBeGreaterThan(0);
+            panel.terria.nowViewing.add(item);
+            var feature = item.dataSource.entities.values[0];
+            var pickedFeatures = new PickedFeatures();
+            pickedFeatures.features.push(feature);
+            pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
+
+            panel.showFeatures(pickedFeatures).then(function() {
+                expect(panel.sections[0].info).toContain('<td>Hoop_Big</td>');
+            }).otherwise(done.fail).then(done);
+        }).otherwise(done.fail);
+
+    });
+
+    it('uses and completes a string-form featureInfoTemplate if present', function(done) {
+        item.featureInfoTemplate = 'A {{type}} made of {{material}} with {{funding_ba}} funding.';
+        item.load().then(function() {
+            expect(item.dataSource.entities.values.length).toBeGreaterThan(0);
+            panel.terria.nowViewing.add(item);
+            var feature = item.dataSource.entities.values[0];
+            var pickedFeatures = new PickedFeatures();
+            pickedFeatures.features.push(feature);
+            pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
+
+            panel.showFeatures(pickedFeatures).then(function() {
+                expect(panel.sections[0].info).toBe('A Hoop_Big made of Stainless Steel with Capex funding.');
+            }).otherwise(done.fail).then(done);
+        }).otherwise(done.fail);
+    });
+
+    it('must use triple braces to embed html in template', function(done) {
+        item.featureInfoTemplate = '<div>Hello {{name}} - {{{name}}}</div>';
+        item.load().then(function() {
+            expect(item.dataSource.entities.values.length).toBeGreaterThan(0);
+            panel.terria.nowViewing.add(item);
+            var feature = item.dataSource.entities.values[0];
+            feature.properties['name'] = 'Jay<br>';
+            var pickedFeatures = new PickedFeatures();
+            pickedFeatures.features.push(feature);
+            pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
+
+            panel.showFeatures(pickedFeatures).then(function() {
+                expect(panel.sections[0].info).toBe('<div>Hello Jay&lt;br&gt; - Jay<br></div>');
+            }).otherwise(done.fail).then(done);
+        }).otherwise(done.fail);
+
+    });
+
+    it('can use a json featureInfoTemplate with partials', function(done) {
+        item.featureInfoTemplate = {template: '<div>test {{>foobar}}</div>', foobar: '<b>{{type}}</b>'};
+        item.load().then(function() {
+            expect(item.dataSource.entities.values.length).toBeGreaterThan(0);
+            panel.terria.nowViewing.add(item);
+            var feature = item.dataSource.entities.values[0];
+            var pickedFeatures = new PickedFeatures();
+            pickedFeatures.features.push(feature);
+            pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
+
+            panel.showFeatures(pickedFeatures).then(function() {
+                expect(panel.sections[0].info).toBe('<div>test <b>Hoop_Big</b></div>');
+            }).otherwise(done.fail).then(done);
+        }).otherwise(done.fail);
+    });
+
+    it('can render a recursive featureInfoTemplate', function(done) {
+
+        item.featureInfoTemplate = {
+            template: '<ul>{{>show_children}}</ul>',
+            show_children: '{{#children}}<li>{{name}}<ul>{{>show_children}}</ul></li>{{/children}}'
+        };
+        item.load().then(function() {
+            expect(item.dataSource.entities.values.length).toBeGreaterThan(0);
+            panel.terria.nowViewing.add(item);
+            var feature = item.dataSource.entities.values[0];
+            feature.properties['children'] = [
+                {name: 'Alice', children: [{name: 'Bailey', children: null}, {name: 'Beatrix', children: null}]}, 
+                {name: 'Xavier', children: [{name: 'Yann', children: null}, {name: 'Yvette', children: null}]}
+            ];
+            var pickedFeatures = new PickedFeatures();
+            pickedFeatures.features.push(feature);
+            pickedFeatures.allFeaturesAvailablePromise = runLater(function() {});
+
+            panel.showFeatures(pickedFeatures).then(function() {
+                var recursedHtml = ''
+                    + '<ul>'
+                    +   '<li>Alice'
+                    +       '<ul>'
+                    +           '<li>' + 'Bailey' + '<ul></ul>' + '</li>'
+                    +           '<li>' + 'Beatrix' + '<ul></ul>' + '</li>'
+                    +       '</ul>'
+                    +   '</li>'
+                    +   '<li>Xavier'
+                    +       '<ul>'
+                    +           '<li>' + 'Yann' + '<ul></ul>' + '</li>'
+                    +           '<li>' + 'Yvette' + '<ul></ul>' + '</li>'
+                    +       '</ul>'
+                    +   '</li>'
+                    + '</ul>';
+                expect(panel.sections[0].info).toBe(recursedHtml);
+            }).otherwise(done.fail).then(done);
+        }).otherwise(done.fail);
+    });
+
+});
+
+function domContainsText(panel, s) {
+    for (var i = 0; i < panel._domNodes.length; ++i) {
+        if (panel._domNodes[i].innerHTML && panel._domNodes[i].innerHTML.indexOf(s) >= 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
