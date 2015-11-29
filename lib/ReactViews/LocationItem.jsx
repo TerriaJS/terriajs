97063fdcb722c23f635b0b22e6a955445d71e880
Chloe Chen <chloeleichen@gmail.com>
2015-11-29 13:58:52 +1100

    bing search

---
+++ lib/ReactViews/LocationItem.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+var LocationItem = React.createClass({
+  render: function(){
+    var item = this.props.item;
+    return (
+      <li className="clearfix location-item"><button className="btn data-item-title col col-11 relative">{item.name}</button><button  className="btn col col-1 relative"><i className='fa fa-map-marker blue'> </i></button></li>
+      ) ;
+  }
+});
+
+module.exports = LocationItem;
--- lib/ReactViews/SearchBox.jsx
+++ lib/ReactViews/SearchBox.jsx
@@ -1,46 +1,86 @@
 'use strict';
 var DataCatalogItem = require('./DataCatalogItem.jsx');
+var LocationItem = require('./LocationItem.jsx');
+var Loader = require('./Loader.jsx');
+
+
 var CatalogItemNameSearchProviderViewModel = require('../ViewModels/CatalogItemNameSearchProviderViewModel.js');
+var BingMapsSearchProviderViewModel = require('../ViewModels/BingMapsSearchProviderViewModel.js');
 var when = require('terriajs-cesium/Source/ThirdParty/when');
 
 var SearchBox = React.createClass({
   getInitialState: function() {
     return {
       value: '',
-      dataCatalogresult: []
+      dataCatalogResults: [],
+      bingMapSearchResults: []
     };
   },
   handleChange: function(event) {
     this.setState({
       value: event.target.value
     });
-    var search = new CatalogItemNameSearchProviderViewModel(this.props);
+    var dataCatalogSearch = new CatalogItemNameSearchProviderViewModel(this.props);
+    var bingMapSearch = new BingMapsSearchProviderViewModel(this.props);
     var that = this;
 
     //this is a promise
-    when(search.search(event.target.value)).then(function(){
+    when(dataCatalogSearch.search(event.target.value)).then(function(){
       that.setState({
-        dataCatalogresult: search.searchResults
+        dataCatalogResults: dataCatalogSearch.searchResults
       })
-    })
+    });
+
+    if(that.props.mapSearch !== false){
+       when(bingMapSearch.search(event.target.value)).then(function(){
+        that.setState({
+          bingMapSearchResults: bingMapSearch.searchResults
+        })
+      });
+    }
   },
 
   render: function() {
     var value = this.state.value;
-    var result = this.state.dataCatalogresult;
+    var dataCatalogResults = this.state.dataCatalogResults;
+    var bingMapSearchResults = this.state.bingMapSearchResults;
     var searchingClass = 'search-data-search '+ (this.state.value.length > 0 ? 'searching': '');
+
+    // if is searching bing map, if result is not empty, show results. otherwise show loader
+    var mapSearchContent = null;
+    if((this.props.mapSearch !== false) && value.length > 0){
+      if(bingMapSearchResults.length === 0){
+        mapSearchContent = (<ul className='list-reset search-result-bing-map'><li> <button className='btn label'> Bing Map Search Results</button></li> <Loader /></ul>);
+      } else{
+        mapSearchContent = (<ul className='list-reset search-result-bing-map'><li> <button className='btn label'> Bing Map Search Results</button></li>{bingMapSearchResults.map(function(item, i) {
+            return (<LocationItem item={item} key={i} />);
+          })}</ul>);
+      }
+    }
+
+    // if is searching data catalog and result is not empty, show results, otherwise show loader
+    var cataLogSearchContent = null;
+    if(value.length > 0){
+      if(dataCatalogResults.length === 0){
+        cataLogSearchContent = (<ul className='list-reset search-result-data-catalog'><li> <button className='btn label'> Data Catalog Search Results</button></li> <Loader /></ul>);
+      } else{
+        cataLogSearchContent = (<ul className='list-reset search-result-data-catalog'><li> <button className='btn label'> Bing Map Search Results</button></li>{dataCatalogResults.map(function(item, i) {
+            return (<DataCatalogItem item={item.catalogItem} key={i} />);
+          })}</ul>);
+      }
+    }
+
     return (
       <div className={searchingClass}>
-      <form className='search-data-form relative'>
-      <label htmlFor='search' className='hide'> Type keyword to search </label>
-      <i className='fa fa-search'></i>
-      <input id='search' type='text' name='search' value={value} onChange={this.handleChange} className='search__field field' placeholder='Search'/>
-      </form>
-      <ul className = 'list-reset search-result'>
-      {result.map(function(group, i) {
-        return (<DataCatalogItem item={group.catalogItem} key={i} />);
-      }, this)}
-      </ul>
+        <form className='search-data-form relative'>
+          <label htmlFor='search' className='hide'> Type keyword to search </label>
+          <i className='fa fa-search'></i>
+          <input id='search' type='text' name='search' value={value} onChange={this.handleChange} className='search__field field' placeholder='Search'/>
+        </form>
+        <ul className ='list-reset search-results'>
+          {cataLogSearchContent}
+          {mapSearchContent}
+        </ul>
       </div>
     );
   }
