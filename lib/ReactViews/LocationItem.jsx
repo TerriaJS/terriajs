615687ef02677e46917ea1f155af02b47b545c46
Chloe <chloeleichen@gmail.com>
2015-12-09 21:55:02 +1100

    location icon placeholder

--- lib/ReactViews/LocationItem.jsx
+++ lib/ReactViews/LocationItem.jsx
@@ -8,7 +8,7 @@ var LocationItem = React.createClass({
     render: function() {
         var item = this.props.item;
         return (
-            <li className="clearfix location-item"><button className="btn data-item-title col col-11 relative">{item.name}</button><button  className="btn col col-1 relative"><i className='icon icon-map-marker blue'> </i></button></li>
+            <li className="clearfix location-item"><button className="btn data-item-title col col-11 relative">{item.name}</button><button  className="btn col col-1 relative"><i className='icon icon-check_box'> </i></button></li>
         );
     }
 });
