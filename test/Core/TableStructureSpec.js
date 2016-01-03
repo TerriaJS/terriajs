c4f1ab017c9d937b2f4d1759c8221f8725c9d603
Arthur Street <arthurstreet@yahoo.com>
2016-01-04 10:15:26 +1100

    add basic tests for tableStructure, clean up some default behaviour for new table styles

---
+++ test/Core/TableStructureSpec.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*global require,describe,it,expect*/
+var TableStructure = require('../../lib/Core/TableStructure');
+
+describe('TableStructure', function() {
+
+    it('can read from json object', function() {
+        var data = [['x', 'y'], [1, 5], [3, 8], [4, -3]];
+        var tableStructure = TableStructure.fromJson(data);
+        expect(tableStructure.columns.length).toEqual(2);
+        expect(tableStructure.columns[0].name).toEqual('x');
+        expect(tableStructure.columns[0].values).toEqual([1, 3, 4]);
+        expect(tableStructure.columns[1].name).toEqual('y');
+        expect(tableStructure.columns[1].values).toEqual([5, 8, -3]);
+    });
+
+    it('can read from csv string', function() {
+        var csvString = 'x,y\r\n1,5\r\n3,8\r\n4,-3\r\n';
+        var tableStructure = TableStructure.fromCsv(csvString);
+        expect(tableStructure.columns.length).toEqual(2);
+        expect(tableStructure.columns[0].name).toEqual('x');
+        expect(tableStructure.columns[0].values).toEqual([1, 3, 4]);
+        expect(tableStructure.columns[1].name).toEqual('y');
+        expect(tableStructure.columns[1].values).toEqual([5, 8, -3]);
+    });
+
+    it('can convert to ArrayOfColumns', function() {
+        var data = [['x', 'y'], [1, 5], [3, 8], [4, -3]];
+        var tableStructure = TableStructure.fromJson(data);
+        var columns = tableStructure.toArrayOfColumns();
+        expect(columns.length).toEqual(2);
+        expect(columns[0]).toEqual(['x', 1, 3, 4]);
+        expect(columns[1]).toEqual(['y', 5, 8, -3]);
+    });
+
+    it('can convert to ArrayOfRows', function() {
+        var data = [['x', 'y'], [1, 5], [3, 8], [4, -3]];
+        var tableStructure = TableStructure.fromJson(data);
+        var rows = tableStructure.toArrayOfRows();
+        expect(rows.length).toEqual(4);
+        expect(rows).toEqual(data);
+    });
+
+    it('can convert to row objects', function() {
+        var data = [['x', 'y'], [1, 5], [3, 8], [4, -3]];
+        var tableStructure = TableStructure.fromJson(data);
+        var rowObjects = tableStructure.toRowObjects();
+        expect(rowObjects.length).toEqual(3);
+        expect(rowObjects[0]).toEqual({x: 1, y: 5});
+        expect(rowObjects[1]).toEqual({x: 3, y: 8});
+        expect(rowObjects[2]).toEqual({x: 4, y: -3});
+    });
+
+    it('can get column names', function() {
+        var data = [['x', 'y'], [1, 5], [3, 8], [4, -3]];
+        var tableStructure = TableStructure.fromJson(data);
+        expect(tableStructure.getColumnNames()).toEqual(['x', 'y']);
+    });
+
+});
