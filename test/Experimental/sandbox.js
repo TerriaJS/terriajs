d04e484263ca8678a203211927d647ae5817a7b0
Kevin Ring <kevin@kotachrome.com>
2016-01-07 11:10:34 +1100

    More experimentation.

--- test/Experimental/sandbox.js
+++ test/Experimental/sandbox.js
@@ -1,27 +1,109 @@
 import {
-    createStore
+    createStore,
+    applyMiddleware
 }
 from 'redux';
 
+import thunk from 'redux-thunk';
+import when from 'terriajs-cesium/Source/ThirdParty/when';
+
 describe('sandbox', function() {
     it('does stuff', function() {
-        class WebMapServiceCatalogItem {
+
+        class CatalogItem {
             constructor() {
-                this.url = undefined;
-                this.layers = [];
                 this.isEnabled = false;
+                this.isLoading = false;
+                this.isShown = false;
             }
 
             assign(changes) {
-                return Object.assign(new WebMapServiceCatalogItem(), this, changes);
+                return Object.assign(new this.constructor(), this, changes);
             }
 
             enable() {
-                return {
-                    type: 'UPDATE',
-                    changes: {
-                        isEnabled: true
+                return dispatch => {
+                    dispatch({
+                        type: 'UPDATE',
+                        changes: {
+                            isEnabled: true
+                        }
+                    });
+
+                    return dispatch(this.load()).then(function() {
+                        // If the catalog item is still enabled after the load completes, do the actual enable and show it.
+                        if (this.isEnabled) {
+                            return dispatch(this._doEnable()).then(function() {
+                                return dispatch(this.show());
+                            });
+                        }
+                    });
+                };
+            }
+        }
+
+        class ImageryLayerCatalogItem extends CatalogItem {
+            constructor() {
+                super();
+                this._imageryLayer = undefined;
+            }
+
+            createImageryLayer() {
+
+            }
+
+            _doEnable() {
+
+            }
+
+        }
+
+        class WebMapServiceCatalogItem extends ImageryLayerCatalogItem {
+            constructor() {
+                super();
+                this.url = undefined;
+                this.layers = [];
+                this.parameters = {};
+            }
+
+            load() {
+                return dispatch => {
+                    dispatch({
+                        type: 'UPDATE',
+                        changes: {
+                            isLoading: true
+                        }
+                    });
+                    return when().then(function() {
+                        dispatch({
+                            type: 'UPDATE',
+                            changes: {
+                                isLoading: false
+                            }
+                        });
+                    });
+                };
+            }
+
+            show() {
+                return dispatch => {
+                    var promise;
+                    if (!this.isEnabled) {
+                        promise = dispatch(this.enable());
+                    } else {
+                        promise = when();
                     }
+
+                    return promise.then(function() {
+                        dispatch({
+                            type: 'UPDATE',
+                            changes: {
+                                _imageryProvider: this.createImageryProvider(),
+                                isShown: true,
+                                isLoading: false
+                            }
+                        });
+                    });
                 };
             }
 
@@ -43,7 +125,8 @@ describe('sandbox', function() {
 
         // Create a Redux store holding the state of your app.
         // Its API is { subscribe, dispatch, getState }.
-        let store = createStore(catalogItemReducer)
+        let createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+        let store = createStoreWithMiddleware(catalogItemReducer)
 
         expect(store.getState().isEnabled).toBe(false);
 
