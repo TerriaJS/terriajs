25d5372820638217794cec279f6dbbb748f3e858
Arthur Street <arthurstreet@yahoo.com>
2016-01-13 10:34:10 +1100

    streamline RegionProvider API

--- test/Models/RegionProviderSpec.js
+++ test/Models/RegionProviderSpec.js
@@ -6,33 +6,36 @@ var Terria = require('../../lib/Models/Terria');
 var RegionProviderList = require('../../lib/Map/RegionProviderList');
 var RegionProvider = require('../../lib/Map/RegionProvider');
 
-describe('RegionProvider', function() {
+// This test would be nice, but regionProvider.processRegionIds is no longer exposed in the API.
+// We could test it by loading in some json via loadRegionIDs instead.
+// 
+// describe('RegionProvider', function() {
 
-    var terria;
-    var regionProviderList;
-    var regionProvider;
+//     var terria;
+//     var regionProviderList;
+//     var regionProvider;
 
-    beforeEach(function() {
-            terria = new Terria({
-                baseUrl: './',
-                regionMappingDefinitionsUrl: 'test/csv/regionMapping.json'
-            });
-            regionProviderList = new RegionProviderList(terria);
-            regionProvider = new RegionProvider('CED');
-    });
+//     beforeEach(function() {
+//             terria = new Terria({
+//                 baseUrl: './',
+//                 regionMappingDefinitionsUrl: 'test/csv/regionMapping.json'
+//             });
+//             regionProviderList = new RegionProviderList(terria);
+//             regionProvider = new RegionProvider('CED');
+//     });
 
-    it('gets the correct region indices', function() {
-        var regions = ['NSW', 'Vic', 'Qld', 'WA'];
-        regionProvider.processRegionIds(regions, undefined, '');
-        var regionValues = ['Vic', 'Qld', 'NSW'];
-        var result = regionProvider.mapRegionsToIndicesInto(regionValues);
-        var target = [2, 0, 1, undefined];
-        target.forEach(function(value, i) {
-            expect(result[i]).toEqual(target[i]);
-        });
-    });
+//     it('gets the correct region indices', function() {
+//         var regions = ['NSW', 'Vic', 'Qld', 'WA'];
+//         regionProvider.processRegionIds(regions, undefined, '');
+//         var regionValues = ['Vic', 'Qld', 'NSW'];
+//         var result = regionProvider.mapRegionsToIndicesInto(regionValues);
+//         var target = [2, 0, 1, undefined];
+//         target.forEach(function(value, i) {
+//             expect(result[i]).toEqual(target[i]);
+//         });
+//     });
 
-});
+// });
 
 describe('RegionProvider parsing', function() {
 
