7db085973b6922885480917c17a5b76e4918f8a4
chloe <chloeleichen@gmail.com>
2016-01-21 12:07:39 +1100

    fix indentation

--- lib/ReactViews/SearchBox.jsx
+++ lib/ReactViews/SearchBox.jsx
@@ -12,177 +12,172 @@ const GazetteerSearchProviderViewModel = require('../ViewModels/GazetteerSearchP
 const when = require('terriajs-cesium/Source/ThirdParty/when');
 
 // Handle any of the three kinds of search based on the props
-let SearchBox = React.createClass({
-    propTypes: {
-        mapSearch: React.PropTypes.bool,
-        dataSearch: React.PropTypes.bool,
-        gazetterSearch : React.PropTypes.bool,
-        callback: React.PropTypes.func
-    },
+const SearchBox = React.createClass({
+  propTypes: {
+    mapSearch: React.PropTypes.bool,
+    dataSearch: React.PropTypes.bool,
+    gazetterSearch: React.PropTypes.bool,
+    callback: React.PropTypes.func
+  },
 
-    getDefaultProps: function() {
-      return {
-        mapSearch: true,
-        dataSearch: true,
-        gazetterSearch: true
-        };
-    },
+  getDefaultProps() {
+    return {
+      mapSearch: true,
+      dataSearch: true,
+      gazetterSearch: true
+    };
+  },
 
-    getInitialState: function() {
-        return {
-            value: '',
-            dataCatalogResults: [],
-            dataCatalogIsSearching: false,
-            bingMapSearchResults: [],
-            bingMapIsSearching: false,
-            gazetterIsSearching: false,
-            gazetterSearchResults: []
-        };
-    },
+  getInitialState() {
+    return {
+      value: '',
+      dataCatalogResults: [],
+      dataCatalogIsSearching: false,
+      bingMapSearchResults: [],
+      bingMapIsSearching: false,
+      gazetterIsSearching: false,
+      gazetterSearchResults: []
+    };
+  },
 
-    componentWillMount: function() {
-        var that = this;
-        window.searchData.addEventListener(function(value) {
-            that.setState({
-                value: value
-            });
-            var fakeEvent = {
-                target: {
-                    value: value
-                }
-            };
-            // This does not work for the first time but works afterwards
-            // Not sure how to let the modal knows search has started
-            that.handleChange(fakeEvent);
-        });
-    },
-
-    handleChange: function(event) {
-        this.setState({
-            value: event.target.value
-        });
-
-        if(this.props.callback && typeof this.props.callback === 'function'){
-            this.props.callback(!event.target.value);
+  componentWillMount() {
+    const that = this;
+    window.searchData.addEventListener((value)=>{
+      that.setState({
+        value: value
+      });
+      const fakeEvent = {
+        target: {
+          value: value
         }
+      };
+      // This does not work for the first time but works afterwards
+      // Not sure how to let the modal knows search has started
+      that.handleChange(fakeEvent);
+    });
+  },
 
-        var dataCatalogSearch = new CatalogItemNameSearchProviderViewModel(this.props);
-        var bingMapSearch = new BingMapsSearchProviderViewModel(this.props);
-        var gazetterSearch = new GazetteerSearchProviderViewModel(this.props);
-        var that = this;
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
 
-        if (that.props.dataSearch !== false) {
-            when(dataCatalogSearch.search(event.target.value)).then(function() {
-                that.setState({
-                    dataCatalogResults: dataCatalogSearch.searchResults,
-                    dataCatalogIsSearching: dataCatalogSearch.isSearching
-                });
-            });
-        }
-
-        if (that.props.mapSearch !== false) {
-            when(bingMapSearch.search(event.target.value)).then(function() {
-                that.setState({
-                    bingMapSearchResults: bingMapSearch.searchResults,
-                    bingMapIsSearching: bingMapSearch.isSearching
-                });
-            });
-        }
-
-        if (that.props.gazetterSearch !== false){
-            when(gazetterSearch.search(event.target.value)).then(function(){
-                that.setState({
-                    gazetterSearchResults: gazetterSearch.searchResults,
-                    gazetterIsSearching: gazetterSearch.isSearching
-                });
-            });
-        }
-    },
-
-    clearSearch: function() {
-        this.setState({
-            value: ''
-        });
-        this.props.callback(true);
-    },
-
-    openDataCatalogSearch: function(){
-        window.searchData.raiseEvent(this.state.value);
-    },
-
-    render: function() {
-        let value = this.state.value;
-        let that = this;
-
-        // if is searching bing map, if result is not empty, show results. otherwise show loader
-        let mapSearchContent = null;
-
-        let bingMapSearchLabel = 'Locations';
-        let gazetterSearchLabel = 'Offical Place Names';
-        let dataCatalogSearchLabel = 'Data Catalog';
-
-        function getSearchResult(searchType, searchResults, searchState, resultLabel) {
-            let content = null;
-            let results = null;
-
-            if((searchType) && value.length > 0){
-                if(searchResults.length === 0){
-                    if(searchState){
-                        results = <Loader />
-                    } else{
-                        results = <li className ='label'>No results found </li>;
-                    }
-                }
-                else{
-                    if(searchType !== that.props.dataSearch){
-                        results = searchResults.map(function(item, i) {
-                                    return (<LocationItem item={item} key={i} />);
-                                  });
-                        }
-                    else{
-                        results = searchResults.map(function(result, i) {
-                            let group = result.catalogItem;
-
-                                if (group.isGroup === true){
-                                    return (<DataCatalogGroup group={group} key={i} />);
-                                } else {
-                                    return (<DataCatalogItem item={group} key={i} />);
-                                }
-                            });
-                    }
-                }
-                content = <div><label className='label'>{resultLabel}</label><ul className='list-reset search-results-items'>{results}</ul></div>
-            }
-            return content;
-        }
-
-        //button to clear search string
-        let clearSearchContent = null;
-        if (value.length > 0) {
-            clearSearchContent = (<button className='btn search-clear' onClick ={this.clearSearch}><i className ='icon icon-close'></i></button>);
-        }
-
-        let linkToSearchData = null;
-        if ((this.props.dataSearch === false) && value.length > 0){
-            linkToSearchData = (<ModalTriggerButton btnHtml={'Search " ' + value + ' " in Data Catalog'} classNames={'btn btn-data-search'} callback={this.openDataCatalogSearch} activeTab={1} />);
-        }
-
-        return (
-            <div className='search-data-search'>
-            <form className='search-data-form relative' autoComplete='off'>
-              <label htmlFor='search' className='hide'> Type keyword to search </label>
-              <i className='icon icon-search'></i>
-              <input id='search' type='text' name='search' value={value} onChange={this.handleChange} className='search__field field' placeholder='Search' autoComplete='off'/>
-              {clearSearchContent}
-            </form>
-            <div className ='search-results'>
-              {linkToSearchData}
-              {getSearchResult(that.props.mapSearch, that.state.bingMapSearchResults, that.state.bingMapIsSearching, bingMapSearchLabel)}
-              {getSearchResult(that.props.gazetterSearch, that.state.gazetterSearchResults, that.state.gazetterIsSearching, gazetterSearchLabel)}
-              {getSearchResult(that.props.dataSearch, that.state.dataCatalogResults, that.state.dataCatalogIsSearching, dataCatalogSearchLabel)}
-            </div>
-          </div>
-        );
+    if(this.props.callback && typeof this.props.callback === 'function') {
+      this.props.callback(!event.target.value);
     }
+
+    const dataCatalogSearch = new CatalogItemNameSearchProviderViewModel(this.props);
+    const bingMapSearch = new BingMapsSearchProviderViewModel(this.props);
+    const gazetterSearch = new GazetteerSearchProviderViewModel(this.props);
+    const that = this;
+
+    if (that.props.dataSearch !== false) {
+      when(dataCatalogSearch.search(event.target.value)).then(()=>{
+        that.setState({
+          dataCatalogResults: dataCatalogSearch.searchResults,
+          dataCatalogIsSearching: dataCatalogSearch.isSearching
+        });
+      });
+    }
+
+    if (that.props.mapSearch !== false) {
+      when(bingMapSearch.search(event.target.value)).then(()=>{
+        that.setState({
+          bingMapSearchResults: bingMapSearch.searchResults,
+          bingMapIsSearching: bingMapSearch.isSearching
+        });
+      });
+    }
+
+    if (that.props.gazetterSearch !== false) {
+      when(gazetterSearch.search(event.target.value)).then(()=>{
+        that.setState({
+          gazetterSearchResults: gazetterSearch.searchResults,
+          gazetterIsSearching: gazetterSearch.isSearching
+        });
+      });
+    }
+  },
+
+  clearSearch() {
+    this.setState({
+      value: ''
+    });
+    this.props.callback(true);
+  },
+
+  openDataCatalogSearch() {
+    window.searchData.raiseEvent(this.state.value);
+  },
+
+  render() {
+    const that = this;
+    const value = this.state.value;
+    const bingMapSearchLabel = 'Locations';
+    const gazetterSearchLabel = 'Offical Place Names';
+    const dataCatalogSearchLabel = 'Data Catalog';
+
+    function getSearchResult(searchType, searchResults, searchState, resultLabel) {
+      let content = null;
+      let results = null;
+
+      if((searchType) && value.length > 0) {
+        if(searchResults.length === 0) {
+          if(searchState) {
+            results = <Loader />;
+          }
+          else {
+            results = <li className ='label'>No results found </li>;
+          }
+        }
+        else {
+          if(searchType !== that.props.dataSearch) {
+            results = searchResults.map((item, i)=>{
+              return (<LocationItem item={item} key={i} />);
+            });
+          }
+          else {
+            results = searchResults.map((result, i)=>{
+              const group = result.catalogItem;
+              if (group.isGroup === true) {
+                return (<DataCatalogGroup group={group} key={i} />);
+              }
+              return (<DataCatalogItem item={group} key={i} />);
+            });
+          }
+        }
+        content = <div><label className='label label-sub-heading'>{resultLabel}</label><ul className='list-reset search-results-items'>{results}</ul></div>;
+      }
+      return content;
+    }
+
+    // button to clear search string
+    let clearSearchContent = null;
+    if (value.length > 0) {
+      clearSearchContent = (<button className='btn search-clear' onClick ={this.clearSearch}><i className ='icon icon-close'></i></button>);
+    }
+
+    let linkToSearchData = null;
+    if ((this.props.dataSearch === false) && value.length > 0) {
+      linkToSearchData = (<ModalTriggerButton btnHtml={'Search " ' + value + ' " in Data Catalog'} classNames={'btn btn-data-search'} callback={this.openDataCatalogSearch} activeTab={1} />);
+    }
+
+    return (
+        <div className='search-data-search'>
+        <form className='search-data-form relative' autoComplete='off'>
+          <label htmlFor='search' className='hide'> Type keyword to search </label>
+          <i className='icon icon-search'></i>
+          <input id='search' type='text' name='search' value={value} onChange={this.handleChange} className='search__field field' placeholder='Search' autoComplete='off'/>
+          {clearSearchContent}
+        </form>
+        <div className ='search-results'>
+          {linkToSearchData}
+          {getSearchResult(that.props.mapSearch, that.state.bingMapSearchResults, that.state.bingMapIsSearching, bingMapSearchLabel)}
+          {getSearchResult(that.props.gazetterSearch, that.state.gazetterSearchResults, that.state.gazetterIsSearching, gazetterSearchLabel)}
+          {getSearchResult(that.props.dataSearch, that.state.dataCatalogResults, that.state.dataCatalogIsSearching, dataCatalogSearchLabel)}
+        </div>
+      </div>
+    );
+  }
 });
 module.exports = SearchBox;
