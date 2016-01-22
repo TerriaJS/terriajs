9a7b672ec4a9a195ee110ff2671f2bc27803304f
Kevin Ring <kevin@kotachrome.com>
2016-01-22 17:13:19 +1100

    Fix a typo I introduced.

--- lib/ReactViews/ModalWindow.jsx
+++ lib/ReactViews/ModalWindow.jsx
@@ -38,7 +38,7 @@ const ModalWindow = React.createClass({
         return (
             <div className="data-panel-wrapper modal-wrapper fixed flex flex-center" id="data-panel-wrapper" aria-hidden={ !this.state.isOpen }>
               <div onClick={ this.closeModal } id="data-panel-overlay" className="modal-overlay absolute" tabIndex="-1"></div>
-              <div id="data-panel" className="data-panelm odal-content mx-auto v-middle" aria-labelledby="modalTitle" aria-describedby="modalDescription" role="dialog">
+              <div id="data-panel" className="data-panel modal-content mx-auto v-middle" aria-labelledby="modalTitle" aria-describedby="modalDescription" role="dialog">
                 <button onClick={ this.closeModal } className="btn btn-close-modal" title="Close data panel" data-target="close-modal"><i className='icon icon-close'></i></button>
                 <Tabs terria={ this.props.terria } activeTab={ this.state.activeTab } />
               </div>
