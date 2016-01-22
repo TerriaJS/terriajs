598813387db9e17a0ad923f69c3ae3aec067aec4
Kevin Ring <kevin@kotachrome.com>
2016-01-22 12:21:00 +1100

    eslint cleanup.

--- lib/ReactViews/ModalWindow.jsx
+++ lib/ReactViews/ModalWindow.jsx
@@ -1,46 +1,49 @@
 'use strict';
+
 const React = require('react');
 const Tabs = require('./Tabs.jsx');
 const ObserveModelMixin = require('./ObserveModelMixin');
 const PureRenderMixin = require('react-addons-pure-render-mixin');
 
 const ModalWindow = React.createClass({
-  mixins: [ObserveModelMixin, PureRenderMixin],
-  propTypes: {
-    terria: React.PropTypes.object
-  },
+    mixins: [ObserveModelMixin, PureRenderMixin],
+    propTypes: {
+        terria: React.PropTypes.object
+    },
 
-  getInitialState() {
-    return {
-      isOpen: true,
-      activeTab: 0
-    };
-  },
+    getInitialState() {
+        return {
+            isOpen: true,
+            activeTab: 0
+        };
+    },
 
-  componentWillMount() {
-    const that = this;
-    window.openModalWindow.addEventListener((_activeTab)=>{
-      that.setState({
-        isOpen: true,
-        activeTab: _activeTab
-      });
-    });
-  },
+    componentWillMount() {
+        const that = this;
+        window.openModalWindow.addEventListener((_activeTab) => {
+            that.setState({
+                isOpen: true,
+                activeTab: _activeTab
+            });
+        });
+    },
 
-  closeModal() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  },
+    closeModal() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    },
 
-  render() {
-    return (<div className="data-panel-wrapper modal-wrapper fixed flex flex-center" id="data-panel-wrapper" aria-hidden={!this.state.isOpen}>
-            <div onClick={this.closeModal} id="data-panel-overlay" className="modal-overlay absolute" tabIndex="-1"></div>
-            <div id="data-panel" className="data-panel modal-content mx-auto v-middle" aria-labelledby="modalTitle" aria-describedby="modalDescription" role="dialog">
-            <button onClick={this.closeModal} className="btn btn-close-modal" title="Close data panel" data-target="close-modal"><i className='icon icon-close'></i></button>
-            <Tabs terria={this.props.terria} activeTab={this.state.activeTab}/>
+    render() {
+        return (
+            <div className="data-panel-wrapper modal-wrapper fixed flex flex-center" id="data-panel-wrapper" aria-hidden={ !this.state.isOpen }>
+              <div onClick={ this.closeModal } id="data-panel-overlay" className="modal-overlay absolute" tabIndex="-1"></div>
+              <div id="data-panel" className="data-panelm odal-content mx-auto v-middle" aria-labelledby="modalTitle" aria-describedby="modalDescription" role="dialog">
+                <button onClick={ this.closeModal } className="btn btn-close-modal" title="Close data panel" data-target="close-modal"><i className='icon icon-close'></i></button>
+                <Tabs terria={ this.props.terria } activeTab={ this.state.activeTab } />
+              </div>
             </div>
-            </div>);
-  }
+            );
+    }
 });
 module.exports = ModalWindow;
