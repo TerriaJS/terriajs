0d539858898ad21ec42453702a6d9b07f191994e
chloe <chloeleichen@gmail.com>
2016-01-27 17:17:52 +1100

    dummy chart

--- lib/ReactViews/Chart.jsx
+++ lib/ReactViews/Chart.jsx
@@ -1,11 +1,21 @@
 'use strict';
 const React = require('react');
 // This is the wrapper for the chart component, not completed yet
+
 const Chart = React.createClass({
+    getDefaultProps() {
+        return {
+            width: 300,
+            height: 200,
+            color: '#FFF'
+        };
+    },
     render() {
         return (
           <div className='chart'>
-            Chart
+            <svg width={this.props.width} height={this.props.height}>
+              <path d={0} stroke={this.props.color} strokeWidth={this.props.stroke} fill="none" />
+            </svg>
           </div>);
     }
 });
