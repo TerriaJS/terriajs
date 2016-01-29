343eb792592e43817f64414393ab6c262d8c30f9
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-01-29 13:29:48 +1100

    Tests for combineFilters and serializeToJson.

--- test/Models/CatalogSpec.js
+++ test/Models/CatalogSpec.js
@@ -10,10 +10,12 @@ var createCatalogMemberFromType = require('../../lib/Models/createCatalogMemberF
 var CatalogGroup = require('../../lib/Models/CatalogGroup');
 var GeoJsonCatalogItem = require('../../lib/Models/GeoJsonCatalogItem');
 var ImageryLayerCatalogItem = require('../../lib/Models/ImageryLayerCatalogItem');
+var WebMapServiceCatalogItem = require('../../lib/Models/WebMapServiceCatalogItem');
 
 
 describe('Catalog', function() {
-    var terria;
+    var terria
+    var catalog;
 
     beforeEach(function() {
         terria = new Terria({
@@ -22,6 +24,8 @@ describe('Catalog', function() {
         createCatalogMemberFromType.register('group', CatalogGroup);
         createCatalogMemberFromType.register('item', CatalogItem);
         createCatalogMemberFromType.register('imageryLayerCatalogItem', ImageryLayerCatalogItem);
+        createCatalogMemberFromType.register('wms', WebMapServiceCatalogItem);
+        catalog = terria.catalog;
     });
 
     it('can register group and geojson, and update from json', function(done) {
@@ -39,11 +43,6 @@ describe('Catalog', function() {
     });
 
     describe('updateByShareKeys', function () {
-        var catalog;
-
-        beforeEach(function() {
-            catalog = terria.catalog;
-        });
 
         it('works when resolving by id', function (done) {
             catalog.updateFromJson([
@@ -213,6 +212,75 @@ describe('Catalog', function() {
                 expect(catalog.group.items[0].opacity).toBe(0.3);
                 done();
             }).otherwise(fail);
+        });
+    });
+
+    describe('serializeToJson', function () {
+        beforeEach(function(done) {
+            catalog.updateFromJson([
+                {
+                    name: 'A',
+                    type: 'group',
+                    items: [
+                        {
+                            id: 'C',
+                            name: 'C',
+                            type: 'wms'
+                        }
+                    ]
+                },
+                {
+                    name: 'B',
+                    type: 'group',
+                    items: [
+                        {
+                            id: 'D',
+                            name: 'D',
+                            type: 'wms'
+                        }
+                    ]
+                }
+            ]).then(done);
+        });
+
+        it('serializes the catalog recursively', function() {
+            var serialized = catalog.serializeToJson();
+
+            expect(serialized.length).toBe(2);
+            expect(serialized[0].name).toBe('A');
+            expect(serialized[1].items.length).toBe(1);
+            expect(serialized[1].items[0].name).toBe('D');
+        });
+
+        it('can round-trip a basic catalog', function(done) {
+            var serialized = catalog.serializeToJson();
+            var newCatalog = new Catalog(terria);
+            newCatalog.updateFromJson(serialized).then(function() {
+                expect(newCatalog.items).toEqual(catalog.items);
+                done();
+            }).otherwise(fail);
+        });
+
+        it('ignores properties filtered out by propertyFilter', function() {
+            var serialized = catalog.serializeToJson({
+                propertyFilter: function(property, item) {
+                    return property !== 'name';
+                }
+            });
+
+            expect(serialized[0].name).toBeUndefined();
+            expect(serialized[0].id).toBe('Root Group/A');
+        });
+
+        it('ignores items filtered out by itemFilter', function() {
+            var serialized = catalog.serializeToJson({
+                itemFilter: function(item) {
+                    return item.name !== 'C';
+                }
+            });
+
+            expect(serialized[0].items.length).toBe(0);
+            expect(serialized[1].items.length).toBe(1);
         });
     });
 });
