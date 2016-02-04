078e9633bbde6e83129cf3aa0efb299ec6c0d9ae
chloe <chloeleichen@gmail.com>
2016-02-04 17:06:06 +1100

    init

---
+++ lib/ReactViews/DragDropFile.jsx
@@ -0,0 +1,9 @@
+'use strict';
+import React from 'react';
+
+const DragDropFile = React.createClass({
+    render() {
+        return <div className='drag-drop-file '></div>;
+    }
+});
+module.exports = DragDropFile;
