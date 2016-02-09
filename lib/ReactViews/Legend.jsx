5e5f433e82514bc0f91937878bfd7c94136290ee
chloe <chloeleichen@gmail.com>
2016-02-09 14:55:09 +1100

    legend style

--- lib/ReactViews/Legend.jsx
+++ lib/ReactViews/Legend.jsx
@@ -1,4 +1,6 @@
 'use strict';
+
+import Loader from './Loader.jsx';
 import ObserveModelMixin from './ObserveModelMixin';
 import proxyCatalogItemUrl from '../Models/proxyCatalogItemUrl';
 import React from 'react';
@@ -29,39 +31,45 @@ const Legend = React.createClass({
     },
 
     renderLegends() {
-        let legends = this.getLegends() && this.getLegends().map((legend, i)=>{
-            if(legend.isImage) {
-                if (legend.imageHasError) {return null;}
+        let legends;
+        if(this.props.nowViewingItem.isLoading) {
+            legends = <Loader/>;
+        }
+        else {
+            legends = this.getLegends() && this.getLegends().map((legend, i)=>{
+                if(legend.isImage) {
+                    if (legend.imageHasError) {return null;}
 
-                if (legend.insertDirectly) {
-                    return (<div key={i} onError={this.onImageError.bind(this, legend)} className="legend--svg" dangerouslySetInnerHTML={legend.safeSvgContent}></div>);
+                    if (legend.insertDirectly) {
+                        return (<li key={i} onError={this.onImageError.bind(this, legend)} className="legend--svg" dangerouslySetInnerHTML={legend.safeSvgContent}></li>);
+                    }
+
+                    return (<li><a key={i} onError={this.onImageError.bind(this, legend)} href={legend.url} target="_blank">
+                                <div className="legend--image">
+                                    <img src={legend.url}/>
+                                </div>
+                            </a></li>);
                 }
+                return (<li><a key={i} className="legend--text"
+                           href={legend.url}
+                           target="_blank">Open legend in a separate tab
+                        </a></li>);
+            });
 
-                return (<a key={i} onError={this.onImageError.bind(this, legend)} href={legend.url} target="_blank">
-                            <div className="legend--image">
-                                <img src={legend.url}/>
-                            </div>
-                        </a>);
-            }
-            return (<a key={i} className="legend--text"
-                       href={legend.url}
-                       target="_blank">Open legend in a separate tab
-                    </a>);
-        });
-
-        if(!this.getLegends || this.getLegends().length === 0) {
-            if (this.props.nowViewingItem.type === 'abs-itt') {
-                legends = <div className="legend--text">Select at least one code in each data area.</div>;
-            }
-            else {
-                legends = <div className="legend--text">No legend available for this data item.</div>;
+              if(!this.getLegends || this.getLegends().length === 0) {
+                if (this.props.nowViewingItem.type === 'abs-itt') {
+                    legends = <li className="legend--text">Select at least one code in each data area.</li>;
+                }
+                else {
+                    legends = <li className="legend--text">No legend available for this data item.</li>;
+                }
             }
         }
         return legends;
     },
 
     render() {
-        return <div className='legend'>{this.renderLegends()}</div>;
+        return <ul className='legend'>{this.renderLegends()}</ul>;
     }
 });
 module.exports = Legend;
