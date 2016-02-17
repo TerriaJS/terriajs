d5d90d345a1da7e05d1f3be8e0d56031c05b3d47
chloe <chloeleichen@gmail.com>
2016-02-17 16:20:03 +1100

    my location

--- lib/ReactViews/MyLocation.jsx
+++ lib/ReactViews/MyLocation.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ObserveModelMixin from './ObserveModelMixin';
 import Rectangle from 'terriajs-cesium/Source/Core/Rectangle';
+import GeoJsonCatalogItem from '../Models/GeoJsonCatalogItem';
 
 const MyLocation = React.createClass({
     mixins: [ObserveModelMixin],
@@ -22,8 +23,30 @@ const MyLocation = React.createClass({
         const longitude = position.coords.longitude;
         const latitude = position.coords.latitude;
         // west, south, east, north, result
-        const rectangle = Rectangle.fromDegrees(longitude - 0.1, latitude + 0.1, longitude + 0.1, latitude + 0.1);
+        const rectangle = Rectangle.fromDegrees(longitude - 0.1, latitude - 0.1, longitude + 0.1, latitude + 0.1);
         this.props.terria.currentViewer.zoomTo(rectangle);
+
+        const marker = new GeoJsonCatalogItem(this.props.terria);
+        marker.name = 'Location';
+        marker.data = {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            },
+            properties: {
+                title: 'Location',
+                longitude: longitude,
+                latitude: latitude
+            }
+        };
+        marker.style = {
+            'marker-size': 25,
+            'marker-color': '#08ABD5',
+            'stroke': '#ffffff',
+            'stroke-width': 3
+        };
+        marker.isEnabled = true;
     },
 
     handleCick() {
