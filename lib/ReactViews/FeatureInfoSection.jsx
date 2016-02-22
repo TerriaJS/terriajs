e6e420eb022083ecab70343e8474b9cf5b96e0be
Arthur Street <arthurstreet@yahoo.com>
2016-02-22 12:37:55 +1100

    add example charts.json, let template be a string or an object in reactview, ensure template is in a single <div>

--- lib/ReactViews/FeatureInfoSection.jsx
+++ lib/ReactViews/FeatureInfoSection.jsx
@@ -12,7 +12,7 @@ import parseCustomHtmlToReact from '../Models/parseCustomHtmlToReact';
 // Individual feature info section
 const FeatureInfoSection = React.createClass({
     propTypes: {
-        template: React.PropTypes.object,
+        template: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.string]),
         feature: React.PropTypes.object,
         clock: React.PropTypes.object,
         catalogItem: React.PropTypes.object,
@@ -65,7 +65,7 @@ const FeatureInfoSection = React.createClass({
             ADD_TAGS: CustomComponents.names(),
             ADD_ATTR: CustomComponents.attributes()
         });
-        return parseCustomHtmlToReact(html, this.props.catalogItem, this.props.feature);
+        return parseCustomHtmlToReact('<div>' + html + '</div>', this.props.catalogItem, this.props.feature);
     },
 
     render() {
