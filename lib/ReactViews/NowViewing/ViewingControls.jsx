7236f5a235d23a70703e8163460123345b318e87
Arthur Street <arthurstreet@yahoo.com>
2016-02-23 13:27:12 +1100

    only show relevant viewing controls

--- lib/ReactViews/NowViewing/ViewingControls.jsx
+++ lib/ReactViews/NowViewing/ViewingControls.jsx
@@ -29,12 +29,30 @@ const ViewingControls = React.createClass({
 
     render() {
         const nowViewingItem = this.props.nowViewingItem;
+        let zoomButton = null;
+        let infoButton = null;
+        let removeButton = null;
+        let showButton = null;
+        if (nowViewingItem.isMappable) {
+            zoomButton = <li className='zoom'><button onClick={this.zoom} data-key={this.props.index} title="Zoom in data" className="btn">Zoom To</button></li>;
+        }
+        if (nowViewingItem.showsInfo) {
+            infoButton = <li className='info'><button onClick={this.previewItem} className='btn' title='info'>info</button></li>;
+        }
+        removeButton = <li className='remove'><button onClick={this.removeFromMap} title="Remove this data" className="btn">Remove</button></li>;
+        if (nowViewingItem.supportsToggleShown) {
+            showButton = (
+                <li className='visibility'>
+                    <button onClick={this.toggleVisibility} title="Data show/hide" className={'btn btn--visibility ' + (nowViewingItem.isShown ? 'is-visible' : '')}></button>
+                </li>
+            );
+        }
         return (
             <ul className="now-viewing__item__control">
-                <li className='zoom'><button onClick={this.zoom} data-key={this.props.index} title="Zoom in data" className="btn">Zoom To</button></li>
-                <li className='info'><button onClick={this.previewItem} className='btn' title='info'>info</button></li>
-                <li className='remove'><button onClick={this.removeFromMap} title="Remove this data" className="btn">Remove</button></li>
-                <li className='visibility'><button onClick={this.toggleVisibility} title="Data show/hide" className={'btn btn--visibility ' + (nowViewingItem.isShown ? 'is-visible' : '')}></button></li>
+                {zoomButton}
+                {infoButton}
+                {removeButton}
+                {showButton}
             </ul>
         );
     }
