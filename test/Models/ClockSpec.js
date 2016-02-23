76d1eeb84d6899d7ef2e7db917683efeca07fc07
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-02-23 17:23:12 +1100

    Completed tests for clock extension

--- test/Models/ClockSpec.js
+++ test/Models/ClockSpec.js
@@ -17,26 +17,28 @@ describe('Clock', function() {
         });
         clock = new Clock();
         catalogItem = new ImageryLayerCatalogItem(terria);
-        catalogItem.clock = new DataSourceClock({
-            startTime: JulianDate.fromIso8601('2016-01-01'),
-            stopTime: JulianDate.fromIso8601('2016-01-24')
-        });
+        catalogItem.clock = new DataSourceClock();
+        catalogItem.clock.startTime = JulianDate.fromIso8601('2016-01-01');
+        catalogItem.clock.stopTime = JulianDate.fromIso8601('2016-01-27');
+        catalogItem.clock.multiplier = 1;
         clock.setCatalogItem(catalogItem);
     });
 
-    it('should allow catalogTime to be get and set as normal if no intervals defined', function() {
-        var date = JulianDate.fromIso8601('1941-12-05');
+    describe('with no intervals', function() {
+        it('should allow catalogTime to be get and set as normal', function() {
+            var date = JulianDate.fromIso8601('1941-12-05');
 
-        expect(clock.currentTime).not.toBe(date);
-        clock.currentTime = date;
-        expect(clock.currentTime).toBe(date);
+            expect(clock.currentTime).not.toBe(date);
+            clock.currentTime = date;
+            expect(clock.currentTime).toBe(date);
+        });
     });
 
     describe('when set with a catalogItem that has intervals', function() {
         beforeEach(function() {
             catalogItem.intervals = new TimeIntervalCollection([
                 new TimeInterval({
-                    start: JulianDate.fromIso8601('2016-01-01'),
+                    start: JulianDate.fromIso8601('2016-01-03'),
                     stop: JulianDate.fromIso8601('2016-01-07')
                 }),
                 new TimeInterval({
@@ -50,9 +52,43 @@ describe('Clock', function() {
             ]);
         });
 
+        it('should advance across a gap on tick if the tick would put currentTime into a gap', function() {
+            clock.currentTime = JulianDate.fromIso8601('2016-01-07');
+            clock.tick();
+
+            expect(clock.currentTime).toEqual(JulianDate.fromIso8601('2016-01-12'));
+        });
+
+        describe('determineGap', function() {
+            it('should correctly determine a gap between intervals', function() {
+                var gap = clock.determineGap(JulianDate.fromIso8601('2016-01-18'));
+
+                expect(gap.start).toEqual(JulianDate.fromIso8601('2016-01-15'));
+                expect(gap.stop).toEqual(JulianDate.fromIso8601('2016-01-20'));
+            });
+
+            it('should return undefined if called for a date that the clock has an interval for', function() {
+                expect(clock.determineGap(JulianDate.fromIso8601('2016-01-13'))).toBeUndefined();
+            });
+
+            it('should start a gap requested before the first interval at the start of the clock\'s time', function() {
+                var gap = clock.determineGap(JulianDate.fromIso8601('2016-01-02'));
+
+                expect(gap.start).toEqual(JulianDate.fromIso8601('2016-01-01'));
+                expect(gap.stop).toEqual(JulianDate.fromIso8601('2016-01-03'));
+            });
+
+            it('should end a gap requested after the last interval at the end of the clock\'s time', function() {
+                var gap = clock.determineGap(JulianDate.fromIso8601('2016-01-25'));
+
+                expect(gap.start).toEqual(JulianDate.fromIso8601('2016-01-24'));
+                expect(gap.stop).toEqual(JulianDate.fromIso8601('2016-01-27'));
+            });
+        });
+
         describe('currentTime', function() {
             it('is set as normal if time is within interval', function() {
-                var date = JulianDate.fromIso8601('2016-01-02');
+                var date = JulianDate.fromIso8601('2016-01-04');
 
                 expect(clock.currentTime).not.toBe(date);
                 clock.currentTime = date;
@@ -62,6 +98,16 @@ describe('Clock', function() {
             it('is shifted to start of next interval if in a gap', function() {
                 clock.currentTime = JulianDate.fromIso8601('2016-01-08');
                 expect(clock.currentTime).toEqual(JulianDate.fromIso8601('2016-01-12'));
+            });
+
+            it('is shifted to start of next interval if before any interval', function() {
+                clock.currentTime = JulianDate.fromIso8601('2016-01-02');
+                expect(clock.currentTime).toEqual(JulianDate.fromIso8601('2016-01-03'));
+            });
+
+            it('is shifted to the end if after all the intervals', function() {
+                clock.currentTime = JulianDate.fromIso8601('2016-01-26');
+                expect(clock.currentTime).toEqual(JulianDate.fromIso8601('2016-01-27'));
             });
         });
     });
