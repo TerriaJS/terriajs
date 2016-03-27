cffff058e6d2645f07cfe158064709a2641dfb49
Chloe Chen <chloeleichen@gmail.com>
2016-03-27 11:58:48 +1100

    imporve workbench styles

--- lib/ReactViews/SidePanel.jsx
+++ lib/ReactViews/SidePanel.jsx
@@ -6,6 +6,7 @@ import NowViewingContainer from './NowViewing/NowViewingContainer.jsx';
 import ObserveModelMixin from './ObserveModelMixin';
 import React from 'react';
 import SidebarSearch from './SidebarSearch.jsx';
+import SearchBox from './Search/SearchBox.jsx';
 
 const SidePanel = React.createClass({
     mixins: [ObserveModelMixin],
@@ -23,23 +24,34 @@ const SidePanel = React.createClass({
         this.props.viewState.openAddData();
     },
 
+    getInitialState() {
+        return {
+            searchText: ''
+        }
+    },
+
+    search(newText) {
+        this.setState({
+            searchText: newText
+        });
+    },
+
     render() {
         const terria = this.props.terria;
-
         return (
             <div className={'workbench__inner ' + (this.props.viewState.isNowViewingOnly ? 'is-now-viewing' : '')}>
+                <SearchBox onSearchTextChanged={this.search}/>
                 <div className='workbench__add-data'>
                     <button onClick={this.onAddDataClicked} className='btn'>Add Data</button>
                 </div>
-                <SidebarSearch terria={this.props.terria}
-                               viewState={this.props.viewState}
-                               searches={[
-                                   new BingMapsSearchProviderViewModel({terria}),
-                                   new GazetteerSearchProviderViewModel({terria})
-                               ]}/>
-                <div className="now-viewing">
-                    {this.renderNowViewing()}
-                </div>
+                {this.state.searchText.length > 0 && <SidebarSearch terria={this.props.terria}
+                                                                     viewState={this.props.viewState}
+                                                                     searches={[
+                                                                         new BingMapsSearchProviderViewModel({terria}),
+                                                                         new GazetteerSearchProviderViewModel({terria})
+                                                                     ]}
+                                                                    searchText={this.state.searchText}/>}
+                {this.state.searchText.length === 0 && this.renderNowViewing()}
             </div>
         );
     },
@@ -47,7 +59,7 @@ const SidePanel = React.createClass({
     renderNowViewing() {
         if (this.props.terria.nowViewing.items && this.props.terria.nowViewing.items.length > 0) {
             return (
-                <div>
+                <div className="now-viewing">
                     <ul className="now-viewing__header">
                         <li><label className='label'>Data Sets</label></li>
                         <li><label className='label--badge label'>{this.props.terria.nowViewing.items.length}</label></li>
