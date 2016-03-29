14ced9001c4f75ea9dbe854aeb47e01b56b03005
chloe <chloeleichen@gmail.com>
2016-03-29 12:21:34 +1100

    remove figure margin

--- lib/ReactViews/Analytics/BooleanParameterEditor.jsx
+++ lib/ReactViews/Analytics/BooleanParameterEditor.jsx
@@ -23,11 +23,11 @@ const BooleanParameterEditor = React.createClass({
         if(state === true) {
             name = this.props.parameter.trueName || this.props.parameter.name;
             description = this.props.parameter.trueDescription || this.props.parameter.description;
-            classNames = this.props.parameterValues[this.props.parameter.id] ? 'btn--radio-on' : 'btn--radio-off';
+            classNames = this.props.parameterValues[this.props.parameter.id] && this.props.parameterValues[this.props.parameter.id] === true ? 'btn--radio-on' : 'btn--radio-off';
         } else {
             name = this.props.parameter.falseName || this.props.parameter.name;
             description = this.props.parameter.falseDescription || this.props.parameter.description;
-            classNames = this.props.parameterValues[this.props.parameter.id] ? 'btn--radio-off' : 'btn--radio-on';
+            classNames = this.props.parameterValues[this.props.parameter.id] && this.props.parameterValues[this.props.parameter.id] === true ? 'btn--radio-off' : 'btn--radio-on';
 
         }
         return (<div className='radio'>
