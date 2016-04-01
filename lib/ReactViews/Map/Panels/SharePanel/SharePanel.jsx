ccf2f9e372aded30a068bd3b382e2b8362fabcc6
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-04-01 14:11:00 +1100

    Finished URL shortening in share panel.

--- lib/ReactViews/Map/Panels/SharePanel/SharePanel.jsx
+++ lib/ReactViews/Map/Panels/SharePanel/SharePanel.jsx
@@ -1,8 +1,9 @@
 'use strict';
 
 import React from 'react';
-import buildShareLink from './BuildShareLink';
+import {buildShareLink, buildShortShareLink} from './BuildShareLink';
 import ObserverModelMixin from '../../../ObserveModelMixin';
+import defined from 'terriajs-cesium/Source/Core/defined';
 
 const SharePanel = React.createClass({
     mixins: [ObserverModelMixin],
@@ -10,16 +11,59 @@ const SharePanel = React.createClass({
     propTypes: {
         terria: React.PropTypes.object,
         userPropWhiteList: React.PropTypes.array,
-        shortenUrls: React.PropTypes.bool,
         isOpen: React.PropTypes.bool
     },
 
     getDefaultProps() {
         return {
-            userPropWhiteList: ['hideExplorerPanel', 'activeTabId'],
-            shortenUrls: true,
             isOpen: false
         };
+    },
+
+    getInitialState() {
+        return {
+            imageUrl: '',
+            shareUrl: ''
+        };
+    },
+
+    componentWillMount() {
+        this.props.terria.currentViewer.captureScreenshot().then(dataUrl => {
+            this.setState({
+                imageUrl: dataUrl
+            });
+        });
+
+        this.updateForShortening();
+    },
+
+    updateForShortening() {
+        this.setState({
+            shareUrl: ''
+        });
+
+        if (this.shouldShorten()) {
+            this.setState({
+                placeholder: 'Shortening...'
+            });
+
+            buildShortShareLink(this.props.terria)
+                .then(shareUrl => this.setState({shareUrl}))
+                .otherwise(() => {
+                    this.setUnshortenedUrl();
+                    this.setState({
+                        errorMessage: 'An error occurred while attempting to shorten the URL.  Please check your internet connection and try again.'
+                    });
+                });
+        } else {
+            this.setUnshortenedUrl();
+        }
+    },
+
+    setUnshortenedUrl() {
+        this.setState({
+            shareUrl: buildShareLink(this.props.terria)
+        });
     },
 
     getInitialState() {
@@ -33,28 +77,51 @@ const SharePanel = React.createClass({
     },
 
     shouldShorten() {
-        return this.props.shortenUrls && this.isUrlShortenable;
+        const localStoragePref = this.props.terria.getLocalProperty('shortenShareUrls');
+
+        return this.isUrlShortenable() && (localStoragePref || !defined(localStoragePref));
+    },
+
+    onShortenClicked(e) {
+        if (this.shouldShorten()) {
+            this.props.terria.setLocalProperty('shortenShareUrls', false);
+        } else if (this.isUrlShortenable()) {
+            this.props.terria.setLocalProperty('shortenShareUrls', true);
+        } else {
+            return;
+        }
+
+        this.updateForShortening();
+        this.forceUpdate();
     },
 
     render() {
         // Only generate it if we're currently open
         if (this.props.isOpen) {
-            const shareLink = buildShareLink(this.props.terria);
+            const iframeCode = this.state.shareUrl.length ? `<iframe style="width: 720px; height: 405px; border: none;"` +
+                `src="${this.state.shareUrl}" allowFullScreen mozAllowFullScreen webkitAllowFullScreen></iframe>` : '';
 
             return (
                 <div>
-                    <label className='label label--setting-panel dd-panel__section'>Share</label>
-                    <img></img>
+                    <h3 className='dd_panel-header dd-panel__section'>Share</h3>
+                    <div className="dd-panel__section">
+                        <img src={this.state.imageUrl}/>
+                    </div>
                     <label className="dd-panel__section">
                         To copy to clipboard, click the link below and press CTRL+C or ⌘+C:
-                        <input type="text" value={shareLink.longUrl} readOnly/>
+                        <input className="field" type="text" value={this.state.shareUrl}
+                               placeholder={this.state.placeholder} readOnly
+                               onClick={e => e.target.select()}/>
                     </label>
                     <label className="dd-panel__section">
                         To embed, copy this code to embed this map into an HTML page:
-                        <input type="text"/>
+                        <input className="field" type="text" readOnly placeholder={this.state.placeholder}
+                               value={iframeCode}
+                               onClick={e => e.target.select()}/>
                     </label>
                     <label className="dd-panel__section">
-                        <input type="checkbox"/>
+                        <input type="checkbox" checked={this.shouldShorten()} onChange={this.onShortenClicked}
+                               disabled={!this.isUrlShortenable()}/>
                         Shorten the share URL using a web service
                     </label>
                 </div>
