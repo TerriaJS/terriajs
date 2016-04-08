10feb09b2f3d471a490f885d215430d9969e2bef
Arthur Street <arthurstreet@yahoo.com>
2016-04-08 16:03:53 +1000

    add tests for the legend coloring as well as the point coloring

--- test/Models/LegendHelperSpec.js
+++ test/Models/LegendHelperSpec.js
@@ -4,8 +4,15 @@
 var LegendHelper = require('../../lib/Models/LegendHelper');
 var TableColumn = require('../../lib/Map/TableColumn');
 var TableStyle = require('../../lib/Models/TableStyle');
+require('../ArrayFromPolyfill');
 
 describe('LegendHelper', function() {
+
+    var tableColumn;
+
+    beforeEach(function() {
+        tableColumn = new TableColumn('foo', [9, 5, 1]);
+    });
 
     it('can be instantiated with nothing', function() {
         var legendHelper = new LegendHelper();
@@ -13,45 +20,66 @@ describe('LegendHelper', function() {
         expect(legendHelper.legendUrl()).not.toBeDefined();
     });
 
-    // These tests only test point coloring, not the way it appears in the legend.
-    describe('point coloring', function() {
+    it('colors different values differently by default', function() {
+        var legendHelper = new LegendHelper(tableColumn);
+        expect(legendHelper).toBeDefined();
+        expect(legendHelper.legendUrl()).toBeDefined();  // This is called for its side-effects. Hmmm.
+        expect(legendHelper.getColorArrayFromValue(9)).not.toEqual(legendHelper.getColorArrayFromValue(5));
+        expect(legendHelper.getColorArrayFromValue(1)).not.toEqual(legendHelper.getColorArrayFromValue(5));
+        var legend = legendHelper._legend;
+        expect(legend.items.length).toEqual(3);
+        expect(+legend.items[0].titleAbove).toEqual(1);
+        expect(getColorArrayFromCssColorString(legend.items[0].color)).toEqual(legendHelper.getColorArrayFromValue(1));
+        expect(getColorArrayFromCssColorString(legend.items[1].color)).toEqual(legendHelper.getColorArrayFromValue(5));
+        expect(getColorArrayFromCssColorString(legend.items[2].color)).toEqual(legendHelper.getColorArrayFromValue(9));
+    });
 
-        var tableColumn;
+    it('can be forced to color different values the same', function() {
+        var tableStyle = new TableStyle({colorMap: 'red-red'});
+        var legendHelper = new LegendHelper(tableColumn, tableStyle);
+        expect(legendHelper).toBeDefined();
+        expect(legendHelper.legendUrl()).toBeDefined();  // Side-effects. Hmmm.
+        expect(legendHelper.getColorArrayFromValue(9)).toEqual(legendHelper.getColorArrayFromValue(5));
+        expect(legendHelper.getColorArrayFromValue(1)).toEqual(legendHelper.getColorArrayFromValue(5));
+        var legend = legendHelper._legend;
+        expect(legend.items.length).toEqual(3);
+        expect(+legend.items[0].titleAbove).toEqual(1);
+        expect(getColorArrayFromCssColorString(legend.items[0].color)).toEqual(legendHelper.getColorArrayFromValue(1));
+        expect(getColorArrayFromCssColorString(legend.items[1].color)).toEqual(legendHelper.getColorArrayFromValue(5));
+        expect(getColorArrayFromCssColorString(legend.items[2].color)).toEqual(legendHelper.getColorArrayFromValue(9));
+    });
 
-        beforeEach(function() {
-            tableColumn = new TableColumn('foo', [9, 5, 1]);
+    it('colors points via a color gradient when colorBins is 0', function() {
+        // This tests the implementation of the color gradient code, which may not be desirable.
+        var tableStyle = new TableStyle({
+            colorMap: '#000-#00F',
+            colorBins: 0
         });
-
-        it('colors different values differently by default', function() {
-            var legendHelper = new LegendHelper(tableColumn);
-            expect(legendHelper).toBeDefined();
-            expect(legendHelper.legendUrl()).toBeDefined();  // This is called for its side-effects. Hmmm.
-            expect(legendHelper.getColorArrayFromValue(9)).not.toEqual(legendHelper.getColorArrayFromValue(5));
-            expect(legendHelper.getColorArrayFromValue(1)).not.toEqual(legendHelper.getColorArrayFromValue(5));
-        });
-
-        it('can be forced to color different values the same', function() {
-            var tableStyle = new TableStyle({colorMap: 'red-red'});
-            var legendHelper = new LegendHelper(tableColumn, tableStyle);
-            expect(legendHelper).toBeDefined();
-            expect(legendHelper.legendUrl()).toBeDefined();  // Side-effects. Hmmm.
-            expect(legendHelper.getColorArrayFromValue(9)).toEqual(legendHelper.getColorArrayFromValue(5));
-            expect(legendHelper.getColorArrayFromValue(1)).toEqual(legendHelper.getColorArrayFromValue(5));
-        });
-
-        it('colors points via a color gradient when colorBins is 0', function() {
-            var tableStyle = new TableStyle({
-                colorMap: '#000-#00F',
-                colorBins: 0
-            });
-            var legendHelper = new LegendHelper(tableColumn, tableStyle);
-            expect(legendHelper).toBeDefined();
-            expect(legendHelper.legendUrl()).toBeDefined();  // Side-effects. Hmmm.
-            expect(legendHelper.getColorArrayFromValue(9)).toEqual([0, 0, 255, 255]);
-            expect(legendHelper.getColorArrayFromValue(5)).toEqual([0, 0, 128, 255]);
-            expect(legendHelper.getColorArrayFromValue(1)).toEqual([0, 0, 0, 255]);
-        });
-
+        var legendHelper = new LegendHelper(tableColumn, tableStyle);
+        expect(legendHelper).toBeDefined();
+        expect(legendHelper.legendUrl()).toBeDefined();  // Side-effects. Hmmm.
+        expect(legendHelper.getColorArrayFromValue(9)).toEqual([0, 0, 255, 255]);
+        expect(legendHelper.getColorArrayFromValue(5)).toEqual([0, 0, 128, 255]);
+        expect(legendHelper.getColorArrayFromValue(1)).toEqual([0, 0, 0, 255]);
+        var legend = legendHelper._legend;
+        var numItems = legend.items.length;
+        expect(+legend.items[0].titleBelow).toEqual(1);
+        expect(+legend.items[numItems - 1].titleAbove).toEqual(9);
+        expect(getColorArrayFromCssColorString(legend.gradientColorMap[0].color)).toEqual(legendHelper.getColorArrayFromValue(1));
+        expect(getColorArrayFromCssColorString(legend.gradientColorMap[1].color)).toEqual(legendHelper.getColorArrayFromValue(9));
     });
 
 });
+
+// This is copied from LegendHelper, without the alpha override parameter.
+// It also converts the result from a Uint8ClampedArray to an Array (requires a polyfill in IE).
+function getColorArrayFromCssColorString(cssString) {
+    var canvas = document.createElement("canvas");
+    if (!canvas) {
+        return defaultColorArray; // Failed
+    }
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = cssString;
+    ctx.fillRect(0, 0, 2, 2);
+    return Array.from(ctx.getImageData(0, 0, 1, 1).data);
+}
