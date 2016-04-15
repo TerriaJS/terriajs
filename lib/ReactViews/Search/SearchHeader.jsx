80040d8d65f40f132b08334b0972f4485bc0a276
Kevin Ring <kevin@kotachrome.com>
2016-04-15 20:50:34 +1000

    Make the app start up again.
    This might undo some of Alex's eslint work.  Sorry Alex.

--- lib/ReactViews/Search/SearchHeader.jsx
+++ lib/ReactViews/Search/SearchHeader.jsx
@@ -2,18 +2,17 @@
 
 import React from 'react';
 import Loader from '../Loader.jsx';
-/**
- *  Renders either a loader or a message based off search state.
- *
- *  @param {Object} props props
- *  @returns {ReactComponent} The component
- */
-export default props => {
-    if (this.props.isSearching) {
-        return <div><Loader key="loader"/></div>;
-    } else if (this.props.searchMessage) {
-        return <div key="message" className='label no-results'>{this.props.searchMessage}</div>;
-    }
+import defined from 'terriajs-cesium/Source/Core/defined';
 
-    return null;
-};
+/** Renders either a loader or a message based off search state. */
+export default React.createClass({
+    render() {
+        if (this.props.isSearching) {
+            return <div><Loader key="loader"/></div>;
+        } else if (this.props.searchMessage) {
+            return <div key="message" className='label no-results'>{this.props.searchMessage}</div>;
+        } else {
+            return null;
+        }
+    }
+});
