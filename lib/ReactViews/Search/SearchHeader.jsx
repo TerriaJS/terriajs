96b01aae62f3300273b54d5798797194a6117cef
Kevin Ring <kevin@kotachrome.com>
2016-04-17 16:28:16 +1000

    Fix eslint warnings.

--- lib/ReactViews/Search/SearchHeader.jsx
+++ lib/ReactViews/Search/SearchHeader.jsx
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import Loader from '../Loader.jsx';
-import defined from 'terriajs-cesium/Source/Core/defined';
 
 /** Renders either a loader or a message based off search state. */
 export default React.createClass({
+    propTypes: {
+        isSearching: React.PropTypes.bool,
+        searchMessage: React.PropTypes.string
+    },
+
     render() {
         if (this.props.isSearching) {
             return <div><Loader key="loader"/></div>;
