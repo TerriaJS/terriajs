9d46ef4472626bc995353ff8f81a09657115f4de
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-04-19 11:05:54 +1000

    now using CustomDataSource.

--- lib/ReactViews/Map/Navigation/MeasureTool.jsx
+++ lib/ReactViews/Map/Navigation/MeasureTool.jsx
@@ -3,16 +3,28 @@ import React from 'react';
 import ObserveModelMixin from '../../ObserveModelMixin';
 import MapInteractionMode from '../../../Models/MapInteractionMode';
 import defined from 'terriajs-cesium/Source/Core/defined';
-import knockout from 'terriajs-cesium/Source/ThirdParty/knockout';
+var Cartesian3 = require('terriajs-cesium/Source/Core/Cartesian3');
+var PolylineGraphics = require('terriajs-cesium/Source/DataSources/PolylineGraphics');
+var knockout = require('terriajs-cesium/Source/ThirdParty/knockout');
+var Color = require('terriajs-cesium/Source/Core/Color');
+var CallbackProperty = require('terriajs-cesium/Source/DataSources/CallbackProperty');
+var CustomDataSource = require('terriajs-cesium/Source/DataSources/CustomDataSource');
 var CesiumCartesian = require('terriajs-cesium/Source/Core/Cartesian3');
 
-const GeoJsonCatalogItem = require('../../../Models/GeoJsonCatalogItem');
 
 const MeasureTool = React.createClass({
     mixins: [ObserveModelMixin],
 
     propTypes: {
         terria: React.PropTypes.object
+    },
+
+    getInitialState() {
+        return {
+            pointOne : undefined,
+            pointTwo : undefined,
+            datasource: new CustomDataSource('User polygon')
+        };
     },
 
     prettifyDistance(distance) {
@@ -37,6 +49,51 @@ const MeasureTool = React.createClass({
         // Cancel any feature picking already in progress.
         terria.pickedFeatures = undefined;
 
+        that.state.datasource.entities.add({
+            name: 'First Point',
+            point : {
+                color : Color.WHITE,
+                pixelSize : 8,
+                outlineColor : Color.BLACK,
+                outlineWidth : 2
+            },
+            position: new CallbackProperty(function(date, result) {
+                if (defined(that.state.pointOne))
+                {
+                    return that.state.pointOne;
+                }
+            }, false)
+        });
+        that.state.datasource.entities.add({
+            name: 'Second Point',
+            point : {
+                color : Color.WHITE,
+                pixelSize : 8,
+                outlineColor : Color.BLACK,
+                outlineWidth : 2
+            },
+            position: new CallbackProperty(function(date, result) {
+                if (defined(that.state.pointTwo))
+                {
+                    return that.state.pointTwo;
+                }
+            }, false)
+        });
+        that.state.datasource.entities.add({
+            name: 'Line',
+            polyline: {
+                        positions : new CallbackProperty(function(date, result) {
+                                if (defined(that.state.pointOne) & defined(that.state.pointTwo))
+                                {
+                                    return [that.state.pointOne, that.state.pointTwo];
+                                }
+                        }, false),
+                        material : Color.WHITE,
+                        width : 1
+                    }
+        });
+        terria.dataSources.add(this.state.datasource);
+
         const pickPointMode = new MapInteractionMode({
             message: 'Measure Tool: Select first point.',
             onCancel: function() {
@@ -47,6 +104,7 @@ const MeasureTool = React.createClass({
 
         knockout.getObservable(pickPointMode, 'pickedFeatures').subscribe(function(pickedFeatures) {
             if (defined(pickedFeatures.pickPosition)) {
+
                 // User has picked one point, wait for second.
                 terria.mapInteractionModeStack.pop();
                 const pickPointMode = new MapInteractionMode({
@@ -58,20 +116,21 @@ const MeasureTool = React.createClass({
                 terria.mapInteractionModeStack.push(pickPointMode);
                 var firstPt = pickedFeatures.pickPosition;
                 console.log("First point picked: " + firstPt);
-                terria.userSelectedPoints.push(firstPt);
+                that.setState({pointOne : firstPt});
 
                 knockout.getObservable(pickPointMode, 'pickedFeatures').subscribe(function(pickedFeatures) {
                     if (defined(pickedFeatures.pickPosition)) {
                         // User has picked both points
                         var secondPt = pickedFeatures.pickPosition;
                         console.log("Second point picked: " + secondPt);
-                        terria.userSelectedPoints.push(secondPt);
-                        var firstPt = terria.userSelectedPoints[0];
+                        var firstPt = that.state.pointOne;
+                        that.setState({pointTwo : secondPt});
                         var distance = CesiumCartesian.distance(firstPt, secondPt);
                         console.log("DISTANCE...");
                         var distanceStr = that.prettifyDistance(distance);
                         console.log(distanceStr);
                         terria.mapInteractionModeStack.pop();
+
                     }
                 });
             }
