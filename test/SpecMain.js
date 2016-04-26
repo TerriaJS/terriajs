bb663bd2bfdf35bb53984b0445a18d0fcd1c0c4d
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-04-26 12:21:55 +1000

    Resetting timeout interval

--- test/SpecMain.js
+++ test/SpecMain.js
@@ -6,7 +6,7 @@ require('terriajs-jasmine-ajax');
 // require('babel-polyfill');  // Polyfills Map, Set and other ES6.
 
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 
 afterEach(function() {
     jasmine.Ajax.uninstall();
