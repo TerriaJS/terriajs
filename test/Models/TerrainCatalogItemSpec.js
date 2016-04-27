493ef0caf59781eb0c225895114567e05b4f6d68
Kevin Ring <kevin@kotachrome.com>
2016-04-27 11:26:38 +1000

    Add specs.

---
+++ test/Models/TerrainCatalogItemSpec.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/*global require*/
+var TerrainCatalogItem = require('../../lib/Models/TerrainCatalogItem');
+var Terria = require('../../lib/Models/Terria');
+var when = require('terriajs-cesium/Source/ThirdParty/when');
+
+describe('TerrainCatalogItem', function() {
+    var terria;
+    var item;
+
+    beforeEach(function() {
+        terria = new Terria({
+            baseUrl: './'
+        });
+        item = new TerrainCatalogItem(terria);
+    });
+
+    it('sets terrainProvider on Cesium scene when enabled', function(done) {
+        var fakeTerrainProvider = {};
+        spyOn(item, '_createTerrainProvider').and.returnValue(fakeTerrainProvider);
+
+        terria.cesium = {
+            scene: {
+                terrainProvider: undefined
+            }
+        };
+
+        item.isEnabled = true;
+
+        when(item.load()).then(function() {
+            expect(terria.cesium.scene.terrainProvider).toBe(fakeTerrainProvider);
+        }).then(done).otherwise(done.fail);
+    });
+
+    it('restores previous terrainProvider when disabled', function(done) {
+        var fakeTerrainProvider = {};
+        spyOn(item, '_createTerrainProvider').and.returnValue(fakeTerrainProvider);
+
+        var originalTerrainProvider = {};
+        terria.cesium = {
+            scene: {
+                terrainProvider: originalTerrainProvider
+            }
+        };
+
+        item.isEnabled = true;
+
+        when(item.load()).then(function() {
+            expect(terria.cesium.scene.terrainProvider).toBe(fakeTerrainProvider);
+
+            item.isEnabled = false;
+            expect(terria.cesium.scene.terrainProvider).toBe(originalTerrainProvider);
+        }).then(done).otherwise(done.fail);
+    });
+
+    it('disables other terrainProvider catalog items when enabled', function(done) {
+        var enabledItem = new TerrainCatalogItem(terria);
+        spyOn(enabledItem, '_createTerrainProvider').and.returnValue({});
+        enabledItem.isEnabled = true;
+
+        when(enabledItem.load()).then(function() {
+            spyOn(item, '_createTerrainProvider').and.returnValue({});
+            item.isEnabled = true;
+
+            return when(item.load()).then(function() {
+                expect(enabledItem.isEnabled).toBe(false);
+            });
+        }).then(done).otherwise(done.fail);
+    });
+});
