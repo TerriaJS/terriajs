d514333d7f1aa8e4489a673e7c157b016e269abc
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-04-28 14:48:54 +1000

    Added message for no data in work bench.

--- lib/ReactViews/SidePanel.jsx
+++ lib/ReactViews/SidePanel.jsx
@@ -45,34 +45,45 @@ const SidePanel = React.createClass({
                     </div>
                 </div>
                 <div className='workbench__body'>
-                    {this.state.searchText.length > 0 && <SidebarSearch terria={this.props.terria}
-                                                                         viewState={this.props.viewState}
-                                                                         searches={[
-                                                                             new BingMapsSearchProviderViewModel({terria}),
-                                                                             new GazetteerSearchProviderViewModel({terria})
-                                                                         ]}
-                                                                        searchText={this.state.searchText}/>}
-                    {this.state.searchText.length === 0 && this.renderNowViewing()}
+                    <If condition={this.state.searchText.length > 0}>
+                        <SidebarSearch terria={this.props.terria}
+                                       viewState={this.props.viewState}
+                                       searches={[
+                                           new BingMapsSearchProviderViewModel({terria}),
+                                           new GazetteerSearchProviderViewModel({terria})
+                                       ]}
+                                       searchText={this.state.searchText}/>}
+                    </If>
+                    <If condition={this.state.searchText.length === 0}>
+                        <Choose>
+                            <When condition={this.props.terria.nowViewing.items && this.props.terria.nowViewing.items.length > 0}>
+                                <div className="now-viewing">
+                                    <ul className="now-viewing__header">
+                                        <li><label className='label'>Data Sets</label></li>
+                                        <li><label className='label--badge label'>{this.props.terria.nowViewing.items.length}</label></li>
+                                        <li><button type='button' onClick={this.removeAll} className='btn right btn-remove'>Remove All</button></li>
+                                    </ul>
+                                    <NowViewingContainer viewState={this.props.viewState}
+                                                         terria={this.props.terria}
+                                    />
+                                </div>
+                            </When>
+                            <Otherwise>
+                                <div>
+                                    <h3>Your workbench is empty</h3>
+                                    <p><strong>Click 'Add Data' above to:</strong></p>
+                                    <ul>
+                                        <li>Browse the Data Catalogue</li>
+                                        <li>Load your own data onto the map</li>
+                                    </ul>
+                                    <p><strong>TIP:</strong> <em>All of your active data sets will be listed here</em></p>
+                                </div>
+                            </Otherwise>
+                        </Choose>
+                    </If>
                 </div>
             </div>
         );
-    },
-
-    renderNowViewing() {
-        if (this.props.terria.nowViewing.items && this.props.terria.nowViewing.items.length > 0) {
-            return (
-                <div className="now-viewing">
-                    <ul className="now-viewing__header">
-                        <li><label className='label'>Data Sets</label></li>
-                        <li><label className='label--badge label'>{this.props.terria.nowViewing.items.length}</label></li>
-                        <li><button type='button' onClick={this.removeAll} className='btn right btn-remove'>Remove All</button></li>
-                    </ul>
-                    <NowViewingContainer viewState={this.props.viewState}
-                                         terria={this.props.terria}
-                    />
-                </div>
-            );
-        }
     },
 });
 
