555597fda675414f84cebcf2914f89016732b7e7
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-06-01 16:50:28 +1000

    Fixed feature info tests.

--- test/ReactViews/FeatureInfoPanelSpec.jsx
+++ test/ReactViews/FeatureInfoPanelSpec.jsx
@@ -52,14 +52,14 @@ describe('FeatureInfoPanel-jsx', function() {
     it('does not have isVisible class when viewState.featureInfoPanelIsVisible not set', function() {
         const panel = <FeatureInfoPanel terria={terria} viewState={viewState}/>;
         const result = getShallowRenderedOutput(panel);
-        expect(result.props.className).not.toContain('isVisible');
+        expect(result.props.className).not.toContain('is-visible');
     });
 
     it('has isVisible class when viewState.featureInfoPanelIsVisible is true', function() {
         viewState.featureInfoPanelIsVisible = true;
         const panel = <FeatureInfoPanel terria={terria} viewState={viewState}/>;
         const result = getShallowRenderedOutput(panel);
-        expect(result.props.className).toContain('isVisible');
+        expect(result.props.className).toContain('is-visible');
     });
 
     it('displays loader while asychronously loading feature information', function() {
