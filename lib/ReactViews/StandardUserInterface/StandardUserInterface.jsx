26772702f9532a8a70682c288beeaf9c4bb225af
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-06-10 09:12:44 +1000

    fix another lint issue

--- lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx
+++ lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx
@@ -127,7 +127,6 @@ const StandardUserInterface = React.createClass({
                     </aside>
                 </If>
 
-
                 <FeatureInfoPanel terria={terria}
                                   viewState={this.props.viewState}
                 />
