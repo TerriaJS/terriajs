0873131bbd6e8b717a46e01d26a5cc8eb11f0281
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-06-30 14:39:20 +1000

    removing measuring tool

--- lib/ReactViews/Map/MapNavigation.jsx
+++ lib/ReactViews/Map/MapNavigation.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Compass from './Navigation/Compass.jsx';
 import MyLocation from './Navigation/MyLocation.jsx';
-import MeasureTool from './Navigation/MeasureTool.jsx';
 import ZoomControl from './Navigation/ZoomControl.jsx';
 import SettingPanel from './Panels/SettingPanel.jsx';
 import SharePanel from './Panels/SharePanel/SharePanel.jsx';
@@ -54,7 +53,6 @@ const MapNavigation = React.createClass({
                     <Compass terria={this.props.terria}/>
                 </If>
                 <MyLocation terria={this.props.terria}/>
-                <MeasureTool terria={this.props.terria}/>
                 <ZoomControl terria={this.props.terria}/>
                 <For each="element" of={this.props.extraNavElements}>
                     {element}
