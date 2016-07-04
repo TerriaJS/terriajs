be56f9c98e46a4a6ea5c06fd02df7b29140c8576
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-07-04 11:34:38 +1000

    changes based on code review

--- test/Map/UserDrawingSpec.js
+++ test/Map/UserDrawingSpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*global require,describe,it,expect*/
-var UserDrawing = require('../../lib/Map/UserDrawing');
+var UserDrawing = require('../../lib/Models/UserDrawing');
 var Terria = require('../../lib/Models/Terria');
 
 describe('UserDrawing', function() {
@@ -14,7 +14,8 @@ describe('UserDrawing', function() {
     });
 
     it('will use default options if options are not specified', function() {
-        var userDrawing = new UserDrawing(terria);
+        var options = { terria: terria };
+        var userDrawing = new UserDrawing(options);
 
         expect(userDrawing._getDialogMessage()).toEqual("<div><strong>Draw on Map</strong></br><i>Click to add a point</i></div>");
     });
