ec161e75437316133ac5b7750b30c2506093dabe
Kevin Ring <kevin@kotachrome.com>
2016-07-11 21:27:59 +1000

    Remove catalog item insta-add on mobile.

--- lib/ReactViews/DataCatalog/DataCatalogItem.jsx
+++ lib/ReactViews/DataCatalog/DataCatalogItem.jsx
@@ -51,7 +51,7 @@ const DataCatalogItem = React.createClass({
                 selected={this.isSelected()}
                 text={item.name}
                 btnState={this.getState()}
-                onBtnClick={defined(item.invoke) ? this.setPreviewedItem : this.toggleEnable}
+                onBtnClick={defined(item.invoke) || this.props.viewState.useSmallScreenInterface ? this.setPreviewedItem : this.toggleEnable}
             />
         );
     },
@@ -59,6 +59,8 @@ const DataCatalogItem = React.createClass({
     getState() {
         if (this.props.item.isLoading) {
             return 'loading';
+        } else if (this.props.viewState.useSmallScreenInterface) {
+            return 'preview';
         } else if (this.props.item.isEnabled) {
             return 'remove';
         } else if (!defined(this.props.item.invoke)) {
