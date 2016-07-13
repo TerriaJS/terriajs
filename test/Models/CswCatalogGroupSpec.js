2c82f541925a1759ee888e7e4a652ffbf053d586
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-07-13 15:03:22 +1000

    cleaned up CswCatalogGroupSpec following code review

--- test/Models/CswCatalogGroupSpec.js
+++ test/Models/CswCatalogGroupSpec.js
@@ -4,7 +4,7 @@
 var CswCatalogGroup = require('../../lib/Models/CswCatalogGroup');
 
 describe('CswCatalogGroup', function() {
-    it('findLevel function test', function() {
+    it('function findLevel is able to sort a hierarchy correctly', function() {
         var metadataGroups = [];
 
         var keyList = [["Multiple Use", "National"],
@@ -16,9 +16,67 @@ describe('CswCatalogGroup', function() {
             CswCatalogGroup._findLevel(keys, 0, metadataGroups, " | ", "subject");
         }
 
+        var expectedObj = [
+           {
+              field: "subject",
+              value: "^Multiple Use\\ \\|\\ ",
+              regex: true,
+              group: "Multiple Use",
+              children:[
+                 {
+                    field: "subject",
+                    value: "Multiple Use | National",
+                    regex: false,
+                    group: "National"
+                 },
+                 {
+                    field: "subject",
+                    value: "Multiple Use | Western Australia",
+                    regex: false,
+                    group: "Western Australia"
+                 }
+              ]
+           },
+           {
+              field: "subject",
+              value: "^Wave Models\\ \\|\\ ",
+              regex: true,
+              group: "Wave Models",
+              children:[
+                 {
+                    field: "subject",
+                    value: "^Wave Models\\ \\|\\ Direction of Maximum Directionally Resolved Wave Power\\ \\|\\ ",
+                    regex: true,
+                    group: "Direction of Maximum Directionally Resolved Wave Power",
+                    children: [
+                       {
+                          field: "subject",
+                          value: "Wave Models | Direction of Maximum Directionally Resolved Wave Power | Averages",
+                          regex: false,
+                          group: "Averages"
+                       }
+                    ]
+                 },
+                 {
+                    field: "subject",
+                    value: "^Wave Models\\ \\|\\ Maximum Directionally Resolved Wave Power\\ \\|\\ ",
+                    regex: true,
+                    group: "Maximum Directionally Resolved Wave Power",
+                    children: [
+                       {
+                          field: "subject",
+                          value: "Wave Models | Maximum Directionally Resolved Wave Power | Averages",
+                          regex: false,
+                          group: "Averages"
+                       }
+                    ]
+                 }
+              ]
+           }
+        ];
         // If you're trying to debug this, run it through a json formatter. Then the structure becomes clear.
-        var strExpected = '[{"field":"subject","value":"^Multiple Use\\\\ \\\\|\\\\ ","regex":true,"group":"Multiple Use","children":[{"field":"subject","value":"Multiple Use | National","regex":false,"group":"National"},{"field":"subject","value":"Multiple Use | Western Australia","regex":false,"group":"Western Australia"}]},{"field":"subject","value":"^Wave Models\\\\ \\\\|\\\\ ","regex":true,"group":"Wave Models","children":[{"field":"subject","value":"^Wave Models\\\\ \\\\|\\\\ Direction of Maximum Directionally Resolved Wave Power\\\\ \\\\|\\\\ ","regex":true,"group":"Direction of Maximum Directionally Resolved Wave Power","children":[{"field":"subject","value":"Wave Models | Direction of Maximum Directionally Resolved Wave Power | Averages","regex":false,"group":"Averages"}]},{"field":"subject","value":"^Wave Models\\\\ \\\\|\\\\ Maximum Directionally Resolved Wave Power\\\\ \\\\|\\\\ ","regex":true,"group":"Maximum Directionally Resolved Wave Power","children":[{"field":"subject","value":"Wave Models | Maximum Directionally Resolved Wave Power | Averages","regex":false,"group":"Averages"}]}]}]';
         var strActual = JSON.stringify(metadataGroups);
+        var strExpected = JSON.stringify(expectedObj);
         expect(strActual).toEqual(strExpected);
     });
 });
