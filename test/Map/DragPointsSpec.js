f397553538123da7948f6e2dcdc1be785dfa60a5
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-09-06 15:08:33 +1000

    removed non-functional tests

--- test/Map/DragPointsSpec.js
+++ test/Map/DragPointsSpec.js
@@ -10,10 +10,6 @@ var EntityCollection = require('terriajs-cesium/Source/DataSources/EntityCollect
 var CesiumWidget = require('terriajs-cesium/Source/Widgets/CesiumWidget/CesiumWidget');
 var L = require('leaflet');
 var Leaflet = require('../../lib/Models/Leaflet');
-var Cartesian3 = require('terriajs-cesium/Source/Core/Cartesian3');
-var CustomDataSource = require('terriajs-cesium/Source/DataSources/CustomDataSource');
-var CesiumDragPoints = require('../../lib/Map/CesiumDragPoints');
-var LeafletDragPoints = require('../../lib/Map/LeafletDragPoints');
 
 describe('DragPoints', function() {
     var terria;
@@ -58,7 +54,7 @@ describe('DragPoints', function() {
         expect(dragPointsHelper._dragPointsHelper.type).toEqual("Cesium");
     });
 
-    it('will inform new helper about existing entites if helper is changed to Leaflet', function() {
+    it('will inform new helper about existing entities if helper is changed to Leaflet', function() {
         var entityArray = [new Entity({"name": "first test entity"}),
                            new Entity({"name": "second test entity"})];
         terria.viewerMode = ViewerMode.CesiumTerrain;
@@ -72,7 +68,7 @@ describe('DragPoints', function() {
         expect(dragPointsHelper._dragPointsHelper._draggableObjects).toBe(entityArray);
     });
 
-    it('will inform new helper about existing entites if helper is changed to Cesium', function() {
+    it('will inform new helper about existing entities if helper is changed to Cesium', function() {
         var entityArray = [new Entity({"name": "first test entity"}),
                            new Entity({"name": "second test entity"})];
         terria.viewerMode = ViewerMode.Leaflet;
@@ -84,61 +80,5 @@ describe('DragPoints', function() {
         terria.viewerMode = ViewerMode.CesiumTerrain;
         // Same as before, but dragPointsHelper is now new.
         expect(dragPointsHelper._dragPointsHelper._draggableObjects).toBe(entityArray);
-    });
-
-    it('will only allow drag if entity clicked on is in draggable list', function() {
-        var entityArray = [new Entity({"name": "first test entity",
-                                       "position": new Cartesian3(-0.1154700545562811, -0.019245009092713514, -1.0000000062211452)}),
-                           new Entity({"name": "second test entity"})];
-        terria.viewerMode = ViewerMode.CesiumTerrain;
-
-        var dragPointsHelper = new DragPoints(terria);
-        dragPointsHelper.setUp();
-        var pointEntities = new CustomDataSource('Points');
-        pointEntities.entities.add(entityArray[0]);
-        pointEntities.entities.add(entityArray[1]);
-        terria.dataSources.add(pointEntities);
-        terria.cesium.viewer.entities.add(entityArray[0]);
-        terria.cesium.viewer.entities.add(entityArray[1]);
-
-        dragPointsHelper.updateDraggableObjects(entityArray);
-
-        var clickEvent = new MouseEvent("mousedown", {
-            'view': window,
-            'bubbles': true,
-            'cancelable': true,
-            'clientX': 128,
-            'clientY': 154
-        });
-        var target = terria.cesium.viewer.canvas;
-        target.addEventListener('mousedown', function(click) { console.log(click); });
-        target.dispatchEvent(clickEvent);
-        expect(1).toEqual(1);
-    });
-
-    it('will cancel drag on mouseup', function() {
-        expect(1).toEqual(1);
-    });
-});
-
-describe('CesiumDragPoints', function() {
-
-    it('will disable camera motion on mousedown (if drag occurs) and reenable afterwards', function() {
-        expect(1).toEqual(1);
-    });
-
-    it('will adjust position of entity being dragged to match mouse move position', function() {
-        expect(1).toEqual(1);
-    });
-});
-
-describe('LeafletDragPoints', function() {
-
-    it('will disable camera motion on mousedown (if drag occurs) and reenable afterwards', function() {
-        expect(1).toEqual(1);
-    });
-
-    it('will adjust position of entity being dragged to match mouse move position', function() {
-        expect(1).toEqual(1);
     });
 });
