ac523124d8b289326c79f2bd74191988dc9fca19
Kevin Ring <kevin@kotachrome.com>
2016-09-09 12:06:16 +1000

    Show layer title instead of name.
    Also don't show it at all if there is only one layer.

--- lib/ReactViews/Workbench/Controls/StyleSelectorSection.jsx
+++ lib/ReactViews/Workbench/Controls/StyleSelectorSection.jsx
@@ -24,11 +24,15 @@ const StyleSelectorSection = React.createClass({
         const item = this.props.item;
 
         // This section only makes sense if we have a layer that supports styles.
-        if (!defined(item.availableStyles) || !defined(item.styles) || !defined(item.layers) || item.layers.length === 0) {
+        if (item.disableUserChanges || !defined(item.availableStyles) || !defined(item.styles) || !defined(item.layers) || item.layers.length === 0) {
             return null;
         }
 
-        const layers = item.layers.split(',').map(item => item.trim());
+        const layerTitles = item.layerTitles;
+        const layers = item.layers.split(',').map((item, i) => ({
+            name: item.trim(),
+            title: layerTitles[i] || item.trim()
+        }));
 
         return (
             <div className={Styles.styleSelector}>
@@ -39,7 +43,7 @@ const StyleSelectorSection = React.createClass({
 
     renderStyleSelectorForLayer(layer) {
         const item = this.props.item;
-        const styles = item.availableStyles[layer];
+        const styles = item.availableStyles[layer.name];
         if (!defined(styles) || styles.length < 2) {
             return null;
         }
@@ -47,10 +51,12 @@ const StyleSelectorSection = React.createClass({
         // TODO: get the friendly title of the layer, instead of the name (ID).
         // TODO: handle multiple layers
 
+        const label = item.layers.indexOf(',') >= 0 ? layer.title + ' Style' : 'Style';
+
         return (
             <div key={layer}>
-                <label htmlFor={layer}>{layer} style: </label>
-                <select className={Styles.field} name={layer} value={item.styles} onChange={this.changeStyle}>
+                <label htmlFor={layer}>{label}</label>
+                <select className={Styles.field} name={layer.name} value={item.styles} onChange={this.changeStyle}>
                     {styles.map(item => <option key={item.name} value={item.name}>{item.title}</option>)}
                 </select>
             </div>
