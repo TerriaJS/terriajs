2dd850d05ca1b656e780eead927c0729fbc113ed
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2016-10-11 17:56:03 +1100

    add a test of tryToLoadObservationData true

--- test/Models/SensorObservationServiceCatalogItemSpec.js
+++ test/Models/SensorObservationServiceCatalogItemSpec.js
@@ -5,8 +5,9 @@ var loadText = require('terriajs-cesium/Source/Core/loadText');
 var Rectangle = require('terriajs-cesium/Source/Core/Rectangle');
 var when = require('terriajs-cesium/Source/ThirdParty/when');
 
-var Terria = require('../../lib/Models/Terria');
 var SensorObservationServiceCatalogItem = require('../../lib/Models/SensorObservationServiceCatalogItem');
+var Terria = require('../../lib/Models/Terria');
+var VarType = require('../../lib/Map/VarType');
 
 describe('SensorObservationServiceCatalogItem', function() {
     var terria;
@@ -70,14 +71,18 @@ describe('SensorObservationServiceCatalogItem', function() {
     });
 
     describe('loading', function() {
-        var getFeatureOfInterestResponse;
+        var getFeatureOfInterestResponse, getObservationResponse;
         beforeEach(function(done) {
             when.all([
-                loadText('test/sos/GetFeatureOfInterestResponse.xml').then(function(text) { getFeatureOfInterestResponse = text; })
+                loadText('test/sos/GetFeatureOfInterestResponse.xml').then(function(text) { getFeatureOfInterestResponse = text; }),
+                loadText('test/sos/GetObservationResponse.xml').then(function(text) { getObservationResponse = text; })
             ]).then(function() {
                 jasmine.Ajax.install();
                 jasmine.Ajax.stubRequest(/.*/).andError(); // Fail all requests by default.
-                jasmine.Ajax.stubRequest('http://sos.example.com').andReturn({ responseText: getFeatureOfInterestResponse });
+                jasmine.Ajax.stubRequest('http://sos.example.com', /.*\<sos:GetFeatureOfInterest.*/)
+                    .andReturn({ responseText: getFeatureOfInterestResponse });
+                jasmine.Ajax.stubRequest('http://sos.example.com', /.*\<sos:GetObservation.*/)
+                    .andReturn({ responseText: getObservationResponse });
             }).then(done).otherwise(done.fail);
         });
 
@@ -98,9 +103,41 @@ describe('SensorObservationServiceCatalogItem', function() {
                 expect(item.regionMapping).not.toBeDefined();
                 // Expect it to have created the right table of data (with no time dimension).
                 var columnNames = item.tableStructure.getColumnNames();
-                expect(columnNames).toEqual(["type", "name", "id", "lat", "lon", "identifier", "Annual average"]);
+                expect(columnNames).toEqual(['type', 'name', 'id', 'lat', 'lon', 'identifier', 'Annual average']);
                 // Test a "slice" of the column's values, to remove knockout stuff.
                 expect(item.tableStructure.columns[6].values[0].indexOf('<chart')).toBeGreaterThan(-1);
+                // Expect it not to show any concepts to the user.
+                expect(item.concepts.length).toEqual(0);
+            }).otherwise(fail).then(done);
+        });
+
+        it('works with tryToLoadObservationData true', function(done) {
+            item.updateFromJson({
+                name: 'Foo',
+                url: 'http://sos.example.com',
+                procedures: [{identifier: "http://sos.example.com/tstypes/Yearly Mean", title: "Annual average"}],
+                observableProperties: [{identifier: "http://sos.example.com/parameters/Storage Level", title: "Storage Level"}],
+                tryToLoadObservationData: true,
+                proceduresName: 'freq',
+                observablePropertiesName: 'thing'
+            });
+            item.load().then(function() {
+                // Expect it to have created the right table of data (with no time dimension).
+                var columnNames = item.tableStructure.getColumnNames();
+                expect(columnNames).toEqual(['date', 'Storage Level Annual average', 'identifier', 'freq', 'thing', 'type', 'name', 'id', 'lat', 'lon']);
+                // Test a "slice" of the column's values, to remove knockout stuff.
+                var values = item.tableStructure.columns.filter(function(column) {return column.id === 'value';})[0].values;
+                var idMapping = item.tableStructure.getIdMapping();
+                function valuesForFeatureIdentifier(identifier) {
+                    return idMapping[identifier].map(function(rowNumber) {
+                        return values[rowNumber];
+                    });
+                }
+                expect(valuesForFeatureIdentifier('http://sos.example.com/stations/1')).toEqual([null, 129.425, null]);
+                expect(valuesForFeatureIdentifier('http://sos.example.com/stations/2')).toEqual([null, null, null]);
+                expect(valuesForFeatureIdentifier('http://sos.example.com/stations/3').length).toEqual(16);
+                // Expect a time dimension
+                expect(item.tableStructure.columnsByType[VarType.TIME].length).toEqual(1);
                 // Expect it not to show any concepts to the user.
                 expect(item.concepts.length).toEqual(0);
             }).otherwise(fail).then(done);
