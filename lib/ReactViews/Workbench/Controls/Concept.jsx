9d99ffd532d500f2703423d1e6df040832ff119d
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2016-11-28 16:50:30 +1100

    don't allow clicks on isLoading concepts

--- lib/ReactViews/Workbench/Controls/Concept.jsx
+++ lib/ReactViews/Workbench/Controls/Concept.jsx
@@ -20,7 +20,9 @@ const Concept = React.createClass({
     },
 
     toggleActive() {
-        this.props.concept.toggleActive();
+        if (!this.props.isLoading) {
+            this.props.concept.toggleActive();
+        }
     },
 
     getColorStyle() {
