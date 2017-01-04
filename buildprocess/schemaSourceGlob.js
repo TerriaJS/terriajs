8c2d7576d662083312c31f2ce93c4d2ae2ff2c6a
Kevin Ring <kevin@kotachrome.com>
2017-01-04 11:02:49 +1100

    Resolve path to terriajs correctly.

--- buildprocess/schemaSourceGlob.js
+++ buildprocess/schemaSourceGlob.js
@@ -1,8 +1,10 @@
+var path = require('path');
+
 module.exports = [
-    './lib/Models/*CatalogItem.js',
-    './lib/Models/*CatalogGroup.js',
-    './lib/Models/*CatalogMember.js',
-    './lib/Models/*CatalogFunction.js',
-    '!./lib/Models/addUserCatalogMember.js',
-    '!./lib/Models/AsyncFunctionResultCatalogItem.js'
+    path.resolve(__dirname, '../lib/Models/*CatalogItem.js'),
+    path.resolve(__dirname, '../lib/Models/*CatalogGroup.js'),
+    path.resolve(__dirname, '../lib/Models/*CatalogMember.js'),
+    path.resolve(__dirname, '../lib/Models/*CatalogFunction.js'),
+    '!' + path.resolve(__dirname, '../lib/Models/addUserCatalogMember.js'),
+    '!' + path.resolve(__dirname, '../lib/Models/AsyncFunctionResultCatalogItem.js')
 ];
