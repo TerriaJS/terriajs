5cde958dd4dd7920e36abc9aab79f281d4ef7506
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2017-01-18 09:29:35 +1100

    make help screens available for instances to create

---
+++ lib/ReactViews/HelpScreens/HelpPanel.jsx
@@ -0,0 +1,151 @@
+'use strict';
+
+import React from 'react';
+import ObserveModelMixin from '../ObserveModelMixin';
+import defined from 'terriajs-cesium/Source/Core/defined';
+import classNames from 'classnames';
+import MenuPanel from '../StandardUserInterface/customizable/MenuPanel.jsx';
+
+import Styles from './help-panel.scss';
+import DropdownStyles from '../Map/Panels/panel.scss';
+import helpIcon from '../../../wwwroot/images/icons/help.svg';
+
+const HelpPanel = React.createClass({
+    mixins: [ObserveModelMixin],
+
+    propTypes: {
+        viewState: React.PropTypes.object.isRequired,
+        helpSequences: React.PropTypes.object
+    },
+
+    componentWillMount() {
+        const that = this;
+        setInterval(function() {
+            if (!defined(that.state.screens)) {
+                return;
+            }
+            if (that.props.helpSequences.cancel) {
+                // Has been cancelled from somewhere else. Abort!
+                that.cancel();
+            }
+            const i = that.state.index;
+            const currentScreen = that.state.screens[i];
+            if (defined(that.state.previousRectangle) && that.state.previousRectangle === that.state.currentRectangle) {
+                // Rectangle hasn't changed so we don't want to redraw everything, but update rectangle in case it has changed now.
+                updateCurrentRectangle(that, currentScreen);
+                return;
+            }
+
+            // If we get here, we have a new rectangle.
+            if (typeof currentScreen.preDisplayHook === 'function') {
+                currentScreen.preDisplayHook(that.props.viewState);
+            }
+            updateCurrentRectangle(that, currentScreen);
+
+            if (defined(that.state.currentRectangle)) {
+                currentScreen.rectangle = that.state.currentRectangle;
+                currentScreen.currentScreenNumber = i+1;
+                currentScreen.totalNumberOfScreens = that.state.screens.length;
+                currentScreen.onNext = function() {
+                    if (typeof currentScreen.postDisplayHook === 'function') {
+                        currentScreen.postDisplayHook(that.props.viewState);
+                    }
+                    if ((i+1) >= that.state.screens.length) {
+                        that.cancel();
+                    } else {
+                        that.help(that.state.screens, i+1);
+                    }
+                };
+                that.props.helpSequences.currentScreen = currentScreen;
+                // Processed current rectangle, set as previous.
+                that.setState({previousRectangle: that.state.currentRectangle});
+            }
+        }, 10);
+    },
+
+    getInitialState() {
+        return {
+            isOpen: false,
+            screens: undefined,
+            index: undefined,
+            currentRectangle: undefined,
+            previousRectangle: undefined
+        };
+    },
+
+    onOpenChanged(open) {
+        this.setState({
+            isOpen: open
+        });
+    },
+
+    cancel() {
+        this.props.helpSequences.currentScreen = undefined;
+        this.setState({
+            screens: undefined,
+            index: undefined,
+            currentRectangle: undefined,
+            previousRectangle: undefined
+        });
+    },
+
+    help(screens, i) {
+        this.props.helpSequences.cancel = false;
+        this.setState({
+            screens: screens,
+            index: i,
+            currentRectangle: undefined,
+            previousRectangle: undefined
+        });
+
+    },
+
+    render() {
+        const dropdownTheme = {
+            btn: Styles.btnShare,
+            outer: Styles.sharePanel,
+            inner: Styles.dropdownInner,
+            icon: helpIcon
+        };
+
+        return (
+            <MenuPanel theme={dropdownTheme}
+                       btnText="Help"
+                       viewState={this.props.viewState}
+                       btnTitle="get help"
+                       onOpenChanged={this.onOpenChanged}
+                       forceClosed={defined(this.props.helpSequences.currentScreen)}
+                       smallScreen={this.props.viewState.useSmallScreenInterface}>
+                <If condition={this.state.isOpen}>
+                    <div className={classNames(Styles.viewer, DropdownStyles.section)}>
+                        <label className={DropdownStyles.heading}>{this.props.helpSequences.menuTitle}</label>
+                        <ul className={Styles.viewerSelector}>
+                            <For each="sequence" index="i" of={this.props.helpSequences.sequences}>
+                                <li key={i} className={Styles.listItem}>
+                                    <button onClick={this.help.bind(this, {sequence}.sequence.screens, 0)}
+                                            className={Styles.btnViewer}>
+                                        {sequence.title}
+                                    </button>
+                                </li>
+                            </For>
+                        </ul>
+                    </div>
+                </If>
+            </MenuPanel>
+        );
+    }
+});
+
+/**
+* Reset currentRectangle to the bounding rectangle of the highlighted element.
+* @private
+*/
+function updateCurrentRectangle(component, currentScreen) {
+    const highlightedElement = document.getElementsByClassName(currentScreen.highlightedComponentId);
+    if (defined(highlightedElement[0])) {
+        const screenRect = highlightedElement[0].getBoundingClientRect();
+        component.setState({currentRectangle: screenRect});
+    }
+}
+
+export default HelpPanel;
---
+++ lib/ReactViews/HelpScreens/HelpScreenWindow.jsx
@@ -0,0 +1,98 @@
+'use strict';
+
+import ObserverModelMixin from '../ObserveModelMixin';
+import React from 'react';
+import parseCustomHtmlToReact from '../Custom/parseCustomHtmlToReact';
+import Styles from './help-screen-window.scss';
+import classNames from 'classnames';
+import defined from 'terriajs-cesium/Source/Core/defined';
+import HelpSequences from '../../Models/HelpSequences';
+
+const HelpScreenWindow = React.createClass({
+    mixins: [ObserverModelMixin],
+
+    propTypes: {
+        helpSequences: React.PropTypes.object
+    },
+
+    render() {
+        const currentScreen = this.props.helpSequences.currentScreen;
+
+        const windowClass = classNames(Styles.window, {
+            [Styles.isActive]: currentScreen
+        });
+        const buttonText = currentScreen && currentScreen.totalNumberOfScreens === currentScreen.currentScreenNumber ? 'DONE' : 'NEXT';
+        const positionLeft = currentScreen && calculateLeftPosition(currentScreen);
+        const positionTop = currentScreen && calculateTopPosition(currentScreen);
+
+        const caretTop = currentScreen && currentScreen.caretTop;
+        const caretLeft = currentScreen && currentScreen.caretLeft;
+
+        const width = currentScreen && currentScreen.width;
+
+        return (
+            <div style={{left: positionLeft + 'px', top: positionTop + 'px', width: width + 'px'}} className={windowClass} aria-hidden={ !currentScreen }>
+              <span style={{left: caretLeft + 'px', top: caretTop + 'px'}} className={Styles.caret}/>
+              <div className={Styles.content}>
+                  {currentScreen && parseCustomHtmlToReact(currentScreen.message())}
+              </div>
+              <div className={Styles.screenCount}>
+                  <strong>{currentScreen && currentScreen.currentScreenNumber + '/' + currentScreen.totalNumberOfScreens}</strong>
+              </div>
+              <div className={Styles.nextButton}>
+                  <button type='button' onClick={currentScreen && currentScreen.onNext}
+                      className={Styles.btn}><strong>{buttonText}</strong></button>
+              </div>
+            </div>);
+    }
+});
+
+/**
+ * Work out the screen pixel value for left positioning based on helpScreen parameters.
+ * @private
+ */
+function calculateLeftPosition(helpScreen) {
+    if (!defined(helpScreen)) {
+        return;
+    }
+    const screenRect = helpScreen.rectangle;
+    let leftPosition = 0;
+    if (helpScreen.positionLeft === HelpSequences.RelativePosition.RECT_LEFT) {
+        leftPosition = screenRect.left;
+    } else if (helpScreen.positionLeft === HelpSequences.RelativePosition.RECT_RIGHT) {
+        leftPosition = screenRect.right;
+    } else if (helpScreen.positionLeft === HelpSequences.RelativePosition.RECT_TOP) {
+        leftPosition = screenRect.top;
+    } else if (helpScreen.positionLeft === HelpSequences.RelativePosition.RECT_BOTTOM) {
+        leftPosition = screenRect.bottom;
+    }
+
+    leftPosition += helpScreen.offsetLeft;
+    return leftPosition;
+}
+
+/**
+ * Work out the screen pixel value for top positioning based on helpScreen parameters.
+ * @private
+ */
+function calculateTopPosition(helpScreen) {
+    if (!defined(helpScreen)) {
+        return;
+    }
+    const screenRect = helpScreen.rectangle;
+    let topPosition = 0;
+    if (helpScreen.positionTop === HelpSequences.RelativePosition.RECT_LEFT) {
+        topPosition = screenRect.left;
+    } else if (helpScreen.positionTop === HelpSequences.RelativePosition.RECT_RIGHT) {
+        topPosition = screenRect.right;
+    } else if (helpScreen.positionTop === HelpSequences.RelativePosition.RECT_TOP) {
+        topPosition = screenRect.top;
+    } else if (helpScreen.positionTop === HelpSequences.RelativePosition.RECT_BOTTOM) {
+        topPosition = screenRect.bottom;
+    }
+
+    topPosition += helpScreen.offsetTop;
+    return topPosition;
+}
+
+module.exports = HelpScreenWindow;
