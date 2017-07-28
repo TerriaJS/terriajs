a6547e701c41278f41aa3de8017284bcf8c798f0
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2017-07-28 17:03:03 +1000

    start to add a datetime picker

--- lib/ReactViews/Workbench/WorkbenchItem.jsx
+++ lib/ReactViews/Workbench/WorkbenchItem.jsx
@@ -8,9 +8,10 @@ import {sortable} from 'react-anything-sortable';
 
 import defined from 'terriajs-cesium/Source/Core/defined';
 
-import DisplayAsPercentSection from './Controls/DisplayAsPercentSection';
 import ConceptViewer from './Controls/ConceptViewer';
+import DateTimeSelectorSection from './Controls/DateTimeSelectorSection';
 import DimensionSelectorSection from './Controls/DimensionSelectorSection';
+import DisplayAsPercentSection from './Controls/DisplayAsPercentSection';
 import getAncestors from '../../Models/getAncestors';
 import Legend from './Controls/Legend';
 import ObserveModelMixin from './../ObserveModelMixin';
@@ -100,6 +101,7 @@ const WorkbenchItem = createReactClass({
                             <ConceptViewer item={workbenchItem}/>
                         </If>
                         <DimensionSelectorSection item={workbenchItem}/>
+                        <DateTimeSelectorSection item={workbenchItem}/>
                         <StyleSelectorSection item={workbenchItem}/>
                         <ColorScaleRangeSection item={workbenchItem}/>
                         <DisplayAsPercentSection item={workbenchItem}/>
