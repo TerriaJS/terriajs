37adebcf8f5788fcd3380be9838c6bf0a825037a
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2017-08-03 16:13:30 +1000

    left/right works in leaflet; start to add toggle-splitter-tool

---
+++ lib/ReactViews/Map/Navigation/ToggleSplitterTool.jsx
@@ -0,0 +1,39 @@
+'use strict';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import defined from 'terriajs-cesium/Source/Core/defined';
+
+import Icon from "../../Icon.jsx";
+import ObserveModelMixin from '../../ObserveModelMixin';
+import Styles from './toggle_splitter_tool.scss';
+
+
+const ToggleSplitterTool = createReactClass({
+    displayName: 'ToggleSplitterTool',
+    mixins: [ObserveModelMixin],
+
+    propTypes: {
+        terria: PropTypes.object
+    },
+
+    handleClick() {
+        this.props.terria.currentViewer.toggleSplitter();
+    },
+
+    render() {
+        if (!defined(this.props.terria.currentViewer.toggleSplitter)) {
+            return null;
+        }
+        return <div className={Styles.toggle_splitter_tool}>
+                  <button type='button' className={Styles.btn}
+                          title='show/hide splitter'
+                          onClick={this.handleClick}>
+                          <Icon glyph={this.props.terria.currentViewer.showSplitter ? Icon.GLYPHS.checkboxOff : Icon.GLYPHS.checkboxOn}/>
+                  </button>
+               </div>;
+    },
+});
+
+export default ToggleSplitterTool;
