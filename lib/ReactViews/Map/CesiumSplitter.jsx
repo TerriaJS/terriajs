cae302fff2bee31b62b4ec8ff740baf83e6c6e29
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2017-08-07 12:12:38 +1000

    fix slight spill-over effect around cesium splitter

--- lib/ReactViews/Map/CesiumSplitter.jsx
+++ lib/ReactViews/Map/CesiumSplitter.jsx
@@ -8,6 +8,11 @@ import ObserveModelMixin from '../ObserveModelMixin';
 import TerriaViewer from '../../ViewModels/TerriaViewer';
 import ViewerMode from '../../Models/ViewerMode';
 
+// When the range slider has value 0, the left edge of the thumb is at the left edge of the screen,
+// However, the divider is at the _center_ of the thumb, which is thumbWidth/2 pixels (say 20px).
+// Thus the imagerySplitPosition is not 0, but 20 / canvasWidth.
+// On the right edge, imagerySplitPosition is not 1, but (canvasWidth - 20) / canvasWidth.
+
 const CesiumSplitter = createReactClass({
     displayName: 'CesiumSplitter',
     mixins: [ObserveModelMixin],
@@ -22,12 +27,18 @@ const CesiumSplitter = createReactClass({
     },
 
     onSliderMove(event) {
+        const scene = this.props.terria.currentViewer.scene;
+        const thumbWidth = this.props.terria.currentViewer.splitterThumbWidth;
+        const canvasWidth = scene.canvas.clientWidth;
         this.setState({value: event.target.value});
-        this.props.terria.currentViewer.scene.imagerySplitPosition = event.target.value;
+        this.props.terria.currentViewer.scene.imagerySplitPosition = (event.target.value * (canvasWidth - thumbWidth) + thumbWidth / 2) / canvasWidth;
     },
 
     render() {
         const terria = this.props.terria;
+        if (!terria.showSplitter) {
+            return null;
+        }
         if (terria.viewerMode === ViewerMode.CesiumTerrain || terria.viewerMode === ViewerMode.CesiumEllipsoid) {
             return (
                 <div className="cesiumSplitter">
