c6bb1bba48c452dc8d82381336eeb6257d860ea9
Kevin Ring <kevin@kotachrome.com>
2017-08-11 10:55:57 +1000

    Use a div instead of range slider.

--- lib/ReactViews/Map/CesiumSplitter.jsx
+++ lib/ReactViews/Map/CesiumSplitter.jsx
@@ -6,41 +6,118 @@ import Styles from './cesium-splitter.scss';
 
 import ObserveModelMixin from '../ObserveModelMixin';
 
-// When the range slider has value 0, the left edge of the thumb is at the left edge of the screen,
-// However, the divider is at the _center_ of the thumb, which is thumbWidth/2 pixels (say 20px).
-// Thus the imagerySplitPosition is not 0, but 20 / canvasWidth.
-// On the right edge, imagerySplitPosition is not 1, but (canvasWidth - 20) / canvasWidth.
-
 const CesiumSplitter = createReactClass({
     displayName: 'CesiumSplitter',
     mixins: [ObserveModelMixin],
 
     propTypes: {
-        terria: PropTypes.object.isRequired
+        terria: PropTypes.object.isRequired,
+        thumbSize: PropTypes.number,
+        padding: PropTypes.number
     },
 
-    getInitialState: function() {
+    getDefaultProps() {
+        return {
+            thumbSize: 42,
+            padding: 0
+        };
+    },
+
+    getInitialState() {
         const scene = this.props.terria.currentViewer.scene;
-        return {value: scene ? scene.imagerySplitPosition : 0.5};
+        return {
+            value: scene ? scene.imagerySplitPosition : 0.5
+        };
     },
 
-    onSliderMove(event) {
+    componentWillUnmount() {
+        this.unsubscribe();
+    },
+
+    startDrag(event) {
+        // While dragging is in progress, subscribe to document-level movement and up events.
+        document.addEventListener('mousemove', this.drag, false);
+        document.addEventListener('mouseup', this.stopDrag, false);
+        event.preventDefault();
+    },
+
+    drag(event) {
         const terria = this.props.terria;
         const viewer = terria.currentViewer;
-        const thumbWidth = viewer.splitterThumbWidth;
-        const canvasWidth = viewer.scene.canvas.clientWidth;
-        this.setState({value: event.target.value});
-        viewer.scene.imagerySplitPosition = (event.target.value * (canvasWidth - thumbWidth) + thumbWidth / 2) / canvasWidth;
+        if (!viewer || !viewer.scene) {
+            return;
+        }
+
+        const canvas = viewer.scene.canvas;
+        if (!canvas) {
+            return;
+        }
+
+        const mapRect = canvas.getBoundingClientRect();
+
+        const width = mapRect.right - mapRect.left;
+        const fraction = (event.clientX - mapRect.left) / width;
+
+        const min = mapRect.left + this.props.padding + (this.props.thumbSize * 0.5);
+        const max = mapRect.right - this.props.padding - (this.props.thumbSize * 0.5);
+        const minFraction = (min - mapRect.left) / width;
+        const maxFraction = (max - mapRect.left) / width;
+
+        const splitFraction = Math.min(maxFraction, Math.max(minFraction, fraction));
+
+        this.setState({
+            value: splitFraction
+        });
+
+        viewer.scene.imagerySplitPosition = splitFraction;
         viewer.notifyRepaintRequired();
     },
 
+    stopDrag(event) {
+        this.unsubscribe();
+    },
+
+    unsubscribe() {
+        document.removeEventListener('mousemove', this.drag, false);
+        document.removeEventListener('mouseup', this.stopDrag, false);
+    },
+
+    getPosition() {
+        const terria = this.props.terria;
+        const viewer = terria.currentViewer;
+        if (!viewer || !viewer.scene) {
+            return 50;
+        }
+
+        const canvas = viewer.scene.canvas;
+        if (!canvas) {
+            return 50;
+        }
+
+        const canvasWidth = canvas.clientWidth;
+        return this.state.value * canvasWidth;
+    },
+
     render() {
+        const thumbWidth = this.props.thumbSize;
+
+        const style = {
+            left: this.getPosition() + 'px',
+            width: thumbWidth + 'px',
+            height: thumbWidth + 'px',
+            marginLeft: '-' + (thumbWidth * 0.5) + 'px',
+            marginTop: '-' + (thumbWidth * 0.5) + 'px',
+            lineHeight: (thumbWidth - 2) + 'px',
+            borderRadius: (thumbWidth * 0.5) + 'px',
+            fontSize: (thumbWidth - 12) + 'px'
+        };
+
         return (
             <div className="cesiumSplitter">
                 <div className={Styles.dividerWrapper}>
-                    <div className="leaflet-sbs-divider" style={{left: this.state.value * 100 + "%"}}></div>
+                    <div className="leaflet-sbs-divider" style={{left: this.getPosition() + 'px'}}></div>
                 </div>
-                <input className="leaflet-sbs-range" type="range" min="0" max="1" step="any" value={this.state.value} onChange={this.onSliderMove}/>
+                <div className="leaflet-sbs-range" style={style} onMouseDown={this.startDrag}>&#x2980;</div>
             </div>
         );
     }
