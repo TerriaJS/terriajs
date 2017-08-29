aaadb39e52e0b12f1b3fc2547121f41ccf32b580
Street, Arthur (Data61, Eveleigh ATP) <Arthur.Street@data61.csiro.au>
2017-08-29 15:51:34 +1000

    shift objectivifyDates into DateTimePicker

--- lib/ReactViews/BottomDock/Timeline/CatalogItemDateTimePicker.jsx
+++ lib/ReactViews/BottomDock/Timeline/CatalogItemDateTimePicker.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
+import React from 'react';
+
 import ObserveModelMixin from '../../ObserveModelMixin';
 import DateTimePicker from './DateTimePicker';
-import uniq from 'lodash.uniq';
 
 const CatalogItemDateTimePicker = createReactClass({
     displayName: 'CatalogItemDateTimePicker',
@@ -17,60 +17,8 @@ const CatalogItemDateTimePicker = createReactClass({
     render() {
       const availableDates = this.props.item.getAvailableDates();
       const currentDate = availableDates[this.props.item.intervals.indexOf(this.props.item.clockForDisplay.currentTime)];
-      return <DateTimePicker name={this.props.item.name} currentDate={currentDate} dates={objectifyDates(availableDates)} onChange={this.props.onChange}/>;
+      return <DateTimePicker name={this.props.item.name} currentDate={currentDate} dates={availableDates} onChange={this.props.onChange}/>;
     }
 });
-
-function getOneYear(year, dates) {
-  // al data from a given year
-  return dates.filter(d => d.getUTCFullYear() === year);
-}
-
-function getOneMonth(yearData, monthIndex) {
-  // all data from certain month of that year
-  return yearData.filter(y => y.getUTCMonth() === monthIndex);
-}
-
-function getOneDay(monthData, dayIndex) {
-  return monthData.filter(m => m.getUTCDate() === dayIndex);
-}
-
-function getMonthForYear(yearData) {
-  // get available months for a given year
-  return uniq(yearData.map(d => d.getUTCMonth()));
-}
-
-function getDaysForMonth(monthData) {
-  // get all available days given a month in a year
-  // start from 1, so we need to change to 0 based
-  return uniq(monthData.map(m => m.getUTCDate()));
-}
-
-/**
- * Process an array of dates into layered objects of years, months and days.
- * @param  {Date[]} An array of dates.
- * @return {Object} Returns an object whose keys are years, whose values are objects whose keys are months (0=Jan),
- *   whose values are objects whose keys are days, whose values are arrays of all the datetimes on that day.
- */
-function objectifyDates(dates) {
-  const years = uniq(dates.map(d => d.getUTCFullYear()));
-  const result = {};
-
-  years.forEach(y => {
-    const yearData = getOneYear(y, dates);
-    const monthInYear = {};
-    getMonthForYear(yearData).forEach(monthIndex => {
-      const monthData = getOneMonth(yearData, monthIndex);
-      const daysInMonth = {};
-
-      getDaysForMonth(monthData).forEach(dayIndex => {
-        daysInMonth[dayIndex] = getOneDay(monthData, dayIndex);
-      });
-      monthInYear[monthIndex] = daysInMonth;
-    });
-    result[y] = monthInYear;
-  });
-  return result;
-}
 
 module.exports = CatalogItemDateTimePicker;
--- lib/ReactViews/BottomDock/Timeline/DateTimePicker.jsx
+++ lib/ReactViews/BottomDock/Timeline/DateTimePicker.jsx
@@ -4,11 +4,14 @@ import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import uniq from 'lodash.uniq';
+
 import defined from 'terriajs-cesium/Source/Core/defined';
+
+import {formatDateTime} from './DateFormats';
+import Icon from '../../Icon.jsx';
 import ObserveModelMixin from '../../ObserveModelMixin';
 import Styles from './timeline.scss';
-import Icon from '../../Icon.jsx';
-import {formatDateTime} from './DateFormats';
 
 function daysInMonth(month,year) {
   const n = new Date(year, month, 0).getDate();
@@ -23,7 +26,7 @@ const DateTimePicker = createReactClass({
 
     propTypes: {
         name: PropTypes.string,
-        dates: PropTypes.object,
+        dates: PropTypes.array,
         currentDate: PropTypes.object,
         onChange: PropTypes.func
     },
@@ -59,45 +62,45 @@ const DateTimePicker = createReactClass({
       this.setState({isOpen: false});
     },
 
-    renderYearGrid(dates) {
-      const years = Object.keys(dates);
+    renderYearGrid(datesObject) {
+      const years = Object.keys(datesObject);
       const monthOfYear = Array.apply(null, {length: 12}).map(Number.call, Number);
       return (
         <div className={Styles.grid}>
           <div className={Styles.gridHeading}>Select a year</div>
           <div className={Styles.gridBody}>{years.map(y => <div className={Styles.gridRow} key={y} onClick={() => this.setState({year: y, month: null, day: null, time: null})}>
             <span className={Styles.gridLabel}>{y}</span>
-            <span className={Styles.gridRowInner12}>{monthOfYear.map(m => <span className={dates[y][m] ? Styles.activeGrid : ''} key={m} ></span>)}</span></div>)}
+            <span className={Styles.gridRowInner12}>{monthOfYear.map(m => <span className={datesObject[y][m] ? Styles.activeGrid : ''} key={m} ></span>)}</span></div>)}
           </div>
         </div>
       );
     },
 
-    renderMonthGrid(dates) {
+    renderMonthGrid(datesObject) {
       const year = this.state.year;
       return (
         <div className={Styles.grid}>
           <div className={Styles.gridHeading}>
             <button className={Styles.backbtn} onClick={()=>{this.setState({year: null, month: null, day: null, time: null});}}>{this.state.year}</button>
           </div>
-          <div className={Styles.gridBody}>{monthNames.map((m, i) => <div className={classNames(Styles.gridRow, {[Styles.inactiveGridRow]: !defined(dates[year][i])})} key={m} onClick={() => defined(dates[year][i]) && this.setState({month: i, day: null, time: null})}>
+          <div className={Styles.gridBody}>{monthNames.map((m, i) => <div className={classNames(Styles.gridRow, {[Styles.inactiveGridRow]: !defined(datesObject[year][i])})} key={m} onClick={() => defined(datesObject[year][i]) && this.setState({month: i, day: null, time: null})}>
             <span className={Styles.gridLabel}>{m}</span>
-            <span className={Styles.gridRowInner31}>{daysInMonth(i + 1, year).map(d => <span className={ defined(dates[year][i]) && defined(dates[year][i][d + 1]) ? Styles.activeGrid : ''} key={d} ></span>)}</span></div>)}
+            <span className={Styles.gridRowInner31}>{daysInMonth(i + 1, year).map(d => <span className={ defined(datesObject[year][i]) && defined(datesObject[year][i][d + 1]) ? Styles.activeGrid : ''} key={d} ></span>)}</span></div>)}
           </div>
         </div>
       );
     },
 
-    renderDayView(dates) {
+    renderDayView(datesObject) {
       // Create one date object per day, using an arbitrary time. This does it via Object.keys and moment().
-      const days = Object.keys(dates[this.state.year][this.state.month]);
+      const days = Object.keys(datesObject[this.state.year][this.state.month]);
       const daysToDisplay = days.map(d => moment().date(d).month(this.state.month).year(this.state.year));
       const selected = defined(this.state.day) ? moment().date(this.state.day).month(this.state.month).year(this.state.year) : null;
       // Aside: You might think this implementation is clearer - use the first date available on each day.
       // However it fails because react-datepicker actually requires a moment() object for selected, not a Date object.
-      // const monthObject = this.props.dates[this.state.year][this.state.month];
+      // const monthObject = this.props.datesObject[this.state.year][this.state.month];
       // const daysToDisplay = Object.keys(monthObject).map(dayNumber => monthObject[dayNumber][0]);
-      // const selected = defined(this.state.day) ? this.props.dates[this.state.year][this.state.month][this.state.day][0] : null;
+      // const selected = defined(this.state.day) ? this.props.datesObject[this.state.year][this.state.month][this.state.day][0] : null;
 
       return (
         <div className={Styles.dayPicker}>
@@ -107,7 +110,7 @@ const DateTimePicker = createReactClass({
           </div>
             <DatePicker
                 inline
-                onChange={this.selectDay.bind(this, dates)}
+                onChange={this.selectDay.bind(this, datesObject)}
                 includeDates={daysToDisplay}
                 selected={selected}
             />
@@ -115,14 +118,14 @@ const DateTimePicker = createReactClass({
       );
     },
 
-    selectDay(dates, value) {
-      const selectedTime = dates[this.state.year][this.state.month][value.date()][0];
+    selectDay(datesObject, value) {
+      const selectedTime = datesObject[this.state.year][this.state.month][value.date()][0];
       this.setState({day: value.date(), time: selectedTime});
       this.props.onChange(selectedTime);
     },
 
-    renderHourView(dates) {
-      const timeOptions = dates[this.state.year][this.state.month][this.state.day].map((m) => ({
+    renderHourView(datesObject) {
+      const timeOptions = datesObject[this.state.year][this.state.month][this.state.day].map((m) => ({
         value: m,
         label: formatDateTime(m)
       }));
@@ -173,16 +176,16 @@ const DateTimePicker = createReactClass({
 
     render() {
       if (this.props.dates) {
-        const dates = this.props.dates;
+        const datesObject = objectifyDates(this.props.dates);
         return (
             <div className={Styles.timelineDatePicker} onClick={(event) => { event.stopPropagation(); }}>
               <button className={Styles.togglebutton} onClick={() => { this.toggleDatePicker(); }}><Icon glyph={Icon.GLYPHS.calendar}/></button>
               {this.state.isOpen && <div className={Styles.datePicker}>
               <button className={Styles.backbutton} disabled={!this.state.year} type='button' onClick={() => this.goBack()}><Icon glyph={Icon.GLYPHS.left}/></button>
-                {!defined(this.state.year) && this.renderYearGrid(dates)}
-                {defined(this.state.year) && !defined(this.state.month) && this.renderMonthGrid(dates)}
-                {(defined(this.state.year) && defined(this.state.month)) && this.renderDayView(dates)}
-                {(defined(this.state.year) && defined(this.state.month) && defined(this.state.day)) && this.renderHourView(dates)}
+                {!defined(this.state.year) && this.renderYearGrid(datesObject)}
+                {defined(this.state.year) && !defined(this.state.month) && this.renderMonthGrid(datesObject)}
+                {(defined(this.state.year) && defined(this.state.month)) && this.renderDayView(datesObject)}
+                {(defined(this.state.year) && defined(this.state.month) && defined(this.state.day)) && this.renderHourView(datesObject)}
               </div>}
             </div>
           );
@@ -191,5 +194,57 @@ const DateTimePicker = createReactClass({
       }
     }
 });
+
+function getOneYear(year, dates) {
+  // al data from a given year
+  return dates.filter(d => d.getUTCFullYear() === year);
+}
+
+function getOneMonth(yearData, monthIndex) {
+  // all data from certain month of that year
+  return yearData.filter(y => y.getUTCMonth() === monthIndex);
+}
+
+function getOneDay(monthData, dayIndex) {
+  return monthData.filter(m => m.getUTCDate() === dayIndex);
+}
+
+function getMonthForYear(yearData) {
+  // get available months for a given year
+  return uniq(yearData.map(d => d.getUTCMonth()));
+}
+
+function getDaysForMonth(monthData) {
+  // get all available days given a month in a year
+  // start from 1, so we need to change to 0 based
+  return uniq(monthData.map(m => m.getUTCDate()));
+}
+
+/**
+ * Process an array of dates into layered objects of years, months and days.
+ * @param  {Date[]} An array of dates.
+ * @return {Object} Returns an object whose keys are years, whose values are objects whose keys are months (0=Jan),
+ *   whose values are objects whose keys are days, whose values are arrays of all the datetimes on that day.
+ */
+function objectifyDates(dates) {
+  const years = uniq(dates.map(d => d.getUTCFullYear()));
+  const result = {};
+
+  years.forEach(y => {
+    const yearData = getOneYear(y, dates);
+    const monthInYear = {};
+    getMonthForYear(yearData).forEach(monthIndex => {
+      const monthData = getOneMonth(yearData, monthIndex);
+      const daysInMonth = {};
+
+      getDaysForMonth(monthData).forEach(dayIndex => {
+        daysInMonth[dayIndex] = getOneDay(monthData, dayIndex);
+      });
+      monthInYear[monthIndex] = daysInMonth;
+    });
+    result[y] = monthInYear;
+  });
+  return result;
+}
 
 module.exports = DateTimePicker;
