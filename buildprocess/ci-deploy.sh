bf9cf64acc25d68b86f3bbdd370accf55a2be09c
Kevin Ring <kevin@kotachrome.com>
2018-03-06 21:58:32 +1100

    Docker build and helm

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-#chmod a+x kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod a+x kubectl
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 git clone https://github.com/TerriaJS/TerriaMap.git
 cd TerriaMap
 sed -i -e 's@"terriajs": ".*"@"terriajs": "'$TRAVIS_REPO_SLUG'#'$TRAVIS_BRANCH'"@g' package.json
 npm install
 npm run gulp release
-#npm run docker-build-local
+npm run "--terriajs-map:docker_name=branch-$TRAVIS_BRANCH" docker-build-local
+helm upgrade --install -f deploy/helm/example-prod.yml "branch-$TRAVIS_BRANCH" deploy/helm/terria
