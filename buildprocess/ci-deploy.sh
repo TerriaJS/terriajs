2dd865d4c6f67689e5420cd725eb10fe04a8a56c
Kevin Ring <kevin@kotachrome.com>
2018-03-06 20:34:57 +1100

    Fix sed.

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
+#curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+#chmod a+x kubectl
 git clone https://github.com/TerriaJS/TerriaMap.git
 cd TerriaMap
-sed -i -e 's/"terriajs": ".*"/"terriajs": "$TRAVIS_REPO_SLUG#$TRAVIS_BRANCH"/g' package.json
+sed -i -e 's@"terriajs": ".*"@"terriajs": "'$TRAVIS_REPO_SLUG'#'$TRAVIS_BRANCH'"@g' package.json
 npm install
 npm run gulp release
+#npm run docker-build-local
