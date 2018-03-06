ee6177151a5cb437983460a72d5261a736492f2c
Kevin Ring <kevin@kotachrome.com>
2018-03-06 19:46:39 +1100

    More buildyness.

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/TerriaJS/TerriaMap.git
 cd TerriaMap
+sed -i -e 's/"terriajs": ".*"/"terriajs": "$TRAVIS_REPO_SLUG#$TRAVIS_BRANCH"/g' package.json
+npm install
+npm run gulp release
