314a0d3dfe6ef674efc2121a7d77de07817563f2
Kevin Ring <kevin@kotachrome.com>
2018-03-07 10:27:55 +1100

    Tweak deployment names.

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -2,10 +2,10 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod a+x kubectl
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-git clone https://github.com/TerriaJS/TerriaMap.git
+git clone -b include-release-name https://github.com/TerriaJS/TerriaMap.git
 cd TerriaMap
 sed -i -e 's@"terriajs": ".*"@"terriajs": "'$TRAVIS_REPO_SLUG'#'$TRAVIS_BRANCH'"@g' package.json
 npm install
 npm run gulp release
-npm run "--terriajs-map:docker_name=branch-$TRAVIS_BRANCH" docker-build-local
-helm upgrade --install -f deploy/helm/example-prod.yml "branch-$TRAVIS_BRANCH" deploy/helm/terria
+npm run "--terriajs-map:docker_name=terriajs-$TRAVIS_BRANCH" docker-build-local
+helm upgrade --install -f deploy/helm/example-prod.yml "terriajs-$TRAVIS_BRANCH" deploy/helm/terria
