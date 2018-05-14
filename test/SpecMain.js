de798b3a89cd4308c2438e9e8ec89749e4833d61
Kevin Ring <kevin@kotachrome.com>
2018-05-14 23:25:16 +1000

    Fix test failures after React upgrade.

--- test/SpecMain.js
+++ test/SpecMain.js
@@ -1,6 +1,10 @@
 /*global require*/
 require('terriajs-jasmine-ajax');
+import Enzyme from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // require('babel-polyfill');  // Polyfills Map, Set and other ES6.
 jasmine.getEnv().addReporter({
