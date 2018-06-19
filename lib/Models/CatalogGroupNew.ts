9c0d76e3731f9e058cdd50d5d7114a300216e4eb
Kevin Ring <kevin@kotachrome.com>
2018-06-19 14:18:30 +1000

    Remove unneeded code.

--- lib/Models/CatalogGroupNew.ts
+++ lib/Models/CatalogGroupNew.ts
@@ -4,14 +4,6 @@ import GroupMixin from '../ModelMixins/GroupMixin';
 import Model from './Model';
 import Terria from './TerriaNew';
 
-// interface ModelWithDefinition extends Model.InterfaceFromDefinition<CatalogGroupTraits> { }
-// class ModelWithDefinition extends Model<CatalogGroupTraits> { }
-
-const C = Model(CatalogGroupTraits);
-const x = new C('test', new Terria());
-console.log(x);
-
-// @Model.definition(CatalogGroupTraits)
 export default class CatalogGroup extends GroupMixin(CatalogMemberMixin(Model(CatalogGroupTraits))) {
     get type() {
         return 'group';
