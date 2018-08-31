e0ce0eb53260b857c2e75e5f26eda5a78237ee44
chloe <chloeleichen@gmail.com>
2018-08-31 16:03:11 +1000

    add button

--- lib/ReactViews/SidePanel/SidePanel.jsx
+++ lib/ReactViews/SidePanel/SidePanel.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import createReactClass from 'create-react-class';
+import createReactClass from "create-react-class";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import knockout from 'terriajs-cesium/Source/ThirdParty/knockout';
+import knockout from "terriajs-cesium/Source/ThirdParty/knockout";
 
-import ObserveModelMixin from '../ObserveModelMixin';
-import SearchBox from '../Search/SearchBox.jsx';
-import SidebarSearch from '../Search/SidebarSearch.jsx';
-import Workbench from '../Workbench/Workbench.jsx';
+import ObserveModelMixin from "../ObserveModelMixin";
+import SearchBox from "../Search/SearchBox.jsx";
+import SidebarSearch from "../Search/SidebarSearch.jsx";
+import Workbench from "../Workbench/Workbench.jsx";
 import Icon from "../Icon.jsx";
-import { removeMarker } from '../../Models/LocationMarkerUtils';
+import { removeMarker } from "../../Models/LocationMarkerUtils";
 
-import Styles from './side-panel.scss';
+import Styles from "./side-panel.scss";
 
 const SidePanel = createReactClass({
-    displayName: 'SidePanel',
+    displayName: "SidePanel",
     mixins: [ObserveModelMixin],
 
     propTypes: {
@@ -40,9 +40,11 @@ const SidePanel = createReactClass({
         this.unsubscribeFromProps();
 
         // Close the search results when the Now Viewing changes (so that it's visible).
-        this._nowViewingChangeSubscription = knockout.getObservable(this.props.terria.nowViewing, 'items').subscribe(() => {
-            this.props.viewState.searchState.showLocationSearchResults = false;
-        });
+        this._nowViewingChangeSubscription = knockout
+            .getObservable(this.props.terria.nowViewing, "items")
+            .subscribe(() => {
+                this.props.viewState.searchState.showLocationSearchResults = false;
+            });
     },
 
     unsubscribeFromProps() {
@@ -78,46 +80,84 @@ const SidePanel = createReactClass({
         return (
             <div className={Styles.workBench}>
                 <div className={Styles.header}>
-                    <SearchBox onSearchTextChanged={this.changeSearchText}
-                               onDoSearch={this.search}
-                               onFocus={this.startLocationSearch}
-                               searchText={searchState.locationSearchText}
-                               placeholder="Search for locations" />
+                    <SearchBox
+                        onSearchTextChanged={this.changeSearchText}
+                        onDoSearch={this.search}
+                        onFocus={this.startLocationSearch}
+                        searchText={searchState.locationSearchText}
+                        placeholder="Search for locations"
+                    />
                     <div className={Styles.addData}>
-                        <button type='button' onClick={this.onAddDataClicked} className={Styles.button}>
-                            <Icon glyph={Icon.GLYPHS.add}/>Add data
+                        <button
+                            type="button"
+                            onClick={this.onAddDataClicked}
+                            className={Styles.button}
+                        >
+                            <Icon glyph={Icon.GLYPHS.add} />Add data
+                        </button>
+                        <button
+                            type="button"
+                            onClick={this.onAddDataClicked}
+                            className={Styles.uploadData}
+                        >
+                            <Icon glyph={Icon.GLYPHS.add} />
                         </button>
                     </div>
                 </div>
                 <div className={Styles.body}>
                     <Choose>
-                        <When condition={searchState.locationSearchText.length > 0 && searchState.showLocationSearchResults}>
+                        <When
+                            condition={
+                                searchState.locationSearchText.length > 0 &&
+                                searchState.showLocationSearchResults
+                            }
+                        >
                             <SidebarSearch
                                 terria={this.props.terria}
                                 viewState={this.props.viewState}
-                                isWaitingForSearchToStart={searchState.isWaitingToStartLocationSearch} />
+                                isWaitingForSearchToStart={
+                                    searchState.isWaitingToStartLocationSearch
+                                }
+                            />
                         </When>
                         <When
-                            condition={this.props.terria.nowViewing.items && this.props.terria.nowViewing.items.length > 0}>
-                            <Workbench viewState={this.props.viewState} terria={this.props.terria} />
+                            condition={
+                                this.props.terria.nowViewing.items &&
+                                this.props.terria.nowViewing.items.length > 0
+                            }
+                        >
+                            <Workbench
+                                viewState={this.props.viewState}
+                                terria={this.props.terria}
+                            />
                         </When>
                         <Otherwise>
                             <div className={Styles.workbenchEmpty}>
                                 <div>Your workbench is empty</div>
-                                <p><strong>Click &apos;Add data&apos; above to:</strong></p>
+                                <p>
+                                    <strong>
+                                        Click &apos;Add data&apos; above to:
+                                    </strong>
+                                </p>
                                 <ul>
                                     <li>Browse the Data Catalogue</li>
                                     <li>Load your own data onto the map</li>
                                 </ul>
-                                <p><Icon glyph={Icon.GLYPHS.bulb}/><strong>TIP:</strong> <em>All your active data sets will be listed
-                                    here</em></p>
+                                <p>
+                                    <Icon glyph={Icon.GLYPHS.bulb} />
+                                    <strong>TIP:</strong>{" "}
+                                    <em>
+                                        All your active data sets will be listed
+                                        here
+                                    </em>
+                                </p>
                             </div>
                         </Otherwise>
                     </Choose>
                 </div>
             </div>
         );
-    },
+    }
 });
 
 module.exports = SidePanel;
