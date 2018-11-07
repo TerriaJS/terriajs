dfffde57ce8a302ff17163daba652d62fa690a8b
Steve Bennett <stevage@gmail.com>
2018-11-08 08:53:22 +1100

    Restore concept viewer.

--- lib/ReactViews/Workbench/Controls/ConceptViewer.jsx
+++ lib/ReactViews/Workbench/Controls/ConceptViewer.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import SummaryConcept from './SummaryConcept/SummaryConcept';
+import Concept from './Concept';
 import ObserveModelMixin from '../../ObserveModelMixin';
 import SummaryConceptModel from '../../../Map/SummaryConcept';
 
@@ -20,6 +21,7 @@ const ConceptViewer = createReactClass({
     render() {
         // All non-additive-conditions go in a single section. (If there are none, don't show a <div class=section> so we don't get an extra padding.)
         // Each additive-condition goes in its own section.
+        const nonSummaryConcept = this.props.item.concepts.filter(concept => concept.isVisible && !SummaryConceptModel.prototype.isPrototypeOf(concept));
         return (
             <div className={Styles.root}>
                 <If condition={nonSummaryConcept.length > 0}>
