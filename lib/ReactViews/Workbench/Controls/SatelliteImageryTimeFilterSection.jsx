ea076ba01bbe9aed13e9cbaf64852d46b2b72a16
Kevin Ring <kevin@kotachrome.com>
2019-02-13 18:01:14 +1100

    Manually pick points from the workbench.

--- lib/ReactViews/Workbench/Controls/SatelliteImageryTimeFilterSection.jsx
+++ lib/ReactViews/Workbench/Controls/SatelliteImageryTimeFilterSection.jsx
@@ -1,9 +1,12 @@
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import React from 'react';
+import knockout from 'terriajs-cesium/Source/ThirdParty/knockout';
+import MapInteractionMode from '../../../Models/MapInteractionMode';
 import LocationItem from '../../LocationItem.jsx';
 import ObserveModelMixin from '../../ObserveModelMixin';
 import Styles from './satellite-imagery-time-filter-section.scss';
+import Loader from '../../Loader';
 
 const SatelliteImageryTimeFilterSection = createReactClass({
     displayName: 'SatelliteImageryTimeFilterSection',
@@ -22,11 +25,42 @@ const SatelliteImageryTimeFilterSection = createReactClass({
     },
 
     newLocation() {
-        alert('TODO');
+        // Cancel any feature picking already in progress.
+        const terria = this.props.item.terria;
+        terria.pickedFeatures = undefined;
+
+        const pickPointMode = new MapInteractionMode({
+            message: 'Select a point by clicking on the map.',
+            onCancel: () => {
+                terria.mapInteractionModeStack.pop();
+            }
+        });
+        terria.mapInteractionModeStack.push(pickPointMode);
+
+        knockout.getObservable(pickPointMode, 'pickedFeatures').subscribe(pickedFeatures => {
+            pickPointMode.customUi = function() {
+                return (
+                    <Loader message="Querying position..." />
+                );
+            };
+
+            pickedFeatures.allFeaturesAvailablePromise.then(() => {
+                const item = this.props.item;
+                const thisLayerFeature = pickedFeatures.features.filter(feature => {
+                    return feature.imageryLayer === item.imageryLayer && item.canFilterIntervalsByFeature(feature);
+                })[0];
+
+                if (thisLayerFeature !== undefined) {
+                    item.filterIntervalsByFeature(thisLayerFeature);
+                }
+
+                terria.mapInteractionModeStack.pop();
+            });
+        });
     },
 
     render() {
-        if (this.props.item.featureTimesProperty === undefined) {
+        if (this.props.item.canFilterIntervalsByFeature === undefined) {
             return null;
         }
 
