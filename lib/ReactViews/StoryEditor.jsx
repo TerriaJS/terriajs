616926842b4b42671d1a59d62309556b310ab73a
chloe <chloeleichen@gmail.com>
2019-03-11 14:52:27 +1100

    medium editor

--- lib/ReactViews/StoryEditor.jsx
+++ lib/ReactViews/StoryEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from "./Icon.jsx";
-import Editor from 'react-medium-editor';
+import { Editor } from 'react-draft-wysiwyg';
 import Styles from './story-editor.scss';
 
 export default class StoryEditor extends React.Component {
