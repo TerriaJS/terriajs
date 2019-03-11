61ddf74420a11c845f757bd77c6760647290910a
chloe <chloeleichen@gmail.com>
2019-03-11 16:22:28 +1100

    remove debug share code

--- lib/ReactViews/StoryBuilder.jsx
+++ lib/ReactViews/StoryBuilder.jsx
@@ -74,7 +74,9 @@ const StoryBuilder = createReactClass({
 
     render() {
         const hasStories = defined(this.props.terria.stories) && this.props.terria.stories.length > 0;
-        const shareUrlTextBox = <input type="text" value={ new URI(canShorten(this.props.terria) ? buildShortShareLink(this.props.terria, hasStories) : buildShareLink(this.props.terria,hasStories))} readOnly id='share-story' />;
+        const shareLink = 
+ canShorten(this.props.terria) ? buildShortShareLink(this.props.terria, hasStories) : buildShareLink(this.props.terria,hasStories);
+        const shareUrlTextBox = <input type="text" value={new URI("www.facebook.com")} readOnly id='share-story' />;
         return (
             <div className={Styles.storyPanel}>
                 <div className={Styles.header}>
