47c051e3de1f01b62b9a17f9b6ee19f55de352a6
Stephen Davies <steve9164@gmail.com>
2019-03-22 20:19:11 +1100

    Add async WMS loading test

--- test/Models/WebMapServiceCatalogItemSpec.ts
+++ test/Models/WebMapServiceCatalogItemSpec.ts
@@ -1,5 +1,5 @@
 import WebMapServiceCatalogItem from '../../lib/Models/WebMapServiceCatalogItem';
-import { autorun } from 'mobx';
+import { autorun, runInAction, observable } from 'mobx';
 import Terria from '../../lib/Models/Terria';
 
 describe('WebMapServiceCatalogItem', function() {
@@ -19,8 +19,43 @@ describe('WebMapServiceCatalogItem', function() {
         const definition = wms.getOrCreateStratum('definition');
         definition.url = 'https://programs.communications.gov.au/geoserver/ows';
         definition.layers = 'mobile-black-spot-programme:funded-base-stations-group';
+        wms.loadData();
         autorun(() => {
             console.log(wms.availableStyles);
+        });
+    });
+
+    it('updates description from a GetCapabilities', function(done) {
+        let wms: WebMapServiceCatalogItem;
+        const terria = new Terria();
+        wms = new WebMapServiceCatalogItem('test', terria);
+        runInAction(() => {
+            const definition = wms.getOrCreateStratum('definition');
+            definition.url = 'https://programs.communications.gov.au/geoserver/ows';
+            definition.layers = 'mobile-black-spot-programme:funded-base-stations-group';
+        });
+        let description: String | undefined;
+        const cleanup = autorun(() => {
+            if (wms.info !== undefined) {
+                const descSection = wms.info.find(section => section.name === 'Data Description');
+                if (descSection !== undefined && descSection.content !== undefined) {
+                    description = descSection.content;
+                }
+            }
+        });
+        autorun(reaction => {
+            const p = wms.loadData();
+            p.then(() => {
+                wms;
+                expect(description).toBe('Layer-Group type layer: mobile-black-spot-programme:funded-base-stations-group');
+                cleanup();
+                done();
+            }, err => {
+                fail(err);
+                cleanup();
+                done();
+            });
+            reaction.dispose();
         });
     });
 });
