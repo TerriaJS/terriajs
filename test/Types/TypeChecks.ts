5d83715521350daa13c08a5f6cdce64b96404714
Kevin Ring <kevin@kotachrome.com>
2019-03-27 22:01:04 +1100

    Clean up TypeChecks.ts.

--- test/Types/TypeChecks.ts
+++ test/Types/TypeChecks.ts
@@ -1,20 +1,29 @@
-type IfEquals<X, Y, True=X, False=never> =
+// Based on the approach described here: https://stackoverflow.com/a/49579497
+type IfEquals<X, Y, Then=true, Else=false> =
     (<T>() => T extends X ? 1 : 2) extends
-    (<T>() => T extends Y ? 1 : 2) ? True : False;
+    (<T>() => T extends Y ? 1 : 2) ? Then : Else;
+
+type Writable<T, P extends keyof T = keyof T> = {
+    -readonly [K in P]: T[P];
+};
 
 type WritableKeys<T> = {
-    [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+    [P in keyof T]-?: IfEquals<Pick<T, P>, Writable<Pick<T, P>>, P, never>
 }[keyof T];
 
 type ReadonlyKeys<T> = {
-    [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, never, P>
+    [P in keyof T]-?: IfEquals<Pick<T, P>, Writable<Pick<T, P>>, never, P>
 }[keyof T];
-    
-export type Equals<A, B> = IfEquals<A, B, true, false>;
-export type IsWritableArray<T> = T extends Array<infer U> ? true : false;
-export type IsReadonlyArray<T> = IsWritableArray<T> extends true ? false : T extends ReadonlyArray<infer U> ? true : false;
-export type IsReadonly<T, K extends keyof T> = K extends ReadonlyKeys<T> ? true : false;
-export type IsWritable<T, K extends keyof T> = K extends WritableKeys<T> ? true : false;
+
+type And<A, B> = A extends true ? (B extends true ? true : false) : false;
+type Not<C> = C extends true ? false : true;
+
+export type Equals<A, B> = IfEquals<A, B>;
+export type Extends<A, B> = A extends B ? true : false;
+export type IsWritableArray<T> = Extends<T, Array<any>>;
+export type IsReadonlyArray<T> = And<Extends<T, ReadonlyArray<any>>, Not<IsWritableArray<T>>>;
+export type IsReadonly<T, K extends keyof T> = Extends<K, ReadonlyKeys<T>>;
+export type IsWritable<T, K extends keyof T> = Extends<K, WritableKeys<T>>;
 
 export function expectTrue<T extends true>() {}
 export function expectFalse<T extends false>() {}
