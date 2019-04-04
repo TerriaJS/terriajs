f5cf4d5a5e4fb1bb978054ae56e8941a4fce5aa8
Chloe <chloeleichen@gmail.com>
2019-04-05 10:25:49 +1100

    some minor refactor

--- lib/ReactViews/StoryBuilder.jsx
+++ lib/ReactViews/StoryBuilder.jsx
@@ -8,20 +8,19 @@ import Icon from "./Icon.jsx";
 import Story from './Story.jsx';
 import StoryEditor from './StoryEditor.jsx';
 import uniqid from 'uniqid';
-import { activateStory } from './StoryPanel.jsx';
 
 import Styles from './story-builder.scss';
 
-function array_move(arr, old_index, new_index) {
-    if (new_index >= arr.length) {
-        var k = new_index - arr.length + 1;
+function arrayMove(arr, oldIndex, newIndex) {
+    if (newIndex >= arr.length) {
+        let k = newIndex - arr.length + 1;
         while (k--) {
             arr.push(undefined);
         }
     }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-    return arr; // for testing
-};
+    arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+    return arr; 
+}
 
 const StoryBuilder = createReactClass({
     displayName: 'StoryBuilder',
@@ -93,12 +92,12 @@ const StoryBuilder = createReactClass({
    
   moveUp(index, story) {
     const stories = this.props.terria.stories || [];
-    this.props.terria.stories = array_move(stories, index, index-1);
+    this.props.terria.stories = arrayMove(stories, index, index-1);
   },
 
   moveDown(index, story) {
     const stories = this.props.terria.stories || [];
-    this.props.terria.stories = array_move(stories, index, index+1);
+    this.props.terria.stories = arrayMove(stories, index, index+1);
   },
     
      renderIntro() {
