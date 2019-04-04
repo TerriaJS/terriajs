1e3a65b6ea8c303b35b5a0a182632e04c466f17c
Wing Ho <mrwingh@gmail.com>
2019-04-04 19:23:23 +1100

    wip add previewed item state to share

--- lib/ReactViews/ExplorerWindow/ExplorerWindow.jsx
+++ lib/ReactViews/ExplorerWindow/ExplorerWindow.jsx
@@ -3,6 +3,7 @@ import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ko from 'terriajs-cesium/Source/ThirdParty/knockout';
+import defined from 'terriajs-cesium/Source/Core/defined';
 
 import ObserveModelMixin from '../ObserveModelMixin';
 import Tabs from './Tabs.jsx';
@@ -41,6 +42,9 @@ const ExplorerWindow = createReactClass({
 
         if (terria.sharedFromExplorerPanel) {
             viewState.openAddData();
+        }
+        if (defined(terria.previewedItemId) && terria.catalog.shareKeyIndex[terria.previewedItemId]) {
+            viewState.viewCatalogMember(terria.catalog.shareKeyIndex[terria.previewedItemId]);
         }
         this.onVisibilityChange(this.isVisible());
     },
