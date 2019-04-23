f1f653e77e4eeb1204c53e68c06866482b19f05c
Kevin Ring <kevin@kotachrome.com>
2019-04-23 11:24:42 +1000

    Doc and better name for createTransformerAllowUndefined.

--- lib/Core/createTransformerAllowUndefined.ts
+++ lib/Core/createTransformerAllowUndefined.ts
@@ -12,10 +12,18 @@ function isUndefinedObject(x: any): x is UndefinedObject {
 
 const undefinedObject = new UndefinedObject();
 
+/**
+ * This function is identical to the MobX `createTransformer` function except that it allows `undefined` to be used as a transformed value.
+ * @param transformer The transformer function.
+ * @param onCleanup A function called when a memoized result is no longer needed.
+ */
 export default function createTransformerAllowUndefined<A, B>(transformer: ITransformer<A | undefined, B>, onCleanup?: (resultObject: B | undefined, sourceObject?: A) => void): ITransformer<A | undefined, B> {
     function unwrap(object: A | UndefinedObject): B {
         return transformer(isUndefinedObject(object) ? undefined : object);
     }
+    Object.defineProperty(unwrap, 'name', {
+        value: (transformer.name || 'anonymous') + '-allowUndefined'
+    });
     const unwrapOnCleanup = onCleanup == undefined ? undefined : function(resultObject: B | undefined, sourceObject?: A | UndefinedObject): void {
         const unwrapped = isUndefinedObject(sourceObject) ? undefined : sourceObject;
         return onCleanup(resultObject, unwrapped);
