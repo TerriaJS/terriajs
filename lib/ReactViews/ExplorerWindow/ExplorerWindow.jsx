580803c5688490cb87fad14bc4176d8ee0a07af8
Wing Ho <mrwingh@gmail.com>
2019-05-07 14:42:48 +1000

    Lint

--- lib/ReactViews/ExplorerWindow/ExplorerWindow.jsx
+++ lib/ReactViews/ExplorerWindow/ExplorerWindow.jsx
@@ -3,7 +3,6 @@ import createReactClass from "create-react-class";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import ko from "terriajs-cesium/Source/ThirdParty/knockout";
-import defined from "terriajs-cesium/Source/Core/defined";
 
 import ObserveModelMixin from "../ObserveModelMixin";
 import Tabs from "./Tabs.jsx";
