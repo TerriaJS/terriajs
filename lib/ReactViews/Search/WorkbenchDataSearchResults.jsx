0b027177b1e397dcedd2877c996109f0ca828d9f
rowanwins <rowanwins@yahoo.com.au>
2019-05-13 14:07:59 +1000

    Rejig with promises. Add search capability to WMS. Displays results on click in seperate panel.

--- lib/ReactViews/Search/WorkbenchDataSearchResults.jsx
+++ lib/ReactViews/Search/WorkbenchDataSearchResults.jsx
@@ -7,6 +7,7 @@ import SearchResult from './SearchResult.jsx';
 import classNames from 'classnames';
 import Icon from "../Icon.jsx";
 import Styles from './location-search-result.scss';
+import { addGeoJsonFeatureFromWorkbenchData } from '../../Models/workbenchDataSearchUtils.js';
 
 const WorkbenchDataSearchResults = createReactClass({
     displayName: 'WorkbenchDataSearchResults',
@@ -32,8 +33,8 @@ const WorkbenchDataSearchResults = createReactClass({
         };
     },
 
-    onMatchClick(d) {
-        console.log(d);
+    onMatchClick(feature) {
+        addGeoJsonFeatureFromWorkbenchData(this.props.terria, feature);
     },
 
     toggleGroup() {
@@ -49,22 +50,15 @@ const WorkbenchDataSearchResults = createReactClass({
     },
 
     renderResultsFooter() {
-      if(this.state.isExpanded) {
-        return `View less ${this.props.search.name} results`;
+      if (this.state.isExpanded) {
+        return `Hide results from Workbench Data`;
       }
-      return `View more ${this.props.search.name} results`;
+      return `View ${this.props.search.searchResults.length} results from Workbench Data`;
     },
 
     render() {
         const search = this.props.search;
-        const allDatasetResults = [];
-        for (let i = 0; i < search.searchResults.length; i++) {
-            const workbenchItem = search.searchResults[i];
-            for (let ii = 0; ii < workbenchItem.matches.length; ii++) {
-                allDatasetResults.push({match: workbenchItem.matches[ii].matchFieldText, WorkbenchItem: workbenchItem.workbenchItem});
-            }
-        }
-        const results = allDatasetResults.length > 5 ? (this.state.isExpanded ? allDatasetResults : allDatasetResults.slice(0, 5)) : allDatasetResults;
+        const results = this.state.isExpanded ? search.searchResults : [];
         return (<div className={classNames(Styles.providerResult, {[Styles.isOpen]: this.state.isOpen, [Styles.dark]: this.props.theme === 'dark', [Styles.light]: this.props.theme === 'light'})}>
                     <button onClick={this.toggleGroup} className={Styles.heading}>
                         <span>Workbench Data</span>
@@ -76,12 +70,12 @@ const WorkbenchDataSearchResults = createReactClass({
 
                         {results.map((result, i) => (
                             <SearchResult key={i}
-                                          clickAction={this.onMatchClick.bind(null, result)}
-                                          name={`${result.match.toUpperCase()} (${result.WorkbenchItem.name})`}
+                                          clickAction={this.onMatchClick.bind(null, result.feature)}
+                                          name={`${result.matchFieldText.toUpperCase()} (${result.workbenchItem.name})`}
                                           icon='data'
                                           theme={this.props.theme}/>
                         ))}
-                        {allDatasetResults.length > 5  && <button className={Styles.footer} onClick={this.toggleExpand}>{this.renderResultsFooter()}<Icon glyph={this.state.isExpanded ? Icon.GLYPHS.opened : Icon.GLYPHS.closed}/></button>}
+                        {search.searchResults.length > 0 && <button className={Styles.footer} onClick={this.toggleExpand}>{this.renderResultsFooter()}<Icon glyph={this.state.isExpanded ? Icon.GLYPHS.opened : Icon.GLYPHS.closed}/></button>}
                     </ul>
                 </div>);
     },
