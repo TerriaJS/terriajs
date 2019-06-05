0f7dd4fc514435d4a5bf939ad86c614de660e7f4
Kevin Ring <kevin@kotachrome.com>
2019-06-05 22:42:11 +1000

    Save models and strata to JSON.

--- lib/Traits/Trait.ts
+++ lib/Traits/Trait.ts
@@ -23,6 +23,7 @@ export default abstract class Trait {
   abstract getValue(strataTopToBottom: StratumFromTraits<ModelTraits>[]): any;
 
   abstract fromJson(model: BaseModel, stratumName: string, jsonValue: any): any;
+  abstract toJson(value: any): any;
 
   abstract isSameType(trait: Trait): boolean;
 }
