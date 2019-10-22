a1be45441405047d0fd979a0d08355b72abbab85
Kevin Ring <kevin@kotachrome.com>
2019-10-22 21:19:36 +1100

    Fix test failure.

--- lib/ViewModels/TerriaViewer.ts
+++ lib/ViewModels/TerriaViewer.ts
@@ -3,7 +3,8 @@ import {
   IComputedValue,
   IObservableValue,
   observable,
-  action
+  action,
+  runInAction
 } from "mobx";
 import CesiumEvent from "terriajs-cesium/Source/Core/Event";
 import Rectangle from "terriajs-cesium/Source/Core/Rectangle";
@@ -97,28 +98,38 @@ export default class TerriaViewer {
 
   @observable
   private _Cesium: typeof import("../Models/Cesium").default | undefined;
+  private _cesiumPromise: Promise<void> | undefined;
 
   private _getCesiumIfLoaded() {
     if (this._Cesium) {
       return this._Cesium;
     } else {
-      import("../Models/Cesium").then(Cesium => {
-        this._Cesium = Cesium.default;
-      });
+      if (!this._cesiumPromise) {
+        this._cesiumPromise = import("../Models/Cesium").then(Cesium => {
+          runInAction(() => {
+            this._Cesium = Cesium.default;
+          });
+        });
+      }
       return NoViewer;
     }
   }
 
   @observable
   private _Leaflet: typeof import("../Models/Leaflet").default | undefined;
+  private _leafletPromise: Promise<void> | undefined;
 
   private _getLeafletIfLoaded() {
     if (this._Leaflet) {
       return this._Leaflet;
     } else {
-      import("../Models/Leaflet").then(Leaflet => {
-        this._Leaflet = Leaflet.default;
-      });
+      if (!this._leafletPromise) {
+        this._leafletPromise = import("../Models/Leaflet").then(Leaflet => {
+          runInAction(() => {
+            this._Leaflet = Leaflet.default;
+          });
+        });
+      }
       return NoViewer;
     }
   }
