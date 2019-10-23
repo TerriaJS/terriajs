aec6b94acff25c2c79bab2846ab1a28e6f8f04a4
Wing Ho <mrwingh@gmail.com>
2019-10-23 17:19:18 +1100

    Add images

--- lib/ReactViews/Guide/Guide.jsx
+++ lib/ReactViews/Guide/Guide.jsx
@@ -41,6 +41,7 @@ const GuidanceProgress = props => {
 
 const guideData = [
   {
+    imageSrc: require("../../../wwwroot/images/guides/satellite-feature.jpg"),
     title: "Satellite Imagery: How do I…?",
     body:
       "A super-quick guide to getting the most out of historical satellite imagery.",
@@ -48,22 +49,26 @@ const guideData = [
     nextText: "View guide"
   },
   {
+    imageSrc: require("../../../wwwroot/images/guides/satellite-zoom.jpg"),
     title: "Where is my satellite imagery?",
     body:
       "You may need to adjust your zoom level to locate near real-time satellite imagery on the map.",
     hidePrev: true
   },
   {
+    imageSrc: require("../../../wwwroot/images/guides/satellite-time.jpg"),
     title: "Loading historical imagery",
     body:
       "In your data workbench, click on the capture date dropdown and select from available times to load historical imagery."
   },
   {
+    imageSrc: require("../../../wwwroot/images/guides/satellite-location.jpg"),
     title: "Filtering imagery by location",
     body:
       "Satellite imagery may not always be available at your preferred time and location. If you only want to view imagery for a particular location, click ‘filter by location’ in your data workbench."
   },
   {
+    imageSrc: require("../../../wwwroot/images/guides/satellite-styles.jpg"),
     title: "How do I view false-colour imagery?",
     body:
       "You can apply a range of false-colour styles to satellite imagery by clicking on the styles dropdown in your data workbench.",
@@ -123,7 +128,7 @@ export const Guide = ({ children }) => {
   return (
     <div className={Styles.guidance}>
       <div className={Styles.image}>
-        {/* <img src={currentGuide.imageSrc} /> */}
+        <img src={currentGuide.imageSrc} />
       </div>
       <button
         type="button"
