0f0795ed80588a6f9d713272244f1faa4dc29579
Emma Krantz <3796838+KeyboardSounds@users.noreply.github.com>
2019-11-21 10:16:36 +1100

    removed unused import

--- lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx
+++ lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import createReactClass from "create-react-class";
 import PropTypes from "prop-types";
 import arrayContains from "../../Core/arrayContains";
 import Branding from "../SidePanel/Branding";
