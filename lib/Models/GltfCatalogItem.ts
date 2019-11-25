0288ab781759c0a5e28a645c2823cdcf6f8a3331
Stephen Davies <steve9164@gmail.com>
2019-11-25 17:27:46 +1100

    Fix TypeScript error

--- lib/Models/GltfCatalogItem.ts
+++ lib/Models/GltfCatalogItem.ts
@@ -121,7 +121,7 @@ export default class GltfCatalogItem
     dataSource.entities.add(
       new Entity({
         position: new ConstantPositionProperty(position),
-        orientation: new ConstantPositionProperty(Quaternion.IDENTITY),
+        orientation: new ConstantProperty(Quaternion.IDENTITY),
         model: this._model
       })
     );
