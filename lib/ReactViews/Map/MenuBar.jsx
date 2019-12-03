7d631abd28fc560684183b1c107f6b056b841bbc
zoran995 <zorankokeza95@gmail.com>
2019-12-03 12:15:39 +0100

    changing to correct aria prop

--- lib/ReactViews/Map/MenuBar.jsx
+++ lib/ReactViews/Map/MenuBar.jsx
@@ -92,7 +92,7 @@ const MenuBar = createReactClass({
                 className={Styles.storyBtn}
                 type="button"
                 onClick={this.onStoryButtonClick}
-                aria-open={this.props.viewState.storyBuilderShown}
+                aria-expanded={this.props.viewState.storyBuilderShown}
               >
                 <Icon glyph={Icon.GLYPHS.story} />
                 <span>Story</span>
