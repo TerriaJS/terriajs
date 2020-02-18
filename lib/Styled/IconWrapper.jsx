2db555086c4dd29e4fbed8d10523865a7d594ee9
Wing Ho <mrwingh@gmail.com>
2020-02-18 15:19:31 +1100

    Rework workbench header incl. lock icon support
    https://github.com/TerriaJS/nsw-digital-twin/issues/246

--- lib/Styled/IconWrapper.jsx
+++ lib/Styled/IconWrapper.jsx
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const IconWrapper = styled.span`
   display: grid;
 
-  ${props => `width: ${props.width || 16}px`};
+  // ${props => `width: ${props.width || 16}px`};
   & > * {
     margin: auto;
   }
-  margin-right: 8px;
+  ${props => props.marginRight && `margin-right: 8px;`}
 
   flex-shrink: 0;
   ${props => props.wide && `margin: auto 16px;`}
