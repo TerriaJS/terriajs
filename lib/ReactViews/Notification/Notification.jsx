252d38cf955e09a9dc406cd817f46d5a387ffb01
Regina Pramesti <regina.pramesti.8@gmail.com>
2020-05-13 19:13:04 +1000

    Remove notification console log

--- lib/ReactViews/Notification/Notification.jsx
+++ lib/ReactViews/Notification/Notification.jsx
@@ -48,7 +48,6 @@ const Notification = observer(
     },
 
     render() {
-      console.log(this.props.viewState.notifications);
       const notification =
         (this.props.viewState.notifications.length > 0 &&
           this.props.viewState.notifications[0]) ||
