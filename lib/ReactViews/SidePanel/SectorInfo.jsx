3a65fce92d0c7d0a9f60f0e117a7eb9a2052f491
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-05-20 11:33:40 +0200

    Added welcome message and sector specific info

--- lib/ReactViews/SidePanel/SectorInfo.jsx
+++ lib/ReactViews/SidePanel/SectorInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Styles from "./sector_info.scss";
+import PropTypes from "prop-types";
 
 class SectorInfo extends React.Component {
   constructor(props) {
@@ -20,28 +21,36 @@ class SectorInfo extends React.Component {
         </>
       );
     }
-    return null;
-    // return (
-    //     <>
-    //         <div className={Styles.panelHeading}>
-    //             <span className={Styles.sectorTitle}>Agriculture</span>
-    //         </div>
-    //         <div className={Styles.sectorInfo}>
-    //             <p>
-    //                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-    //                 convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-    //                 urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-    //                 elementum. Sed nec aliquam velit, bibendum volutpat justo. Proin
-    //                 semper viverra risus at porta. Nunc tincidunt felis eget bibendum
-    //                 elementum. In hac habitasse platea dictumst. Ut eu ullamcorper orci.
-    //                 Suspendisse potenti. Sed eu dolor consectetur ex pulvinar porttitor
-    //                 sit amet a augue. Quisque cursus blandit orci, mattis accumsan dolor
-    //                 auctor a. Mauris et velit eget massa placerat aliquet. Donec eu
-    //                 risus mi.
-    //         </p>
-    //         </div>
-    //     </>
-    // );
+    return (
+      <>
+        <div className={Styles.panelHeading}>
+          <span className={Styles.sectorTitle}>Welcome</span>
+        </div>
+        <div className={Styles.messageConatiner}>
+          <h3>
+            The RECEIPT climate story explorer allows you to explore storylines
+            that show indirect impact of climate change on EU.
+          </h3>
+          <p>
+            As much of the wealth and many of the products that are eaten or
+            used in the EU are produced or sourced in the rest of the world,
+            climate change impacts the EU not only directly, but also through
+            impact on remote regions. With this application, we can build and
+            show stories to highlight several of these climate impact hotspots.
+          </p>
+          <p>
+            More information on the RECEIPT Horizon 2020 project can be found on
+          </p>
+          <a href="https://climatestorylines.eu/ ">
+            https://climatestorylines.eu/{" "}
+          </a>
+        </div>
+      </>
+    );
   }
 }
+SectorInfo.propTypes = {
+  sector: PropTypes.object.isRequired
+};
+
 export default SectorInfo;
--- lib/ReactViews/SidePanel/SectorTabs.jsx
+++ lib/ReactViews/SidePanel/SectorTabs.jsx
@@ -1,54 +1,50 @@
 import React from "react";
 import Icon from "../Icon";
 import Styles from "./sector_tabs.scss";
-
+import PropTypes from "prop-types";
 class SectorTabs extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     const sectors = [
       {
         title: "Agriculture",
         icon: Icon.GLYPHS.agriculture,
-        info: `Agriculture  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-        urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-        elementum. Sed nec aliquam velit, bibendum volutpat justo.`
+        info: `European food security and agri-food based economy are vulnerable to anomalous weather features, for example concerning water scarcity and drought affecting soybean, rice, cocoa and coffee production outside Europe. 
+               This may impact raw material supply chains, food security or price volatility. `,
+        image: "../../wwwroot/images/sectors/agriculture.jpg"
       },
       {
         title: "Manufacturing",
         icon: Icon.GLYPHS.manufacturing,
-        info: `Manufacturing  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-        urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-        elementum. Sed nec aliquam velit, bibendum volutpat justo.`
+        info: `Supply of raw or processed input materials for European industries can be disrupted temporarily in case of heatwaves, 
+               floods or storms in source areas `,
+        image: "../../wwwroot/images/sectors/agriculture.jpg"
       },
       {
         title: "International Cooperation And Development",
         icon: Icon.GLYPHS.internationalCooperationAndDevelopment,
-        info: `International Cooperation And Development Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-        urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-        elementum. Sed nec aliquam velit, bibendum volutpat justo.`
+        info: `Europe’s foreign and development policy involving concerns for migration, food security, political crises, 
+               development aid and disaster risk reduction is highly affected by climatic risks and extremes,
+               and the forced displacement and migration patterns in response to these.`,
+        image: "../../wwwroot/images/sectors/agriculture.jpg"
       },
       {
         title: "Coastal Infrastructure",
         icon: Icon.GLYPHS.coastalInfrastructure,
-        info: `Coastal Infrastructure Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-        urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-        elementum. Sed nec aliquam velit, bibendum volutpat justo.
-        `
+        info: `Civil protection and industrial production are heavily affected when storms or floods, 
+               aggravated by remote ice-sheet melting and sea level rise, 
+               lead to large damage to cities, ports or industrial plants in connected areas.`,
+        image: "../../wwwroot/images/sectors/agriculture.jpg"
       },
       {
         title: "Finance",
         icon: Icon.GLYPHS.finance,
-        info: `Finance Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        convallis ex nulla, eu volutpat urna faucibus quis. Aliquam porta
-        urna eu urna posuere dignissim. Sed bibendum ipsum in eros rhoncus
-        elementum. Sed nec aliquam velit, bibendum volutpat justo.
-        `
+        info: `Strong or multiple tropical cyclones may affect the solvency of (re)insurance companies, investors and EU public finance.  
+              The finance sector and business are exposed via their portfolio and foreign direct investments`,
+        image: "../../wwwroot/images/sectors/agriculture.jpg"
       }
     ];
 
@@ -65,4 +61,7 @@ class SectorTabs extends React.Component {
     );
   }
 }
+SectorTabs.propTypes = {
+  showSectorInfo: PropTypes.func.isRequired
+};
 export default SectorTabs;
