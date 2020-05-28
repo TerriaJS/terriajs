3c02313fbacf340198ba817aeb024bb594f92938
Regina Pramesti <regina.pramesti.8@gmail.com>
2020-05-28 15:46:23 +1000

    Wrap welcome message with theme provider

--- test/ReactViews/WelcomeMessageSpec.tsx
+++ test/ReactViews/WelcomeMessageSpec.tsx
@@ -7,6 +7,8 @@ import { runInAction } from "mobx";
 import WelcomeMessage, {
   WelcomeMessagePure
 } from "../../lib/ReactViews/WelcomeMessage/WelcomeMessage";
+import { ThemeProvider } from "styled-components";
+import { terriaTheme } from "../../lib/ReactViews/StandardUserInterface/StandardTheme";
 
 describe("WelcomeMessage", function() {
   let terria: Terria;
@@ -28,7 +30,11 @@ describe("WelcomeMessage", function() {
   it("renders when showWelcomeMessage is set to true in config file", function() {
     runInAction(() => (terria.configParameters.showWelcomeMessage = true));
     act(() => {
-      testRenderer = create(<WelcomeMessage viewState={viewState} />);
+      testRenderer = create(
+        <ThemeProvider theme={terriaTheme}>
+          <WelcomeMessage viewState={viewState} />
+        </ThemeProvider>
+      );
     });
     const welcomeMessagePure = testRenderer.root.findByType(WelcomeMessagePure);
     expect(welcomeMessagePure.props.showWelcomeMessage).toEqual(true);
@@ -37,7 +43,11 @@ describe("WelcomeMessage", function() {
   it("doesn't render when showWelcomeMessage is set to true in config file", function() {
     runInAction(() => (terria.configParameters.showWelcomeMessage = false));
     act(() => {
-      testRenderer = create(<WelcomeMessage viewState={viewState} />);
+      testRenderer = create(
+        <ThemeProvider theme={terriaTheme}>
+          <WelcomeMessage viewState={viewState} />
+        </ThemeProvider>
+      );
     });
     const welcomeMessagePure = testRenderer.root.findByType(WelcomeMessagePure);
     expect(welcomeMessagePure.props.showWelcomeMessage).toEqual(false);
