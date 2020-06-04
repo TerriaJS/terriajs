b7f2ac65a8ee02f9aedac4196c1276d80c24b8e4
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-06-04 22:38:37 +1000

    remove span element

--- lib/ReactViews/Analytics/ParameterEditor.jsx
+++ lib/ReactViews/Analytics/ParameterEditor.jsx
@@ -50,15 +50,11 @@ const ParameterEditor = createReactClass({
           {this.props.parameter.isRequired && <span> (required)</span>}
         </label>
         {typeof this.props.parameter.description === "string" &&
-        this.props.parameter.description !== "" ? (
-          <span>
-            {parseCustomMarkdownToReact(this.props.parameter.description, {
+        this.props.parameter.description !== ""
+          ? parseCustomMarkdownToReact(this.props.parameter.description, {
               parameter: this.props.parameter
-            })}
-          </span>
-        ) : (
-          ""
-        )}
+            })
+          : ""}
       </div>
     );
   },
