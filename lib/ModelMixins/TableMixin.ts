a015187d14a174a98974efeb224eb991a650b1eb
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-06-04 23:53:13 +1000

    fix tableMixin regionColumnDimensions options

--- lib/ModelMixins/TableMixin.ts
+++ lib/ModelMixins/TableMixin.ts
@@ -367,10 +367,10 @@ function TableMixin<T extends Constructor<Model<TableTraits>>>(Base: T) {
         get name(): string {
           return "Region Column";
         },
-        options: this.tableStyles.map(tableStyle => {
+        options: this.tableColumns.map(col => {
           return {
-            name: tableStyle.id,
-            id: tableStyle.id
+            name: col.name,
+            id: col.name
           };
         }),
         selectedId: this.activeTableStyle.regionColumn?.name,
