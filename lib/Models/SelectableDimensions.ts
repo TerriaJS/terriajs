c041dd18e1258cfbb9c447022d039ceb4ec701b7
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-06-09 22:03:58 +1000

    add selectabledimensions name

--- lib/Models/SelectableDimensions.ts
+++ lib/Models/SelectableDimensions.ts
@@ -1,11 +1,11 @@
 export interface DimensionOption {
   readonly id: string;
-  readonly name: string;
+  readonly name?: string;
 }
 
 export interface SelectableDimension {
   readonly id: string;
-  readonly name: string;
+  readonly name?: string;
   readonly options: readonly DimensionOption[];
   readonly selectedId: string | undefined;
   readonly allowUndefined?: boolean;
