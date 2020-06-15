df34e5089a5e014b6b612a2f73e67ccd1e0fc155
Mike Wu <mike.wu@data61.csiro.au>
2020-06-15 17:08:22 +1000

    Correct unit test description errors.

--- test/Models/esriLineStyleSpec.ts
+++ test/Models/esriLineStyleSpec.ts
@@ -10,99 +10,94 @@ describe("esriLineStyle", function() {
       expect(value).toEqual(7);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSDashDot", function() {
       const value = getLineStyleCesium("esriSLSDashDot");
       expect(value).toEqual(2017);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSDashDotDot", function() {
       const value = getLineStyleCesium("esriSLSDashDotDot");
       expect(value).toEqual(16273);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSLongDash", function() {
       const value = getLineStyleCesium("esriSLSLongDash");
       expect(value).toEqual(2047);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSLongDashDot", function() {
       const value = getLineStyleCesium("esriSLSLongDashDot");
       expect(value).toEqual(4081);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDash", function() {
       const value = getLineStyleCesium("esriSLSShortDash");
       expect(value).toEqual(4095);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDot", function() {
       const value = getLineStyleCesium("esriSLSShortDot");
       expect(value).toEqual(13107);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDashDot", function() {
       const value = getLineStyleCesium("esriSLSShortDashDot");
       expect(value).toEqual(8179);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDashDotDot", function() {
       const value = getLineStyleCesium("esriSLSShortDashDotDot");
       expect(value).toEqual(16281);
     });
 
-    it("esriSLSNull", function() {
-      const value = getLineStyleCesium("esriSLSNull");
-      expect(value).toBeUndefined();
-    });
-
-    it("esriSLSSolid", function() {
-      const value = getLineStyleCesium("esriSLSNull");
+    it("unknown", function() {
+      const value = getLineStyleCesium("unknown");
       expect(value).toBeUndefined();
     });
   });
 
-  describe("corect cesium values", function() {
+  describe("corect leaflet values", function() {
     it("esriSLSDot", function() {
       const value = getLineStyleLeaflet(7);
       expect(value).toEqual([1, 3]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSDashDot", function() {
       const value = getLineStyleLeaflet(2017);
       expect(value).toEqual([4, 3, 1, 3]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSDashDotDot", function() {
       const value = getLineStyleLeaflet(16273);
       expect(value).toEqual([8, 3, 1, 3, 1, 3]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSLongDash", function() {
       const value = getLineStyleLeaflet(2047);
       expect(value).toEqual([8, 3]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSLongDashDot", function() {
       const value = getLineStyleLeaflet(4081);
       expect(value).toEqual([8, 3, 1, 3]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDash", function() {
       const value = getLineStyleLeaflet(4095);
       expect(value).toEqual([4, 1]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDot", function() {
       const value = getLineStyleLeaflet(13107);
       expect(value).toEqual([1, 1]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDashDot", function() {
       const value = getLineStyleLeaflet(8179);
       expect(value).toEqual([4, 1, 1, 1]);
     });
 
-    it("esriSLSDot", function() {
+    it("esriSLSShortDashDotDot", function() {
       const value = getLineStyleLeaflet(16281);
       expect(value).toEqual([4, 1, 1, 1, 1, 1]);
     });
