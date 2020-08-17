f791f29698ad78cbd6b2fb40c04d49f433554222
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-08-17 17:36:41 +1000

    fix drag and drop files

--- lib/ModelMixins/TimeVarying.ts
+++ lib/ModelMixins/TimeVarying.ts
@@ -1,8 +1,16 @@
-import Model from "../Models/Model";
+import Model, { BaseModel } from "../Models/Model";
 import TimeVaryingTraits from "../Traits/TimeVaryingTraits";
 
 export default interface TimeVarying extends Model<TimeVaryingTraits> {
   readonly currentTimeAsJulianDate: Cesium.JulianDate | undefined;
   readonly startTimeAsJulianDate: Cesium.JulianDate | undefined;
   readonly stopTimeAsJulianDate: Cesium.JulianDate | undefined;
+}
+
+export function isTimeVarying(model: BaseModel): model is TimeVarying {
+  return (
+    "currentTimeAsJulianDate" in model &&
+    "startTimeAsJulianDate" in model &&
+    "stopTimeAsJulianDate" in model
+  );
 }
