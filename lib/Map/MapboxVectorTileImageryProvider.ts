3d2f8e9dac04b90d3021f4cd09c1879273e95697
Stephen Davies <steve9164@gmail.com>
2020-08-19 16:56:17 +1000

    Remove unused variables

--- lib/Map/MapboxVectorTileImageryProvider.ts
+++ lib/Map/MapboxVectorTileImageryProvider.ts
@@ -68,8 +68,6 @@ export default class MapboxVectorTileImageryProvider
   ) => ImageryLayerFeatureInfo | undefined;
   private readonly _errorEvent = new CesiumEvent();
   private readonly _ready = true;
-  private readonly _defaultNightAlpha: number | undefined;
-  private readonly _defaultDayAlpha: number | undefined;
 
   constructor(options: MapboxVectorTileImageryProviderOptions) {
     this._uriTemplate = new URITemplate(options.url);
