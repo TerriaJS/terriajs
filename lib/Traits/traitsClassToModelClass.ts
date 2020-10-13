a4f34093e405f75f99c593a13a6685a5b24c8150
Nanda <nanda.sankaran@gmail.com>
2020-10-14 10:23:19 +1100

    Add note with reason for not using `createTransformer`.

--- lib/Traits/traitsClassToModelClass.ts
+++ lib/Traits/traitsClassToModelClass.ts
@@ -3,6 +3,11 @@ import CreateModel from "../Models/CreateModel";
 import ModelTraits from "./ModelTraits";
 import TraitsConstructor from "./TraitsConstructor";
 
+// Unlike other places, we use lodash-es/memoize instead of `createTransformer`
+// to memoize because this method is called during model class definition stage
+// (check objectTrait & objectArrayTrait) and it will always be run outside an
+// `autorun` or an `observer`. Therefore, thecall to `createTransformer` will
+// not memoize and logs a warning.
 const traitsClassToModelClass = memoize(function<T extends ModelTraits>(
   traitsClass: TraitsConstructor<T>
 ) {
