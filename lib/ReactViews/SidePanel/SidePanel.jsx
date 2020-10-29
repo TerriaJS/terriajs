c3e601aebd1c3d2596a7d4bc44ed994c0d901485
Wing Ho <mrwingh@gmail.com>
2020-10-29 20:45:10 +1100

    Clean SidePanel

--- lib/ReactViews/SidePanel/SidePanel.jsx
+++ lib/ReactViews/SidePanel/SidePanel.jsx
@@ -179,25 +179,6 @@ const SidePanel = observer(
             />
             <Spacing bottom={2} />
             <Box justifySpaceBetween>
-              {/* <Link
-              to={CATALOG_ROUTE}
-              type="button"
-              onClick={this.onAddDataClicked}
-              className={Styles.button}
-              title={addData}
-            >
-              <Icon glyph={Icon.GLYPHS.add} />
-              {addData}
-            </Link>
-            <Link
-              to={CATALOG_ROUTE}
-              type="button"
-              onClick={this.onAddLocalDataClicked}
-              className={Styles.uploadData}
-              title={t("addData.load")}
-            >
-              <Icon glyph={Icon.GLYPHS.upload} />
-            </Link> */}
               <SidePanelButton
                 to={CATALOG_ROUTE}
                 type="button"
