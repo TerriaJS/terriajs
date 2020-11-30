4d1fa04a5e68fde4195cf903a104e250c95ab7b1
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-11-30 23:13:55 +1100

    Revert "Add few more magda assertions"
    This reverts commit 69f045dc5f82efb3c91c2a8193a240f968177834.

--- test/Models/TerriaSpec.ts
+++ test/Models/TerriaSpec.ts
@@ -121,6 +121,26 @@ describe("Terria", function() {
     });
 
     describe("via loadMagdaConfig", function() {
+      it("should dereference uniqueId to `/`", function(done) {
+        jasmine.Ajax.stubRequest(/.*api\/v0\/registry.*/).andReturn({
+          // terria's "Magda derived url"
+          responseText: mapConfigBasicString
+        });
+        // no init sources before starting
+        expect(terria.initSources.length).toEqual(0);
+
+        terria
+          .start({
+            configUrl: "test/Magda/map-config-basic.json"
+          })
+          .then(function() {
+            expect(terria.catalog.group.uniqueId).toEqual("/");
+            done();
+          })
+          .catch(error => {
+            done.fail(error);
+          });
+      });
       it("works with basic initializationUrls", function(done) {
         // no init sources before starting
         expect(terria.initSources.length).toEqual(0);
@@ -172,6 +192,10 @@ describe("Terria", function() {
         }
       });
       it("parses dereferenced group aspect", function(done) {
+        // dereferenced res
+        jasmine.Ajax.stubRequest(/.*api\/v0\/registry.*/).andReturn({
+          responseText: mapConfigDereferencedString
+        });
         terria
           .start({
             configUrl: "test/Magda/map-config-dereferenced.json"
@@ -179,10 +203,6 @@ describe("Terria", function() {
           .then(function() {
             const groupAspect = mapConfigDereferencedJson.aspects["group"];
             const ids = groupAspect.members.map((member: any) => member.id);
-            expect(terria.catalog.group.uniqueId).toEqual("/");
-            // ensure user added data co-exists with dereferenced magda members
-            expect(terria.catalog.group.members.length).toEqual(3);
-            expect(terria.catalog.userAddedDataGroup).toBeDefined();
             ids.forEach((id: string) => {
               const model = terria.getModelById(MagdaReference, id);
               if (!model) {
