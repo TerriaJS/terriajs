3859b96730ba7bf9ce4e0a8fe0e0d45dbe52bc44
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-12-01 17:24:47 +1100

    remove default magda root shareKeys + now uses terria aspect

--- lib/Models/Terria.ts
+++ lib/Models/Terria.ts
@@ -980,13 +980,12 @@ export default class Terria {
 
     if (aspects.group && aspects.group.members) {
       const id = config.id;
-      // Add share key for magda root group so we have backward compatibility with JSON-based map share links
-      this.addShareKey(id, "/");
 
       let existingReference = this.getModelById(MagdaReference, id);
       if (existingReference === undefined) {
         existingReference = new MagdaReference(id, this);
-        this.addModel(existingReference);
+        // Add terria aspects shareKeys
+        this.addModel(existingReference, aspects?.terria?.shareKeys);
       }
 
       const reference = existingReference;
