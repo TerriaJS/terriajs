606a12f5ec74dbeb2d3567a7574e66dc06c5f41f
Jesús García <ctw@ctwhome.com>
2020-12-04 17:36:34 +0100

    Style Sector info panel

--- lib/ReactViews/SidePanel/SectorInfo.jsx
+++ lib/ReactViews/SidePanel/SectorInfo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Styles from "./sector_info.scss";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import Icon from "../Icon.jsx";
 
 class SectorInfo extends React.Component {
@@ -14,46 +13,53 @@ class SectorInfo extends React.Component {
       return (
         <div className={Styles.panel}>
           <div className={Styles.panelBarTitle}>
-            <h3 className={Styles.sectorTitle}>{sector.title}</h3>
+            <h2 style={{ marginTop: 0 }}>{sector.title}</h2>
             <button className={Styles.exitBtn} onClick={this.props.close}>
-              <Icon glyph={Icon.GLYPHS.close} />
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.16732 0L0 1.17188L8.81971 10L0 18.8281L1.16732 20L10.013 11.1719L18.8327 20L20 18.8281L11.1803 10L20 1.17188L18.8327 0L10.013 8.82812L1.16732 0Z"
+                  fill="#00194C"
+                />
+              </svg>
             </button>
           </div>
 
-          <div className={Styles.infoContainer}>
-            <div
-              className={classNames(Styles.imageContainer, Styles.lr)}
-              style={{
-                background: `url(${sector.image}) no-repeat center`
-              }}
-            />
-            <p className={Styles.details}>{sector.info}</p>
+          <div className="rc-card">
+            <img src={sector.image} alt="" />
+            <div className="rc-card-text">{sector.info}</div>
           </div>
         </div>
       );
     }
     return (
       <div className={Styles.panel}>
-        <h2>Welcome</h2>
+        <h2 style={{ marginTop: 0 }}>Welcome</h2>
         <div className="rc-card">
-          <p className="bold">
-            The RECEIPT climate story explorer allows you to explore storylines
-            that show indirect impact of climate change on EU.
-          </p>
-          <p>
-            As much of the wealth and many of the products that are eaten or
-            used in the EU are produced or sourced in the rest of the world,
-            climate change impacts the EU not only directly, but also through
-            impact on remote regions. With this application, we can build and
-            show stories to highlight several of these climate impact hotspots.
-          </p>
-          <p>
-            More information on the RECEIPT Horizon 2020 project can be found on
-            <a href="https://climatestorylines.eu/">
-              {" "}
-              chrhttps://climatestorylines.eu/
-            </a>
-          </p>
+          <div className="rc-card-text">
+            <p className="bold">
+              The RECEIPT climate story explorer allows you to explore
+              storylines that show indirect impact of climate change on EU.
+            </p>
+            <p>
+              As much of the wealth and many of the products that are eaten or
+              used in the EU are produced or sourced in the rest of the world,
+              climate change impacts the EU not only directly, but also through
+              impact on remote regions. With this application, we can build and
+              show stories to highlight several of these climate impact
+              hotspots.
+            </p>
+            <p>
+              More information on the RECEIPT Horizon 2020 project can be found
+              on
+              <a href="https://climatestorylines.eu/"> climatestorylines.eu</a>
+            </p>
+          </div>
         </div>
       </div>
     );
