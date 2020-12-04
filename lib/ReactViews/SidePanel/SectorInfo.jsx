83039e3a425ece46581d8e9c39e912bb2c065093
Jesús García <ctw@ctwhome.com>
2020-12-04 16:42:02 +0100

    Welcome panel

--- lib/ReactViews/SidePanel/SectorInfo.jsx
+++ lib/ReactViews/SidePanel/SectorInfo.jsx
@@ -34,25 +34,27 @@ class SectorInfo extends React.Component {
     }
     return (
       <div className={Styles.panel}>
-        <h3 className={Styles.sectorTitle}>Welcome</h3>
-        <h4>
-          The RECEIPT climate story explorer allows you to explore storylines
-          that show indirect impact of climate change on EU.
-        </h4>
-        <p>
-          As much of the wealth and many of the products that are eaten or used
-          in the EU are produced or sourced in the rest of the world, climate
-          change impacts the EU not only directly, but also through impact on
-          remote regions. With this application, we can build and show stories
-          to highlight several of these climate impact hotspots.
-        </p>
-        <p>
-          More information on the RECEIPT Horizon 2020 project can be found on
-          <a href="https://climatestorylines.eu/">
-            {" "}
-            chrhttps://climatestorylines.eu/
-          </a>
-        </p>
+        <h2>Welcome</h2>
+        <div className="rc-card">
+          <p className="bold">
+            The RECEIPT climate story explorer allows you to explore storylines
+            that show indirect impact of climate change on EU.
+          </p>
+          <p>
+            As much of the wealth and many of the products that are eaten or
+            used in the EU are produced or sourced in the rest of the world,
+            climate change impacts the EU not only directly, but also through
+            impact on remote regions. With this application, we can build and
+            show stories to highlight several of these climate impact hotspots.
+          </p>
+          <p>
+            More information on the RECEIPT Horizon 2020 project can be found on
+            <a href="https://climatestorylines.eu/">
+              {" "}
+              chrhttps://climatestorylines.eu/
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
