4ceabdc5fc44ee5decb858d1619f61de7922aca0
Nanda <nanda.sankaran@gmail.com>
2021-01-15 14:22:13 +1100

    Refactor & document for clarity.

--- lib/Models/ItemSearchProviders/IndexedItemSearchProvider.ts
+++ lib/Models/ItemSearchProviders/IndexedItemSearchProvider.ts
@@ -1,13 +1,13 @@
 import i18next from "i18next";
 import BoundingSphere from "terriajs-cesium/Source/Core/BoundingSphere";
 import Cartesian3 from "terriajs-cesium/Source/Core/Cartesian3";
-import URI from "urijs";
 import loadJson5 from "../../Core/loadJson5";
 import ItemSearchProvider, {
   ItemSearchParameter,
   ItemSearchResult
 } from "../ItemSearchProvider";
 import { Index, IndexRoot, parseIndexRoot } from "./Index";
+import joinUrl from "./joinUrl";
 import loadCsv from "./loadCsv";
 
 const t = i18next.t.bind(i18next);
@@ -23,6 +23,13 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
   indexRoot?: IndexRoot;
   data?: Promise<Record<string, string>[]>;
 
+  /**
+   * Construct a IndexedItemSearchProvider.
+   *
+   * Throws an error if indexRootUrl option is not defined.
+   *
+   * @param options An object containing {indexRootUrl: string}
+   */
   constructor(options: any) {
     super(options);
     const indexRootUrl = options?.indexRootUrl;
@@ -31,6 +38,9 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     this.indexRootUrl = indexRootUrl;
   }
 
+  /**
+   * Returns a Map of searchable parameters indexed by the parameter id.
+   */
   get parameters(): Map<string, Parameter> {
     const indexes = this.indexRoot?.indexes;
     if (!indexes) return new Map();
@@ -42,14 +52,14 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     );
   }
 
-  toAbsoluteUrl(url: string) {
-    const uri = URI(url);
-    return uri.is("absolute")
-      ? url
-      : uri.absoluteTo(this.indexRootUrl).toString();
-  }
-
-  buildParameterForIndex(
+  /**
+   * Returns a parameter object for the specified property.
+   *
+   * @param propertyId ID of the property
+   * @param index The index definition of the property
+   * @return The parameter object
+   */
+  private buildParameterForIndex(
     propertyId: string,
     index: Index
   ): ItemSearchParameter {
@@ -80,6 +90,10 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     }
   }
 
+  /**
+   * Fetches & parses the indexRoot file and then triggers fetching of the data
+   * file but does not wait for it to complete.
+   */
   async initialize() {
     const indexRootUrl = this.indexRootUrl;
     const json = await loadJson5(indexRootUrl);
@@ -100,6 +114,9 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     return idPropertyName;
   }
 
+  /**
+   * @returns A promise that resolves the search parameters.
+   */
   async describeParameters(): Promise<ItemSearchParameter[]> {
     return [...this.parameters.values()].map(({ index, ...rest }) => rest);
   }
@@ -115,8 +132,21 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     }
   };
 
+  /**
+   * Search the indexes for all the given parameter values and return the results.
+   *
+   * @param parameterValues A Map from parameterId to a search query
+   * @returns A promise that resolves to the search results.
+   */
   async search(parameterValues: Map<string, any>): Promise<ItemSearchResult[]> {
-    const searchParameters = Promise.all(
+    // This is roughly what happens in this function
+    // 1) For each parameter search the corresponding index
+    // 2) This gives a set of matching IDs
+    // 3) Lookup the data corresponding to each matching ID from data.csv
+    // 4) Use the data to build the search results.
+
+    // Iterate each parameter and search its index.
+    const search = Promise.all(
       Array.from(parameterValues).map(async ([parameterId, value]) => {
         const parameter = this.parameters.get(parameterId);
         if (!parameter) throw new Error(`Unexpected parameter ${parameterId}`);
@@ -124,10 +154,15 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
         return ids;
       })
     );
+
+    // Meanwhile, load data.csv
     const data = await this.getOrLoadData();
-    const idSets = await searchParameters;
-    await this.getOrLoadData();
+
+    // Merge the IDs from the search into a single set
+    const idSets = await search;
     const matchingIds = intersectSets(idSets);
+
+    // Map the IDs to data and build the search result.
     const results = [...matchingIds].map(id =>
       this.buildResult(this.lookupDataForId(data, id), id)
     );
@@ -139,6 +174,13 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     return index.load(this.indexRootUrl, value).then(() => index.search(value));
   }
 
+  /**
+   * Load data.csv and return it.
+   *
+   * Caches the data so that subsequent calls do not result in a network request & parsing.
+   *
+   * @return A promise that resolves to the data from data.csv
+   */
   async getOrLoadData() {
     if (this.data) {
       return this.data;
@@ -146,7 +188,7 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     if (!this.indexRoot?.dataUrl) {
       throw new Error(`indexRoot is not loaded`);
     }
-    const dataUrl = this.toAbsoluteUrl(this.indexRoot.dataUrl);
+    const dataUrl = joinUrl(this.indexRootUrl, this.indexRoot.dataUrl);
     const promise = loadCsv(dataUrl, {
       dynamicTyping: true,
       header: true
@@ -164,6 +206,12 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     return row;
   }
 
+  /**
+   * Build search result from raw data.
+   *
+   * @param record The raw data for the result read from data.csv
+   * @param dataIdx The index of the record in data.csv
+   */
   buildResult(
     record: Record<string, string>,
     dataIdx: number
@@ -175,12 +223,16 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
         `ID property not defined for data record at index ${dataIdx}`
       );
     }
+
+    // The record can have a bunch of arbitrary properties and a few known
+    // properties. We use the latitude, longitude, height & radius for
+    // constructing a zoom target for the search result.
     let { latitude, longitude, height, radius, ...properties } = record;
     const _latitude = parseFloat(latitude);
     const _longitude = parseFloat(longitude);
-    const _height = parseFloat(height);
-    const _radius = parseFloat(radius);
-    if (isNaN(_latitude) || isNaN(_longitude) || isNaN(_radius)) {
+    const _featureHeight = parseFloat(height);
+    const _tileRadius = parseFloat(radius);
+    if (isNaN(_latitude) || isNaN(_longitude) || isNaN(_tileRadius)) {
       throw new Error(
         `No valid zoom point defined for data record at index ${dataIdx}`
       );
@@ -188,9 +240,10 @@ export default class IndexedItemSearchProvider extends ItemSearchProvider {
     const center = Cartesian3.fromDegrees(_longitude, _latitude);
     const boundingSphere = new BoundingSphere(
       center,
-      // If height is provided zoom to it with a threshold so that we don't get
-      // too close. This might need more fine tuning.
-      isNaN(_height) ? _radius : Math.max(_height * 2, 50)
+      // Prefer the user specified feature height over the tile radius which is
+      // automatically calculated by the indexer.
+      // We also don't want to zoom close to small objects, so threshold the zoom radius to 50.
+      isNaN(_featureHeight) ? _tileRadius : Math.max(_featureHeight * 2, 50)
     );
     return {
       id,
