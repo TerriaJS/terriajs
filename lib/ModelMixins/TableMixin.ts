bb9a88714394ee4f16e7d0cedc384509552176e0
Emma Krantz <emma.krantz@data61.csiro.au>
2021-01-20 17:19:19 +0800

    Add line and point chart type, supported in chart custom component
    - ChartCustomComponent now has a "chart-type" attribute

--- lib/ModelMixins/TableMixin.ts
+++ lib/ModelMixins/TableMixin.ts
@@ -270,7 +270,7 @@ function TableMixin<T extends Constructor<Model<TableTraits>>>(Base: T) {
             name: yColumn.title,
             categoryName: this.name,
             key: `key${this.uniqueId}-${this.name}-${yColumn.name}`,
-            type: "line",
+            type: this.chartType,
             xAxis,
             points,
             domain: calculateDomain(points),
