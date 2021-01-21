55f3c4296b6178056d1cdbed8e53922fd439bbea
zoran995 <zorankokeza95@gmail.com>
2021-01-21 15:06:06 +0100

    update useTranslationIfExists to request translation# at start of the key and add deprecation warning

--- lib/Language/languageHelpers.ts
+++ lib/Language/languageHelpers.ts
@@ -4,5 +4,15 @@ import i18next from "i18next";
  * Takes a given string and translates it if it exists, otherwise return
  */
 export function useTranslationIfExists(keyOrString: string) {
-  return i18next.exists(keyOrString) ? i18next.t(keyOrString) : keyOrString;
+  if (keyOrString && keyOrString.indexOf("translate#") === 0) {
+    const translationKey = keyOrString.substr("translate#".length);
+    return i18next.exists(translationKey)
+      ? i18next.t(translationKey)
+      : translationKey;
+  } else {
+    console.warn(
+      "using translation key within config won't work in future unless you prefix it with `translate#`"
+    );
+    return keyOrString;
+  }
 }
