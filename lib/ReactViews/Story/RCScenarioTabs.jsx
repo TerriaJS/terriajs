558a0d9d0c589e840648a2b4653b7b1d76ed8c14
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-01-24 20:05:12 +0100

    Added Scenarion panel to Scenarios Tabs

--- lib/ReactViews/Story/RCScenarioTabs.jsx
+++ lib/ReactViews/Story/RCScenarioTabs.jsx
@@ -2,37 +2,55 @@ import React from "react";
 import PropTypes from "prop-types";
 import Styles from "./story.scss";
 import classNames from "classnames";
+import Icon from "../Icon";
+import RCScenariosPanel from "../Map/Panels/RCScenariosPanel/RCScenariosPanel";
 class RCScenarioTabs extends React.Component {
   constructor(props) {
     super(props);
   }
   state = {
-    selectedScenario: 0
+    selectedScenario: 0,
+    showModal: false
+  };
+  onModalDismiss = () => {
+    this.setState({ showModal: false });
   };
   render() {
     const { selectedScenario } = this.state;
     const { story, onScenarioChange } = this.props;
-
-    console.log("story", story);
     return (
-      <div className={Styles.tabsContainer}>
-        {Object.keys(story.text).map((storyText, id) => {
-          return (
-            <div
-              key={id}
-              onClick={() => {
-                this.setState({ selectedScenario: id });
-                onScenarioChange(id);
-              }}
-              className={classNames(
-                Styles.tab,
-                selectedScenario === id && Styles.selectedTab
-              )}
-            >
-              <span>SSP {storyText}</span>
-            </div>
-          );
-        })}
+      <div className={Styles.scenarios}>
+        <div className={Styles.tabsContainer}>
+          {Object.keys(story.text).map((storyText, id) => {
+            return (
+              <div
+                key={id}
+                onClick={() => {
+                  this.setState({ selectedScenario: id });
+                  onScenarioChange(id);
+                }}
+                className={classNames(
+                  Styles.tab,
+                  selectedScenario === id && Styles.selectedTab
+                )}
+              >
+                <span>SSP {storyText}</span>
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className={Styles.infoIcon}
+          onClick={() => {
+            this.setState({ showModal: true });
+          }}
+        >
+          <Icon glyph={Icon.GLYPHS.info} />
+        </div>
+        <RCScenariosPanel
+          showDropdownAsModal={this.state.showModal}
+          onModalDismiss={this.onModalDismiss}
+        />
       </div>
     );
   }
