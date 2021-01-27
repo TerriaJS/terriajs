1a1e040a7d66b54188683069eedc15c9720da80f
Nanda <nanda.sankaran@gmail.com>
2021-01-27 18:00:11 +1100

    Remove stale code.

--- lib/ReactViews/Tools/ItemSearchTool/HighlightResult.tsx
+++ lib/ReactViews/Tools/ItemSearchTool/HighlightResult.tsx
@@ -1,11 +1,5 @@
 import React, { useEffect } from "react";
-import BoundingSphere from "terriajs-cesium/Source/Core/BoundingSphere";
-import Cartographic from "terriajs-cesium/Source/Core/Cartographic";
-import Ellipsoid from "terriajs-cesium/Source/Core/Ellipsoid";
-import sampleTerrainMostDetailed from "terriajs-cesium/Source/Core/sampleTerrainMostDetailed";
-import LatLonHeight from "../../../Core/LatLonHeight";
 import SearchableItemMixin from "../../../ModelMixins/SearchableItemMixin";
-import Cesium from "../../../Models/Cesium";
 import { ItemSearchResult } from "../../../Models/ItemSearchProvider";
 import usePrevious from "../../Hooks/usePrevious";
 
@@ -43,33 +37,6 @@ function zoomToResult(
       result.zoomToTarget as any,
       undefined as any
     );
-  }
-}
-
-function cesiumZoomToFeature(
-  cesium: Cesium,
-  zoomTarget: ItemSearchResult["zoomToTarget"]
-) {
-  if (zoomTarget instanceof BoundingSphere) {
-    cesium.scene.camera.flyToBoundingSphere(zoomTarget);
-  } else {
-    const { latitude, longitude, featureHeight } = zoomTarget;
-    if (featureHeight > 20) {
-      const cartographic = Cartographic.fromDegrees(longitude, latitude);
-      const center = Ellipsoid.WGS84.cartographicToCartesian(cartographic);
-      const boundingSphere = new BoundingSphere(center, featureHeight * 2);
-      cesium.scene.camera.flyToBoundingSphere(boundingSphere);
-    } else {
-      sampleTerrainMostDetailed(cesium.scene.terrainProvider, [
-        Cartographic.fromDegrees(longitude, latitude)
-      ]).then(([preciseCartographic]) => {
-        preciseCartographic.height += featureHeight;
-        const destination = Ellipsoid.WGS84.cartographicToCartesian(
-          preciseCartographic
-        );
-        cesium.scene.camera.flyTo({ destination });
-      });
-    }
   }
 }
 
