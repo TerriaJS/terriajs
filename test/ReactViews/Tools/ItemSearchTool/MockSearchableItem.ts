6efceef073c8f2746d63695da8783786d1472223
Nanda <nanda.sankaran@gmail.com>
2021-02-16 14:48:01 +1100

    Add ShowableTraits to MockSearchableItem.

--- test/ReactViews/Tools/ItemSearchTool/MockSearchableItem.ts
+++ test/ReactViews/Tools/ItemSearchTool/MockSearchableItem.ts
@@ -1,10 +1,12 @@
 import SearchableItemMixin from "../../../../lib/ModelMixins/SearchableItemMixin";
 import CreateModel from "../../../../lib/Models/CreateModel";
 import { ItemSearchResult } from "../../../../lib/Models/ItemSearchProvider";
+import mixTraits from "../../../../lib/Traits/mixTraits";
 import SearchableItemTraits from "../../../../lib/Traits/SearchableItemTraits";
+import ShowableTraits from "../../../../lib/Traits/ShowableTraits";
 
 export default class MockSearchableItem extends SearchableItemMixin(
-  CreateModel(SearchableItemTraits)
+  CreateModel(mixTraits(SearchableItemTraits, ShowableTraits))
 ) {
   highlightItemSearchResults(results: ItemSearchResult[]) {
     return () => {};
