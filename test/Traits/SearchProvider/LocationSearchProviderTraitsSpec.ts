4fba7f8e5ae0cb37cde091c73a641addf5a434fa
zoran995 <zorankokeza95@gmail.com>
2021-02-18 00:45:08 +0100

    add tests

--- test/Traits/SearchProvider/LocationSearchProviderTraitsSpec.ts
+++ test/Traits/SearchProvider/LocationSearchProviderTraitsSpec.ts
@@ -1,11 +1,32 @@
 import Terria from "../../../lib/Models/Terria";
+import BingMapsSearchProvider from "../../../lib/Models/SearchProvider/BingMapsSearchProvider";
+import LocationSearchProviderMixin from "../../../lib/ModelMixins/SearchProvider/LocationSearchProviderMixin";
 
 describe("LocationSearchProviderTraits", function() {
   let terria: Terria;
+  let bingMapsSearchProvider: BingMapsSearchProvider;
   beforeEach(async function() {
     terria = new Terria({
       baseUrl: "./"
     });
-    //geoJsonCatalogItem = new GeoJsonCatalogItem("test", terria);
+    bingMapsSearchProvider = new BingMapsSearchProvider("test", terria);
+  });
+
+  it(" - properly mixed", function() {
+    expect(
+      LocationSearchProviderMixin.isMixedInto(bingMapsSearchProvider)
+    ).toBeTruthy();
+  });
+
+  it(" - propperly defines default recommendedListLength", function() {
+    expect(bingMapsSearchProvider.recommendedListLength).toEqual(5);
+  });
+
+  it(" - propperly defines default flightDurationSeconds", function() {
+    expect(bingMapsSearchProvider.flightDurationSeconds).toEqual(1.5);
+  });
+
+  it(" - propperly defines default isOpen", function() {
+    expect(bingMapsSearchProvider.isOpen).toEqual(true);
   });
 });
