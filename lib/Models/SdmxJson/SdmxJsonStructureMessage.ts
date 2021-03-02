78b5aa8c5af6008468149293cfefb7d0a72185a4
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-03-02 14:07:02 +1100

    add sdmx structure message type comment

--- lib/Models/SdmxJson/SdmxJsonStructureMessage.ts
+++ lib/Models/SdmxJson/SdmxJsonStructureMessage.ts
@@ -1,3 +1,11 @@
+/*  This was generated from https://github.com/sdmx-twg/sdmx-json/blob/master/structure-message/tools/schemas/1.0/sdmx-json-structure-schema.json using `json-schema-to-typescript`.
+
+It has also been refactored manually to provide better type names and reduce code duplication
+
+These are the structures which are inlcluded:
+  dataStructures, conceptSchemes, codelists, agencySchemes, dataProviderSchemes, dataConsumerSchemes, organisationUnitSchemes, dataflows, metadataflows, provisionAgreements, structureSets, categorisations, attachmentConstraints, contentConstraints
+*/
+
 export type ArrayOneOrMore<T> = {
   0: T;
 } & Array<T>;
