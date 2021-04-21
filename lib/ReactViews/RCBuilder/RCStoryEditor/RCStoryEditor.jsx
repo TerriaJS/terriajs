870cebc8976e9b7d3fb2bda743ab1135d7b4a49c
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-04-21 16:05:07 +0200

    Added sector selection

--- lib/ReactViews/RCBuilder/RCStoryEditor/RCStoryEditor.jsx
+++ lib/ReactViews/RCBuilder/RCStoryEditor/RCStoryEditor.jsx
@@ -1,32 +1,126 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Styles from "./RCStoryEditor.scss";
+import Tooltip from "../../RCTooltip/RCTooltip";
+import Icon from "../../Icon";
+// Sector images
+import Agriculture from "../../../../wwwroot/images/receipt/sectors/agriculture.jpg";
+import Manufacturing from "../../../../wwwroot/images/receipt/sectors/manufacturing.png";
+import InternationalCooperation from "../../../../wwwroot/images/receipt/sectors/international-cooperation.png";
+import CoastalInfra from "../../../../wwwroot/images/receipt/sectors/coastal-Infra.png";
+import Finance from "../../../../wwwroot/images/receipt/sectors/finance.png";
 class RCStoryEditor extends React.Component {
   constructor(props) {
     super(props);
   }
   state = {
-    selectedScenario: 0,
-    showModal: false
+    sector: null,
+    selectedId: -1,
+    selectedSectors: []
   };
-  onModalDismiss = () => {
-    this.setState({ showModal: false });
+  sectors = [
+    {
+      id: "agriculture",
+      title: "Agriculture",
+      icon: Icon.GLYPHS.agriculture,
+      iconHover: Icon.GLYPHS.agricultureHover
+    },
+    {
+      id: "manufacturing",
+      title: "Manufacturing",
+      icon: Icon.GLYPHS.manufacturing,
+      iconHover: Icon.GLYPHS.manufacturingHover
+    },
+    {
+      id: "cooperation",
+      title: "International Cooperation And Development",
+      icon: Icon.GLYPHS.internationalCooperationAndDevelopment,
+      iconHover: Icon.GLYPHS.internationalCooperationAndDevelopmentHover
+    },
+    {
+      id: "coastalInfrastructure",
+      title: "Coastal Infrastructure",
+      icon: Icon.GLYPHS.coastalInfrastructure,
+      iconHover: Icon.GLYPHS.coastalInfrastructureHover
+    },
+    {
+      id: "finance",
+      title: "Finance",
+      icon: Icon.GLYPHS.finance,
+      iconHover: Icon.GLYPHS.financeHover
+    }
+  ];
+  onSectorChanged = event => {
+    // current array of options
+    const selectedSectors = this.state.selectedSectors;
+    let index;
+
+    // check if the check box is checked or unchecked
+    if (event.target.checked) {
+      // add the  value of the checkbox to selectedSectors array
+      selectedSectors.push(event.target.value);
+    } else {
+      // or remove the value from the unchecked checkbox from the array
+      index = selectedSectors.indexOf(event.target.value);
+      selectedSectors.splice(index, 1);
+    }
+
+    // update the state with the new array of options
+    this.setState({ selectedSectors: selectedSectors });
+    console.log("Sectors", this.state.selectedSectors);
   };
   render() {
-    const { selectedScenario } = this.state;
-    const { story, onScenarioChange } = this.props;
+    const { selectedSectors } = this.state;
     return (
       <div className={Styles.RCStoryEditor}>
-        <div>
-          <label>Story Title</label>
-          <input type="text" placeholder="title" />
-        </div>
+        <h3>Edit your story</h3>
+        <form className={Styles.RCStoryCard}>
+          <div className={Styles.group}>
+            <input type="text" required />
+            <span className={Styles.highlight} />
+            <span className={Styles.bar} />
+            <label>Story Title</label>
+          </div>
+          <div className={Styles.group}>
+            <textarea />
+            <span className={Styles.highlight} />
+            <span className={Styles.bar} />
+            <label>Short Description</label>
+          </div>
+          <div>
+            <label>Sector</label>
+            <div className={Styles.tabsContainer}>
+              {this.sectors.map((sector, id) => {
+                return (
+                  <div key={id}>
+                    <Tooltip
+                      content={sector.title}
+                      direction="bottom"
+                      delay="100"
+                    >
+                      <label>
+                        <input
+                          type="checkbox"
+                          value={sector.title}
+                          onChange={this.onSectorChanged}
+                        />
+                        <Icon
+                          glyph={
+                            selectedSectors.includes(sector.title)
+                              ? sector.iconHover
+                              : sector.icon
+                          }
+                          className={Styles.label}
+                        />
+                      </label>
+                    </Tooltip>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
 }
-RCStoryEditor.propTypes = {
-  story: PropTypes.object,
-  onScenarioChange: PropTypes.func
-};
 export default RCStoryEditor;
