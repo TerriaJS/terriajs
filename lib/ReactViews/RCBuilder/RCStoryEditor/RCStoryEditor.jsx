75cfd9f19d370f5d00803b4ac05acc8d90a13d36
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-04-21 19:01:47 +0200

    Cleanup & refactoring

--- lib/ReactViews/RCBuilder/RCStoryEditor/RCStoryEditor.jsx
+++ lib/ReactViews/RCBuilder/RCStoryEditor/RCStoryEditor.jsx
@@ -1,59 +1,20 @@
 import React from "react";
 import Styles from "./RCStoryEditor.scss";
-import Tooltip from "../../RCTooltip/RCTooltip";
-import Icon from "../../Icon";
-// Sector images
-import Agriculture from "../../../../wwwroot/images/receipt/sectors/agriculture.jpg";
-import Manufacturing from "../../../../wwwroot/images/receipt/sectors/manufacturing.png";
-import InternationalCooperation from "../../../../wwwroot/images/receipt/sectors/international-cooperation.png";
-import CoastalInfra from "../../../../wwwroot/images/receipt/sectors/coastal-Infra.png";
-import Finance from "../../../../wwwroot/images/receipt/sectors/finance.png";
+import sectors from "../../../Data/Sectors.js";
+import RCSectorSelection from "./RCSectorSelection/RCSectorSelection";
+
 class RCStoryEditor extends React.Component {
   constructor(props) {
     super(props);
   }
   state = {
-    sector: null,
-    selectedId: -1,
+    sectors: sectors,
     selectedSectors: []
   };
-  sectors = [
-    {
-      id: "agriculture",
-      title: "Agriculture",
-      icon: Icon.GLYPHS.agriculture,
-      iconHover: Icon.GLYPHS.agricultureHover
-    },
-    {
-      id: "manufacturing",
-      title: "Manufacturing",
-      icon: Icon.GLYPHS.manufacturing,
-      iconHover: Icon.GLYPHS.manufacturingHover
-    },
-    {
-      id: "cooperation",
-      title: "International Cooperation And Development",
-      icon: Icon.GLYPHS.internationalCooperationAndDevelopment,
-      iconHover: Icon.GLYPHS.internationalCooperationAndDevelopmentHover
-    },
-    {
-      id: "coastalInfrastructure",
-      title: "Coastal Infrastructure",
-      icon: Icon.GLYPHS.coastalInfrastructure,
-      iconHover: Icon.GLYPHS.coastalInfrastructureHover
-    },
-    {
-      id: "finance",
-      title: "Finance",
-      icon: Icon.GLYPHS.finance,
-      iconHover: Icon.GLYPHS.financeHover
-    }
-  ];
   onSectorChanged = event => {
-    // current array of options
+    // current array of sectors
     const selectedSectors = this.state.selectedSectors;
     let index;
-
     // check if the check box is checked or unchecked
     if (event.target.checked) {
       // add the  value of the checkbox to selectedSectors array
@@ -69,7 +30,7 @@ class RCStoryEditor extends React.Component {
     console.log("Sectors", this.state.selectedSectors);
   };
   render() {
-    const { selectedSectors } = this.state;
+    const { sectors, selectedSectors } = this.state;
     return (
       <div className={Styles.RCStoryEditor}>
         <h3>Edit your story</h3>
@@ -86,38 +47,11 @@ class RCStoryEditor extends React.Component {
             <span className={Styles.bar} />
             <label>Short Description</label>
           </div>
-          <div>
-            <label>Sector</label>
-            <div className={Styles.tabsContainer}>
-              {this.sectors.map((sector, id) => {
-                return (
-                  <div key={id}>
-                    <Tooltip
-                      content={sector.title}
-                      direction="bottom"
-                      delay="100"
-                    >
-                      <label>
-                        <input
-                          type="checkbox"
-                          value={sector.title}
-                          onChange={this.onSectorChanged}
-                        />
-                        <Icon
-                          glyph={
-                            selectedSectors.includes(sector.title)
-                              ? sector.iconHover
-                              : sector.icon
-                          }
-                          className={Styles.label}
-                        />
-                      </label>
-                    </Tooltip>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <RCSectorSelection
+            sectors={sectors}
+            selectedSectors={selectedSectors}
+            onSectorSelected={this.onSectorChanged}
+          />
         </form>
       </div>
     );
--- lib/ReactViews/RCSectorPanel/SidePanelSectorTabs.jsx
+++ lib/ReactViews/RCSectorPanel/SidePanelSectorTabs.jsx
@@ -2,72 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import knockout from "terriajs-cesium/Source/ThirdParty/knockout";
 import { RCChangeUrlParams } from "../../Models/Receipt";
-import Tooltip from "../RCTooltip/RCTooltip";
 import Icon from "../Icon";
+import Tooltip from "../RCTooltip/RCTooltip";
 import Styles from "./SidePanelSectorTabs.scss";
-
-// Sector images
-import Agriculture from "../../../wwwroot/images/receipt/sectors/agriculture.jpg";
-import Manufacturing from "../../../wwwroot/images/receipt/sectors/manufacturing.png";
-import InternationalCooperation from "../../../wwwroot/images/receipt/sectors/international-cooperation.png";
-import CoastalInfra from "../../../wwwroot/images/receipt/sectors/coastal-Infra.png";
-import Finance from "../../../wwwroot/images/receipt/sectors/finance.png";
+import sectors from "../../Data/Sectors.js";
 
 class SidePanelSectorTabs extends React.Component {
   state = {
     sector: null,
+    sectors: sectors,
     selectedHotspotsList: null,
     selectedId: -1
   };
-  sectors = [
-    {
-      id: "agriculture",
-      title: "Agriculture",
-      icon: Icon.GLYPHS.agriculture,
-      iconHover: Icon.GLYPHS.agricultureHover,
-      info: `European food security and agri-food based economy are vulnerable to anomalous weather features, for example concerning water scarcity and drought affecting soybean, rice, cocoa and coffee production outside Europe.
-               This may impact raw material supply chains, food security or price volatility. `,
-      image: Agriculture
-    },
-    {
-      id: "manufacturing",
-      title: "Manufacturing",
-      icon: Icon.GLYPHS.manufacturing,
-      iconHover: Icon.GLYPHS.manufacturingHover,
-      info: `Supply of raw or processed input materials for European industries can be disrupted temporarily in case of heatwaves,
-               floods or storms in source areas `,
-      image: Manufacturing
-    },
-    {
-      id: "cooperation",
-      title: "International Cooperation And Development",
-      icon: Icon.GLYPHS.internationalCooperationAndDevelopment,
-      iconHover: Icon.GLYPHS.internationalCooperationAndDevelopmentHover,
-      info: `Europe’s foreign and development policy involving concerns for migration, food security, political crises,
-               development aid and disaster risk reduction is highly affected by climatic risks and extremes,
-               and the forced displacement and migration patterns in response to these.`,
-      image: InternationalCooperation
-    },
-    {
-      id: "coastalInfrastructure",
-      title: "Coastal Infrastructure",
-      icon: Icon.GLYPHS.coastalInfrastructure,
-      iconHover: Icon.GLYPHS.coastalInfrastructureHover,
-      info: `Civil protection and industrial production are heavily affected when storms or floods,
-               aggravated by remote ice-sheet melting and sea level rise,
-               lead to large damage to cities, ports or industrial plants in connected areas.`,
-      image: CoastalInfra
-    },
-    {
-      id: "finance",
-      title: "Finance",
-      icon: Icon.GLYPHS.finance,
-      iconHover: Icon.GLYPHS.financeHover,
-      info: `Strong or multiple tropical cyclones may affect the solvency of (re)insurance companies, investors and EU public finance.
-              The finance sector and business are exposed via their portfolio and foreign direct investments`,
-      image: Finance
-    }
-  ];
 
   componentDidMount() {
     this._viewStateSelectedSector = knockout
@@ -83,11 +29,11 @@ class SidePanelSectorTabs extends React.Component {
 
   render() {
     const { viewState } = this.props;
-
-    const selectedId = this.sectors.findIndex(
+    const { sectors } = this.state;
+    const selectedId = sectors.findIndex(
       sector => sector.id === viewState.RCSelectedSector
     );
-    const sector = this.sectors.find(
+    const sector = sectors.find(
       sector => sector.id === viewState.RCSelectedSector
     );
     const selectedHotspotsList =
@@ -102,7 +48,7 @@ class SidePanelSectorTabs extends React.Component {
           //
         }
         <div className={Styles.tabsContainer}>
-          {this.sectors.map((sector, id) => {
+          {sectors.map((sector, id) => {
             return (
               <div
                 key={id}
