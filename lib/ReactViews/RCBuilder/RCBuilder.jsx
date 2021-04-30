2f47cd7a7bac695837bc18a6a0cf70a72678394a
Berend Weel <b.weel@esciencecenter.nl>
2021-04-30 09:50:21 +0200

    Added routes to RCBuilder

--- lib/ReactViews/RCBuilder/RCBuilder.jsx
+++ lib/ReactViews/RCBuilder/RCBuilder.jsx
@@ -1,35 +1,52 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import React from "react";
-
-import { withTranslation } from "react-i18next";
-import createReactClass from "create-react-class";
 import PropTypes from "prop-types";
+import React from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import Styles from "./RCBuilder.scss";
+import RCStoryCreator from "./RCStoryCreator/RCStoryCreator";
+import RCStoryEditor from "./RCStoryEditor/RCStoryEditor";
 import RCStoryList from "./RCStoryList/RCStoryList";
-
-const Receipt = require("../../Models/Receipt");
-
-export const RCBuilder = createReactClass({
-  propTypes: {
-    /**
-     * Terria instance
-     */
-    viewState: PropTypes.object.isRequired
-  },
+class RCBuilder extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentDidMount() {
     console.log("🎹", this.props.viewState);
-  },
+  }
 
   render() {
     const { t, viewState } = this.props;
-
+    const path = this.props.match.path;
     return (
       <div>
-        <h1>I am the builder</h1>
-        <RCStoryList />
+        <div>
+          <h1 className={Styles.header}>
+            <img
+              src={require("../../../wwwroot/images/receipt/receipt-logo.svg")}
+            />
+            Story Builder
+          </h1>
+        </div>
+        <Switch>
+          <Route path={`${path}/new`}>
+            <RCStoryCreator viewState={viewState} />
+          </Route>
+          <Route path={`${path}/story/:id/edit`}>
+            <RCStoryEditor viewState={viewState} />
+          </Route>
+          <Route>
+            <RCStoryList viewState={viewState} />
+          </Route>
+        </Switch>
       </div>
     );
   }
-});
-// export default withAuthenticator(withTranslation()(RCBuilder));
-export default withTranslation()(RCBuilder);
+}
+RCBuilder.propTypes = {
+  /**
+   * Terria instance
+   */
+  viewState: PropTypes.object.isRequired
+};
+
+export default withRouter(RCBuilder);
