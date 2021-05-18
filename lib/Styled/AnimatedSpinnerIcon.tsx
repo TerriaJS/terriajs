998670c52c2a78efd82b220829327ec8f80626b9
Nanda <nanda.sankaran@gmail.com>
2021-05-18 17:46:36 +1000

    Remove vendor prefixes for styling in AnimatedSpinnerIcon.

--- lib/Styled/AnimatedSpinnerIcon.tsx
+++ lib/Styled/AnimatedSpinnerIcon.tsx
@@ -5,27 +5,13 @@ import { StyledIcon, GLYPHS } from "./Icon";
 const AnimatedSpinnerIcon = styled(StyledIcon).attrs({
   glyph: GLYPHS.loader
 })`
-  -webkit-animation: spin 2s infinite linear;
   animation: spin 2s infinite linear;
-
-  @-webkit-keyframes spin {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(359deg);
-      transform: rotate(359deg);
-    }
-  }
 
   @keyframes spin {
     0% {
-      -webkit-transform: rotate(0deg);
       transform: rotate(0deg);
     }
     100% {
-      -webkit-transform: rotate(359deg);
       transform: rotate(359deg);
     }
   }
