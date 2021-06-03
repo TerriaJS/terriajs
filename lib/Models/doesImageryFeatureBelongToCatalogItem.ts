b84906463d285589067bbbfe50c06092c780ac9e
Nanda <nanda.sankaran@gmail.com>
2021-06-03 15:15:07 +1000

    Extract doesImageryBelongToCatalogItem.

---
+++ lib/Models/doesImageryFeatureBelongToCatalogItem.ts
@@ -0,0 +1,20 @@
+import MappableMixin, { ImageryParts } from "../ModelMixins/MappableMixin";
+import Feature from "./Feature";
+import { BaseModel } from "./Model";
+
+/**
+ * Returns true if the given feature belongs to the catalog item
+ */
+export default function doesImageryFeatureBelongToItem(
+  feature: Feature,
+  item: BaseModel
+): Boolean {
+  if (!MappableMixin.isMixedInto(item)) return false;
+  const imageryProvider = feature.imageryLayer?.imageryProvider;
+  if (imageryProvider === undefined) return false;
+  return (
+    item.mapItems.find(
+      m => ImageryParts.is(m) && m.imageryProvider === imageryProvider
+    ) !== undefined
+  );
+}
