87142c3c75e2b1f7fc73056ddf3f048a3f8e1052
Nanda <nanda.sankaran@gmail.com>
2021-07-01 13:34:04 +1000

    Add test for end value outside index range.

--- test/Models/ItemSearchProviders/NumericIndexSpec.ts
+++ test/Models/ItemSearchProviders/NumericIndexSpec.ts
@@ -2,7 +2,7 @@ import NumericIndex from "../../../lib/Models/ItemSearchProviders/NumericIndex";
 
 describe("NumericIndex", function() {
   describe("search", function() {
-    it("returns all matching IDs", async function() {
+    it("returns all matching IDs when the search query is within the index range", async function() {
       const index = new NumericIndex("", { min: 0, max: 10 });
       index.idValuePairs = Promise.resolve([
         { dataRowId: 0, value: 0 },
@@ -18,6 +18,24 @@ describe("NumericIndex", function() {
       ]);
       const ids = await index.search({ start: 1, end: 9 });
       expect([...ids]).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
+    it("returns all matching IDs when the end value of the search query is outside the index range", async function() {
+      const index = new NumericIndex("", { min: 0, max: 10 });
+      index.idValuePairs = Promise.resolve([
+        { dataRowId: 0, value: 0 },
+        { dataRowId: 1, value: 1 },
+        { dataRowId: 2, value: 2 },
+        { dataRowId: 3, value: 3 },
+        { dataRowId: 4, value: 3 },
+        { dataRowId: 5, value: 3 },
+        { dataRowId: 6, value: 3 },
+        { dataRowId: 7, value: 3 },
+        { dataRowId: 8, value: 9 },
+        { dataRowId: 9, value: 10 }
+      ]);
+      const ids = await index.search({ start: 1, end: 100 });
+      expect([...ids]).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     });
 
     it("returns all matching IDs when the start and end values are the same", async function() {
