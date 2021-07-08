f94a44d1856ea6db03493371469b3f71e28e3bda
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-07-08 18:43:17 +1000

    add socrata resources to group

--- lib/Models/SocrataCatalogGroup.ts
+++ lib/Models/SocrataCatalogGroup.ts
@@ -1,15 +1,19 @@
 import { action, computed, runInAction } from "mobx";
 import URI from "urijs";
+import isDefined from "../Core/isDefined";
 import loadJson from "../Core/loadJson";
 import runLater from "../Core/runLater";
 import CatalogMemberMixin from "../ModelMixins/CatalogMemberMixin";
 import GroupMixin from "../ModelMixins/GroupMixin";
 import UrlMixin from "../ModelMixins/UrlMixin";
 import ModelReference from "../Traits/ModelReference";
-import SocrataCatalogGroupTraits from "../Traits/TraitsClasses/SocrataCatalogGroupTraits";
+import SocrataCatalogGroupTraits, {
+  FacetFilterTraits
+} from "../Traits/TraitsClasses/SocrataCatalogGroupTraits";
 import CatalogGroup from "./CatalogGroupNew";
 import CommonStrata from "./CommonStrata";
 import CreateModel from "./CreateModel";
+import createStratumInstance from "./createStratumInstance";
 import LoadableStratum from "./LoadableStratum";
 import { BaseModel } from "./Model";
 import proxyCatalogItemUrl from "./proxyCatalogItemUrl";
@@ -19,6 +23,89 @@ export interface Facet {
   facet: string;
   count: number;
   values: { value: string; count: number }[];
+}
+
+export interface ResultResponse {
+  resultSetSize: number;
+  timings: unknown;
+  results: Result[];
+}
+
+export interface Result {
+  resource: {
+    name: string;
+    id: string;
+    parent_fxf: unknown;
+    description?: string;
+    attribution?: string;
+    attribution_link?: string;
+    contact_email?: string;
+    type: string;
+    updatedAt: string;
+    createdAt: string;
+    metadata_updated_at: string;
+    data_updated_at: string;
+    // publication_date: string; not sure if this is official
+    page_views: {
+      page_views_last_week: number;
+      page_views_last_month: number;
+      page_views_total: number;
+      page_views_last_week_log: number;
+      page_views_last_month_log: number;
+      page_views_total_log: number;
+    };
+    columns_name: string[];
+    columns_field_name: string[];
+    columns_datatype: string[];
+    columns_description: string[];
+    columns_format: {
+      precisionStyle?: string;
+      noCommas?: string;
+      align?: string;
+    }[];
+    download_count: number;
+    provenance: string;
+    lens_view_type:
+      | "tabular"
+      | "blobby"
+      | "href"
+      | "geo"
+      | "story"
+      | "measure"
+      | "gateway_plugin"
+      | undefined;
+    blob_mime_type: null | string;
+    hide_from_data_json: boolean;
+  };
+  classification: {
+    categories: string[];
+    tags: string[];
+    domain_category: string;
+    domain_tags: string[];
+    domain_metadata: { key: string; value: string }[];
+  };
+  metadata: {
+    domain: string;
+    license: string;
+  };
+  permalink: string;
+  link: string;
+  owner?: {
+    id: string;
+    user_type?: string;
+    display_name: string;
+  };
+  creator?: {
+    id: string;
+    user_type?: string;
+    display_name: string;
+  };
+}
+
+export interface SocrataError {
+  code?: string;
+  error: true | string;
+  message?: string;
 }
 
 export class SocrataCatalogStratum extends LoadableStratum(
@@ -33,42 +120,94 @@ export class SocrataCatalogStratum extends LoadableStratum(
 
     const domain = URI(catalogGroup.url).hostname();
 
-    const facets = (await loadJson(
-      proxyCatalogItemUrl(
-        catalogGroup,
-        `${catalogGroup.url}/api/catalog/v1/domains/${domain}/facets`
-      )
-    )) as Facet[];
+    let facets: Facet[] = [];
+    let results: Result[] = [];
 
-    if (!Array.isArray(facets))
-      throw `Could not fetch facets for domain ${domain}`;
+    // If not facet filters have been set - get facets
+    if (
+      !isDefined(catalogGroup.facetFilters) ||
+      catalogGroup.facetFilters.length === 0
+    ) {
+      const facetsToUse = catalogGroup.facetGroups;
 
-    if (facets.length === 0)
-      throw `Could not find any facets for domain ${domain}`;
+      const facetResponse = await loadJson(
+        proxyCatalogItemUrl(
+          catalogGroup,
+          `${catalogGroup.url}/api/catalog/v1/domains/${domain}/facets`
+        )
+      );
 
-    return new SocrataCatalogStratum(catalogGroup, facets);
+      if (facetResponse.error) {
+        throw facetResponse.message ?? facetResponse.error;
+      }
+
+      facets = facetResponse;
+
+      if (!Array.isArray(facets))
+        throw `Could not fetch facets for domain ${domain}`;
+
+      facets = facets.filter(f => facetsToUse.includes(f.facet));
+
+      if (facets.length === 0)
+        throw `Could not find any facets for domain ${domain}`;
+    }
+
+    // If facetFilter is set, use it to search for datasets
+    else {
+      // http://api.us.socrata.com/api/catalog/v1?categories=Education&tags=families&search_context=data.seattle.gov
+      const resultsUri = URI(
+        `${catalogGroup.url}/api/catalog/v1?search_context=${domain}`
+      );
+
+      catalogGroup.facetFilters.forEach(({ name, value }) =>
+        name && isDefined(value) ? resultsUri.addQuery(name, value) : null
+      );
+      const resultsResponse = await loadJson(
+        proxyCatalogItemUrl(catalogGroup, resultsUri.toString())
+      );
+
+      if (resultsResponse.error) {
+        throw resultsResponse.message ?? resultsResponse.error;
+      }
+
+      results = (resultsResponse as ResultResponse).results;
+
+      if (!Array.isArray(results) || results.length === 0)
+        throw `Could not find any results for domain ${domain} and facets: ${catalogGroup.facetFilters
+          .map(({ name, value }) => `${name} = ${value}`)
+          .join(", ")}`;
+    }
+
+    return new SocrataCatalogStratum(catalogGroup, facets, results);
   }
 
   duplicateLoadableStratum(model: BaseModel): this {
     return new SocrataCatalogStratum(
       model as SocrataCatalogGroup,
-      this.facets
+      this.facets,
+      this.results
     ) as this;
   }
+
   constructor(
     private readonly catalogGroup: SocrataCatalogGroup,
-    private readonly facets: Facet[]
+    private readonly facets: Facet[],
+    private readonly results: Result[]
   ) {
     super();
   }
 
   @computed
   get members(): ModelReference[] {
-    return this.facets.map(f => this.getFacetId(f));
+    return [
+      ...this.facets.map(f => this.getFacetId(f)),
+      ...this.results.map(r => this.getResultId(r))
+    ];
   }
 
   createMembers() {
     this.facets.forEach(facet => this.createGroupFromFacet(facet));
+    this.results.forEach(result => this.createItemFromResult(result));
   }
 
   /** Turn facet into SocrataCatalogGroup */
@@ -101,11 +240,11 @@ export class SocrataCatalogStratum extends LoadableStratum(
       const facetValueId = `${facetGroupId}/${facetValue.value}`;
 
       let facetValueGroup = this.catalogGroup.terria.getModelById(
-        CatalogGroup,
+        SocrataCatalogGroup,
         facetValueId
       );
       if (facetValueGroup === undefined) {
-        facetValueGroup = new CatalogGroup(
+        facetValueGroup = new SocrataCatalogGroup(
           facetValueId,
           this.catalogGroup.terria,
           undefined
@@ -125,12 +264,50 @@ export class SocrataCatalogStratum extends LoadableStratum(
         }`
       );
 
+      facetValueGroup.setTrait(stratum, "url", this.catalogGroup.url);
+
+      facetValueGroup.setTrait(stratum, "facetFilters", [
+        createStratumInstance(FacetFilterTraits, {
+          name: facet.facet,
+          value: facetValue.value
+        })
+      ]);
+
       facetGroup!.add(CommonStrata.underride, facetValueGroup);
     });
   }
 
+  /** Turn Result into catalog item */
+  @action
+  createItemFromResult(result: Result) {
+    const resultId = this.getResultId(result);
+
+    let resultModel = this.catalogGroup.terria.getModelById(
+      CatalogGroup,
+      resultId
+    );
+    if (resultModel === undefined) {
+      resultModel = new CatalogGroup(
+        resultId,
+        this.catalogGroup.terria,
+        undefined
+      );
+      this.catalogGroup.terria.addModel(resultModel);
+    }
+
+    // Replace the stratum inherited from the parent group.
+    const stratum = CommonStrata.underride;
+    resultModel.strata.delete(stratum);
+
+    resultModel.setTrait(stratum, "name", result.resource.name);
+  }
+
   getFacetId(facet: Facet) {
     return `${this.catalogGroup.uniqueId}/${facet.facet}`;
+  }
+
+  getResultId(result: Result) {
+    return `${this.catalogGroup.uniqueId}/${result.resource.id}`;
   }
 }
 
