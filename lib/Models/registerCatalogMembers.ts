dced0d5d481e77f51704ac3730a02089e5808c39
Nanda <nanda.sankaran@gmail.com>
2021-07-19 14:41:58 +1000

    Add MapboxMapCatalogItem.

--- lib/Models/registerCatalogMembers.ts
+++ lib/Models/registerCatalogMembers.ts
@@ -27,6 +27,7 @@ import GtfsCatalogItem from "./GtfsCatalogItem";
 import IonImageryCatalogItem from "./IonImageryCatalogItem";
 import KmlCatalogItem from "./KmlCatalogItem";
 import MagdaReference from "./MagdaReference";
+import MapboxMapCatalogItem from "./MapboxMapCatalogItem";
 import MapboxVectorTileCatalogItem from "./MapboxVectorTileCatalogItem";
 import OpenDataSoftCatalogGroup from "./OpenDataSoftCatalogGroup";
 import OpenDataSoftCatalogItem from "./OpenDataSoftCatalogItem";
@@ -199,6 +200,10 @@ export default function registerCatalogMembers() {
   CatalogMemberFactory.register(
     SocrataMapViewCatalogItem.type,
     SocrataMapViewCatalogItem
+  );
+  CatalogMemberFactory.register(
+    MapboxMapCatalogItem.type,
+    MapboxMapCatalogItem
   );
 
   UrlToCatalogMemberMapping.register(
