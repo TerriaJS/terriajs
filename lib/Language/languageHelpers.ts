9bb16bd2dac39ea038c4629dc4b38fb931b96e20
Zoran Kokeza <zorankokeza95@gmail.com>
2021-09-02 18:00:01 +0200

    language helpers default value

--- lib/Language/languageHelpers.ts
+++ lib/Language/languageHelpers.ts
@@ -4,15 +4,15 @@ import i18next from "i18next";
  * Takes a given string and translates it if it exists, otherwise return
  */
 export function useTranslationIfExists(
-  keyOrString: string,
+  keyOrString: string = "",
   options?: { [key: string]: string }
 ) {
-  if (keyOrString && keyOrString.indexOf("translate#") === 0) {
+  if (keyOrString.indexOf("translate#") === 0) {
     const translationKey = keyOrString.substr("translate#".length);
     return i18next.exists(translationKey)
       ? i18next.t(translationKey, options)
       : translationKey;
   } else {
-    return keyOrString || "";
+    return keyOrString;
   }
 }
