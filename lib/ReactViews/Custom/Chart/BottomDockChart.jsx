8d5ea3f572e6a0216c57715c7cd59c7409e1fadc
Nanda <nanda.sankaran@gmail.com>
2021-10-12 10:57:07 +1100

    Remove unused imports.

--- lib/ReactViews/Custom/Chart/BottomDockChart.jsx
+++ lib/ReactViews/Custom/Chart/BottomDockChart.jsx
@@ -1,12 +1,5 @@
 import { observer } from "mobx-react";
-import {
-  action,
-  computed,
-  observable,
-  makeObservable,
-  toJS,
-  trace
-} from "mobx";
+import { action, computed, observable, makeObservable } from "mobx";
 import { AxisLeft, AxisBottom } from "@visx/axis";
 import { RectClipPath } from "@visx/clip-path";
 import { localPoint } from "@visx/event";
