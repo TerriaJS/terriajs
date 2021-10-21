5bc7248f6ce04b8aafcb5e60192ea4b8db3d22a7
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-10-21 11:52:20 +1100

    doc change

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -30,7 +30,7 @@ git config --global user.email "info@terria.io"
 git config --global user.name "GitHub Actions"
 git commit -a -m 'temporary commit' # so the version doesn't indicate local modifications
 git tag -a "TerriaMap-$TERRIAMAP_COMMIT_HASH--TerriaJS-$TERRIAJS_COMMIT_HASH" -m 'temporary tag'
-rm yarn.lock # because TerriaMap's package-lock.json won't reflect terriajs dependencies
+rm yarn.lock # because TerriaMap's yarn.lock won't reflect terriajs dependencies
 yarn install
 yarn add -W moment@2.24.0
 npm run gulp build
