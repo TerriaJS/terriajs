86424b84dae9723aca5045e196302675999d7f20
William Cheung <william.cheung@data61.csiro.au>
2022-02-25 15:48:27 +1100

    added explanation to animation added timeout to the animateEnd closure

--- lib/Core/animation.ts
+++ lib/Core/animation.ts
@@ -1,3 +1,22 @@
+/**
+ * The purpose of this module is to provide a standard way to perform actions after an animations ends.
+ * The advantage is that the code is more tightly coupled together and we don't have to guess when the animation ends.
+ * There is also a timeout fallback that will reject the Promise if the animation end event is not fired in time.
+ * 
+ * Old code:
+ *
+  triggerSomeAnimation();
+  setTimeout(function() {
+    somePostAction();
+  }, 200);
+ * 
+ * New code: 
+  animateEnd(this.elementRef.current).finally(somePostAction);
+  triggerAnimation();
+ * 
+ *   
+ */
+
 const ANIMATION_TIMEOUT = 500;
 
 const transitionEnd = (element: Element | null) =>
@@ -13,8 +32,9 @@ const transitionEnd = (element: Element | null) =>
     }
   });
 
-let timeoutID: ReturnType<typeof setTimeout>;
-const animationTimeout = () =>
+const animationTimeout = (
+  timeoutID: ReturnType<typeof setTimeout> | undefined
+) =>
   new Promise<void>((_, reject) => {
     timeoutID = setTimeout(() => {
       reject("Animation timed out. Did you forget to animate the element?");
@@ -27,7 +47,10 @@ const animationTimeout = () =>
  * @returns Promise that will resolve when the element finishes its animation or reject if it is not detected within ANIMATION_DURATION ms
  */
 
-export const animateEnd = (element: Element | null) =>
-  Promise.race([transitionEnd(element), animationTimeout()]).finally(() =>
-    clearTimeout(timeoutID)
-  );
+export const animateEnd = (element: Element | null) => {
+  let timeoutID: ReturnType<typeof setTimeout> | undefined = undefined;
+  return Promise.race([
+    transitionEnd(element),
+    animationTimeout(timeoutID)
+  ]).finally(() => timeoutID && clearTimeout(timeoutID));
+};
