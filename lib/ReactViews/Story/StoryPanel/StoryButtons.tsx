68042856897ecbc75f15690cf767adc2822af224
Zoran Kokeza <zorankokeza95@gmail.com>
2022-03-24 22:47:18 +0100

    update story panel ui

--- lib/ReactViews/Story/StoryPanel/StoryButtons.tsx
+++ lib/ReactViews/Story/StoryPanel/StoryButtons.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Icon from "../../../Styled/Icon";
+import styled, { useTheme } from "styled-components";
+import Button, { RawButton } from "../../../Styled/Button";
+import Icon, { StyledIcon } from "../../../Styled/Icon";
 import Styles from "../story-panel.scss";
 
 interface BtnProp {
@@ -13,39 +15,38 @@ export const CollapseBtn = ({
 }: { isCollapsed: boolean } & BtnProp) => {
   const { t } = useTranslation();
   return (
-    <button
-      className={Styles.exitBtn}
+    <RawButton
       title={isCollapsed ? t("story.expand") : t("story.collapse")}
       onClick={onClick}
     >
-      <Icon glyph={isCollapsed ? Icon.GLYPHS.info : Icon.GLYPHS.arrowDown} />
-    </button>
+      {isCollapsed ? (
+        <StoryIcon styledWidth={"24px"} glyph={Icon.GLYPHS.info} />
+      ) : (
+        <StoryIcon styledWidth={"12px"} glyph={Icon.GLYPHS.arrowDown} />
+      )}
+    </RawButton>
   );
 };
 
 export const ExitBtn = ({ onClick }: BtnProp) => {
   const { t } = useTranslation();
+  const theme = useTheme();
   return (
-    <button
-      className={Styles.exitBtn}
-      title={t("story.exitBtn")}
-      onClick={onClick}
-    >
-      <Icon glyph={Icon.GLYPHS.close} />
-    </button>
+    <RawButton onClick={onClick} title={t("story.exitBtn")}>
+      <StoryIcon
+        styledWidth={"12px"}
+        glyph={Icon.GLYPHS.close}
+        css={`
+          border-radius: 50%;
+          border: 3px solid ${theme.textDark};
+          padding: 3px;
+        `}
+      />
+    </RawButton>
   );
 };
 
-export const LocationBtn = ({ onClick }: BtnProp) => {
-  const { t } = useTranslation();
-
-  return (
-    <button
-      className={Styles.exitBtn}
-      title={t("story.locationBtn")}
-      onClick={onClick}
-    >
-      <Icon glyph={Icon.GLYPHS.location} />
-    </button>
-  );
-};
+export const StoryIcon = styled(StyledIcon).attrs(props => ({
+  fillColor: props.theme.textDark,
+  opacity: 0.5
+}))``;
