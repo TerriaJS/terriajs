c1566ec1913fdee802294f0081f3d8b182b9d1f6
Nanda <nanda.sankaran@gmail.com>
2022-04-04 11:55:41 +1000

    Rename function to `ViewingControls.addMenuItem`.

--- lib/Models/ViewingControlsMenu.ts
+++ lib/Models/ViewingControlsMenu.ts
@@ -9,10 +9,10 @@ export namespace ViewingControlsMenu {
    * Adds a new viewing control menu option.
    *
    * @param viewState - The {@link ViewState} instance.
-   * @param menuOption - A function that either generates a {@link ViewingControl} description for the given catalog item or returns `undefined` if no option should be shown
+   * @param generateViewingControl - A function that either generates a {@link ViewingControl} description for the given catalog item or returns `undefined` if no option should be shown
    * @returns A cleanup function that when called will remove the option
    */
-  export function addMenuOption(
+  export function addMenuItem(
     viewState: ViewState,
     generateViewingControl: (
       item: CatalogMemberMixin.Instance
