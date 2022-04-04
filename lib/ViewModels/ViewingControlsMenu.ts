1770d5b35478551c135a5402af3a93c583223557
Nanda <nanda.sankaran@gmail.com>
2022-04-04 14:53:12 +1000

    Add bit more doc for `ViewingControlsMenu.addItem`.

--- lib/ViewModels/ViewingControlsMenu.ts
+++ lib/ViewModels/ViewingControlsMenu.ts
@@ -10,9 +10,16 @@ export type ViewingControlGenerateFunction = (
   item: CatalogMemberMixin.Instance
 ) => Optional<ViewingControl, "id"> | undefined;
 
+/**
+ * Functions that modify ViewingControls menu.
+ *
+ * ViewingControls menu is the 3-dot menu that every item in the workbench gets.
+ */
 export namespace ViewingControlsMenu {
   /**
    * Adds a new viewing control menu option.
+   *
+   * `addMenuItem` adds a `generateViewingControl` function that gets called for each workbench item. This function can at runtime decide if it wants to show an option for the item and return the `ViewingControl` definition or `undefined` if it does not want to show an option.
    *
    * @param viewState - The {@link ViewState} instance.
    * @param generateViewingControl - A function that either generates a {@link ViewingControl} description for the given catalog item or returns `undefined` if no option should be shown
