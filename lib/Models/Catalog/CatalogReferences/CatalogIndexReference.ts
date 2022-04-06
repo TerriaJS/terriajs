6f639e495bdd09a4adb8cbd3fb7e2d53e16a399a
Nick Forbes-Smith <nick@forbes-smith.com>
2022-04-06 17:22:24 +1000

    Improve `CatalogIndexReference` error messages

--- lib/Models/Catalog/CatalogReferences/CatalogIndexReference.ts
+++ lib/Models/Catalog/CatalogReferences/CatalogIndexReference.ts
@@ -94,11 +94,15 @@ export default class CatalogIndexReference extends ReferenceMixin(
       return member;
     }
 
+    const parentErrorMessage = new TerriaError({
+      title: `Failed to find dataset "${this.name ?? this.uniqueId}"`,
+      message: {
+        key: "core.terriaError.networkRequestMessage"
+      },
+      importance: 1
+    });
+
     // No member exists - throw error
-    throw TerriaError.combine(
-      errors,
-      `Failed to find member ${this.name ?? this.uniqueId}`
-    ) ??
-      TerriaError.from(`Failed to find member ${this.name ?? this.uniqueId}`);
+    throw TerriaError.combine(errors, parentErrorMessage) ?? parentErrorMessage;
   }
 }
