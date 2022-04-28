be7f0ad017eee5185ba7f3dea8f85b4ab16ece07
Staf Smith <staffordsmith83@gmail.com>
2022-04-28 11:24:25 +0800

    Change backgroundColor of ProgressBar depending on baseMapContrastColor
    Improves visibility on light map backgrounds

--- lib/ReactViews/Map/ProgressBar.jsx
+++ lib/ReactViews/Map/ProgressBar.jsx
@@ -5,67 +5,76 @@ import PropTypes from "prop-types";
 import createReactClass from "create-react-class";
 import EventHelper from "terriajs-cesium/Source/Core/EventHelper";
 import classNames from "classnames";
+import { observer } from "mobx-react";
 
 import Styles from "./progress-bar.scss";
 
 // The map navigation region
-const ProgressBar = createReactClass({
-  propTypes: {
-    terria: PropTypes.object.isRequired
-  },
+const ProgressBar = observer(
+  createReactClass({
+    displayName: "ProgressBar",
+    propTypes: {
+      terria: PropTypes.object.isRequired
+    },
 
-  getInitialState() {
-    return {
-      visible: "hidden"
-    };
-  },
+    getInitialState() {
+      return {
+        visible: "hidden"
+      };
+    },
 
-  /* eslint-disable-next-line camelcase */
-  UNSAFE_componentWillMount() {
-    this.eventHelper = new EventHelper();
+    /* eslint-disable-next-line camelcase */
+    UNSAFE_componentWillMount() {
+      this.eventHelper = new EventHelper();
 
-    this.eventHelper.add(
-      this.props.terria.tileLoadProgressEvent,
-      this.setProgress
-    );
+      this.eventHelper.add(
+        this.props.terria.tileLoadProgressEvent,
+        this.setProgress
+      );
 
-    // TODO - is this actually needed now? load events always get called when
-    // changing viewer. if still reuqired,
-    // clear progress when new viewer observed, rather than mounting to a 'current viewer'
+      // TODO - is this actually needed now? load events always get called when
+      // changing viewer. if still reuqired,
+      // clear progress when new viewer observed, rather than mounting to a 'current viewer'
 
-    // // Clear progress when the viewer changes so we're not left with an invalid progress bar hanging on the screen.
-    // this.eventHelper.add(
-    //   this.props.terria.currentViewer.beforeViewerChanged,
-    //   this.setProgress.bind(this, 0, 0)
-    // );
-  },
+      // // Clear progress when the viewer changes so we're not left with an invalid progress bar hanging on the screen.
+      // this.eventHelper.add(
+      //   this.props.terria.currentViewer.beforeViewerChanged,
+      //   this.setProgress.bind(this, 0, 0)
+      // );
+    },
 
-  setProgress(remaining, max) {
-    const rawPercentage = (1 - remaining / max) * 100;
-    const sanitisedPercentage = Math.floor(remaining > 0 ? rawPercentage : 100);
+    setProgress(remaining, max) {
+      const rawPercentage = (1 - remaining / max) * 100;
+      const sanitisedPercentage = Math.floor(
+        remaining > 0 ? rawPercentage : 100
+      );
 
-    this.setState({
-      percentage: sanitisedPercentage
-    });
-  },
+      this.setState({
+        percentage: sanitisedPercentage
+      });
+    },
 
-  componentWillUnmount() {
-    this.eventHelper.removeAll();
-  },
+    componentWillUnmount() {
+      this.eventHelper.removeAll();
+    },
 
-  render() {
-    const width = this.state.percentage + "%";
-    const visibility = this.state.percentage < 100 ? "visible" : "hidden";
-    const complete = this.state.percentage === 100;
+    render() {
+      const width = this.state.percentage + "%";
+      const visibility = this.state.percentage < 100 ? "visible" : "hidden";
+      const complete = this.state.percentage === 100;
+      // use the baseMapContrastColor to ensure progress bar is visible on light backgrounds
+      const backgroundColor =
+        this.props.terria.baseMapContrastColor ?? "#ffffff";
 
-    return (
-      <div
-        className={classNames(Styles.progressBar, {
-          [Styles.complete]: complete
-        })}
-        style={{ visibility, width }}
-      />
-    );
-  }
-});
+      return (
+        <div
+          className={classNames(Styles.progressBar, {
+            [Styles.complete]: complete
+          })}
+          style={{ visibility, width, backgroundColor }}
+        />
+      );
+    }
+  })
+);
 module.exports = ProgressBar;
