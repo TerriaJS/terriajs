0d4f2a8744bfac0fd17d3a7b4b44633420e08b78
Zoran Kokeza <zorankokeza95@gmail.com>
2022-07-19 09:20:51 +0200

    refactor: merge shareUrlInput into shareUrlClipbord

--- lib/ReactViews/Map/Panels/SharePanel/ShareUrl/ShareUrlClipboard.tsx
+++ lib/ReactViews/Map/Panels/SharePanel/ShareUrl/ShareUrlClipboard.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+
+import Input from "../../../../../Styled/Input";
+
 import Clipboard from "../../../../Clipboard";
-import { ShareUrlInput } from "./ShareUrlInput";
 import Terria from "../../../../../Models/Terria";
 import {
   Category,
@@ -24,18 +27,26 @@ export const ShareUrlClipboard: FC<IShareUrlClipboardProps> = ({
   shareUrl,
   placeholder
 }) => {
+  const { t } = useTranslation();
+
   return (
     <Clipboard
       theme={theme}
       text={shareUrl}
       source={
-        <ShareUrlInput
-          theme={inputTheme}
-          shareUrl={shareUrl}
-          placeholder={placeholder}
+        <Input
+          light={inputTheme === "light"}
+          dark={inputTheme === "dark"}
+          large
+          type="text"
+          value={shareUrl}
+          placeholder={placeholder ?? t("share.shortLinkShortening")}
+          readOnly
+          onClick={e => e.currentTarget.select()}
           css={`
             ${rounded ? `border-radius:  32px 0 0 32px;` : ""}
           `}
+          id="share-url"
         />
       }
       id="share-url"
