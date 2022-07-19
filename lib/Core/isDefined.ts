fcaf3f3b5312b9cbb2d8906d36049f817a88b320
Zoran Kokeza <zorankokeza95@gmail.com>
2022-07-19 19:55:19 +0200

    remove type assertions from isDefined

--- lib/Core/isDefined.ts
+++ lib/Core/isDefined.ts
@@ -1,3 +1,3 @@
 export default function isDefined<T>(value: T | undefined | null): value is T {
-  return <T>value !== undefined && <T>value !== null;
+  return value !== undefined && value !== null;
 }
