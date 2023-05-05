021fb9d8a3c41ac16f77f5b6702d3f653820515f
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2023-05-05 11:50:48 +0200

    fix: animation

--- lib/ReactViews/ExplorerWindow/ExplorerWindow.tsx
+++ lib/ReactViews/ExplorerWindow/ExplorerWindow.tsx
@@ -1,54 +1,120 @@
 import { action } from "mobx";
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useViewState } from "../StandardUserInterface/ViewStateContext";
 import ModalPopup from "./ModalPopup";
 import Box from "../../Styled/Box";
 import { Tabs } from "./Tabs";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
 
 export const ExplorerWindowElementName = "AddData";
 
+const SLIDE_DURATION = 300;
+
 export default observer<React.FC>(function ExplorerWindow() {
   const viewState = useViewState();
-
-  const onClose = action(() => {
-    viewState.closeCatalog();
-    viewState.switchMobileView("nowViewing");
-  });
-
-  const onStartAnimatingIn = action(() => {
-    viewState.explorerPanelAnimating = true;
-  });
-
-  const onDoneAnimatingIn = action(() => {
-    viewState.explorerPanelAnimating = false;
-  });
 
   const isVisible =
     !viewState.useSmallScreenInterface &&
     !viewState.hideMapUi &&
     viewState.explorerPanelIsVisible;
 
+  const [visible, setVisible] = useState(
+    isVisible || viewState.explorerPanelAnimating
+  );
+  const animationTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(
+    null
+  );
+
+  const onStartAnimating = action(() => {
+    viewState.explorerPanelAnimating = true;
+  });
+
+  const onDoneAnimating = action(() => {
+    viewState.explorerPanelAnimating = false;
+  });
+
+  const slideIn = () => {
+    setVisible(true);
+    onStartAnimating?.();
+    animationTimeoutRef.current = setTimeout(() => {
+      onDoneAnimating?.();
+    }, SLIDE_DURATION);
+  };
+
+  const slideOut = () => {
+    setVisible(false);
+    animationTimeoutRef.current = setTimeout(() => {
+      viewState.closeCatalog();
+      viewState.switchMobileView("nowViewing");
+    }, SLIDE_DURATION);
+  };
+
+  useEffect(() => {
+    // Clear previous timeout
+    if (animationTimeoutRef.current !== null) {
+      clearTimeout(animationTimeoutRef.current);
+      animationTimeoutRef.current = null;
+    }
+    if (isVisible) {
+      slideIn();
+    } else {
+      slideOut();
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    const escKeyListener = (e: KeyboardEvent) => {
+      // Only explicitly check share modal state, move to levels/"layers of modals" logic if we need to go any deeper
+      if (e.key === "Escape" && !viewState.shareModalIsVisible) {
+        slideOut();
+      }
+    };
+    window.addEventListener("keydown", escKeyListener, true);
+  });
+
   return (
-    <ModalPopup
-      viewState={viewState}
-      isVisible={isVisible}
-      isTopElement={true}
-      onClose={onClose}
-      onStartAnimatingIn={onStartAnimatingIn}
-      onDoneAnimatingIn={onDoneAnimatingIn}
-    >
-      <Box
+    <ModalPopup isVisible={isVisible} isTopElement={true} onClose={slideOut}>
+      <StyledContainer
         id="explorer-panel"
         aria-labelledby="modalTitle"
         aria-describedby="modalDescription"
         role="dialog"
         styledMaxWidth="1200px"
         flex="1"
-        css={{ zIndex: 99999 }}
+        delay={SLIDE_DURATION}
+        visible={visible}
       >
-        <Tabs viewState={viewState} onClose={onClose} />
-      </Box>
+        <Tabs viewState={viewState} onClose={slideOut} />
+      </StyledContainer>
     </ModalPopup>
   );
 });
+
+const StyledContainer = styled(Box)<{ visible: boolean; delay: number }>`
+  z-index: 99999;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  animation: ${(props) => (props.visible ? fadeIn : fadeOut)}
+    ${(props) => props.delay}ms linear;
+  transition: visibility ${(props) => `${props.delay}ms`} linear,
+    opacity ${(props) => props.delay}ms linear;
+`;
--- lib/ReactViews/ExplorerWindow/ModalPopup.tsx
+++ lib/ReactViews/ExplorerWindow/ModalPopup.tsx
@@ -5,85 +5,37 @@ import Box from "../../Styled/Box";
 import { PrefaceBox } from "../Generic/PrefaceBox";
 import styled from "styled-components";
 
-const SLIDE_DURATION = 300;
-
 interface IProps {
   isVisible?: boolean;
   onClose: () => void;
-  viewState: ViewState;
-  onStartAnimatingIn?: () => void;
-  onDoneAnimatingIn?: () => void;
   children: React.ReactNode;
   isTopElement?: boolean;
 }
 
-const ModalPopup: React.FC<IProps> = (props) => {
-  const [inTransition, setInTransition] = useState(false);
-  const animationTimeout = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-  /* function slideIn() {
-    props.onStartAnimatingIn?.();
-    setInTransition(true);
-    animationTimeout.current = setTimeout(() => {
-      setInTransition(false);
-      setTimeout(() => {
-        props.onDoneAnimatingIn?.();
-      }, SLIDE_DURATION);
-    });
-  }
-
-  function slideOut() {
-    setInTransition(true);
-    animationTimeout.current = setTimeout(() => {
-      setInTransition(false);
-    }, SLIDE_DURATION);
-  }
-
-  useEffect(() => {
-    // Clear previous timeout
-    if (animationTimeout.current !== null) {
-      clearTimeout(animationTimeout.current);
-      animationTimeout.current = null;
-    }
-    if (props.isVisible) {
-      slideIn();
-    } else {
-      slideOut();
-    }
-  }, [props.isVisible]); */
-
-  useEffect(() => {
-    const escKeyListener = (e: KeyboardEvent) => {
-      // Only explicitly check share modal state, move to levels/"layers of modals" logic if we need to go any deeper
-      if (e.key === "Escape" && !props.viewState.shareModalIsVisible) {
-        props.onClose();
-      }
-    };
-    window.addEventListener("keydown", escKeyListener, true);
-  });
-
-  // Render explorer panel when explorer panel should be visible
-  //  or when sliding out (animation)
-  const renderUi = props.isVisible || inTransition;
+const ModalPopup: React.FC<IProps> = ({
+  isVisible,
+  isTopElement,
+  onClose,
+  children
+}) => {
+  const renderUi = isVisible;
 
   return renderUi ? (
-    <>
-      <ModalPopupBox
-        className={props.isTopElement ? "top-element" : ""}
-        id="explorer-panel-wrapper"
-        aria-hidden={!props.isVisible}
-      >
-        <PrefaceBox
-          className={props.isTopElement ? "top-element" : ""}
-          onClick={props.onClose}
-          role="presentation"
-          aria-hidden="true"
-          pseudoBg
-          css={{ top: 0, left: 0, zIndex: 99989 }}
-        ></PrefaceBox>
-        {props.children}
-      </ModalPopupBox>
-    </>
+    <ModalPopupBox
+      className={isTopElement ? "top-element" : ""}
+      id="explorer-panel-wrapper"
+      aria-hidden={!isVisible}
+    >
+      <PrefaceBox
+        className={isTopElement ? "top-element" : ""}
+        onClick={onClose}
+        role="presentation"
+        aria-hidden="true"
+        pseudoBg
+        css={{ top: 0, left: 0, zIndex: 99989 }}
+      ></PrefaceBox>
+      {children}
+    </ModalPopupBox>
   ) : null;
 };
 
