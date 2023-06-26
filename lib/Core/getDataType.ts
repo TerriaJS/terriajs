f259ab5e31db18fd773493440f291dc47120adf2
Staf Smith <staffordsmith83@gmail.com>
2023-06-26 14:25:36 +0800

    Remove cog from local data types

--- lib/Core/getDataType.ts
+++ lib/Core/getDataType.ts
@@ -184,11 +184,6 @@ const builtinLocalDataTypes: LocalDataType[] = [
     value: "shp",
     name: "core.dataType.shp",
     extensions: ["zip"]
-  },
-  {
-    value: "cog",
-    name: "core.dataType.cog",
-    extensions: ["tif"]
   }
   // Add next builtin local upload type
 ];
