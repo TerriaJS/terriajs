2cd52b1a7d641ca75cc1d97fe5b9341e8cc0ae11
Staf Smith <staffordsmith83@gmail.com>
2023-07-20 12:27:50 +0800

    Add new tiling scheme and modify imports

---
+++ lib/ThirdParty/tiff-imagery-provider/TIFFImageryProviderTilingScheme.ts
@@ -0,0 +1,59 @@
+import { Math as CMath } from "terriajs-cesium";
+
+import Cartesian2 from "terriajs-cesium/Source/Core/Cartesian2";
+import Cartesian3 from "terriajs-cesium/Source/Core/Cartesian3";
+import Cartographic from "terriajs-cesium/Source/Core/Cartographic";
+import Ellipsoid from "terriajs-cesium/Source/Core/Ellipsoid";
+import WebMercatorTilingScheme from "terriajs-cesium/Source/Core/WebMercatorTilingScheme";
+
+class TIFFImageryProviderTilingScheme extends WebMercatorTilingScheme {
+  constructor(options?: {
+    ellipsoid?: Ellipsoid;
+    numberOfLevelZeroTilesX?: number;
+    numberOfLevelZeroTilesY?: number;
+    rectangleSouthwestInMeters?: Cartesian2;
+    rectangleNortheastInMeters?: Cartesian2;
+    project?: (pos: number[]) => number[];
+    unproject?: (pos: number[]) => number[];
+  }) {
+    super(options);
+
+    const { project, unproject } = options;
+    if (project) {
+      // @ts-ignore
+      this._projection.project = function (cartographic: Cartographic) {
+        const [x, y] = unproject(
+          [cartographic.longitude, cartographic.latitude].map(CMath.toDegrees)
+        );
+        const z = cartographic.height;
+        return new Cartesian3(x, y, z);
+      };
+    }
+    if (unproject) {
+      // @ts-ignore
+      this._projection.unproject = function (cartesian: Cartesian3) {
+        const [longitude, latitude] = project([cartesian.x, cartesian.y]).map(
+          CMath.toRadians
+        );
+        const height = cartesian.z;
+        return new Cartographic(longitude, latitude, height);
+      };
+    }
+
+    const southwest = this.projection.unproject(
+      options?.rectangleSouthwestInMeters as any
+    );
+    const northeast = this.projection.unproject(
+      options?.rectangleNortheastInMeters as any
+    );
+    // @ts-ignore
+    this._rectangle = new Rectangle(
+      southwest.longitude,
+      southwest.latitude,
+      northeast.longitude,
+      northeast.latitude
+    );
+  }
+}
+
+export default TIFFImageryProviderTilingScheme;
