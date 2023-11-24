c71f1c59bf0b94cf02e966bf60f6b1c3d7c7e751
Mike Wu <mike.wu@data61.csiro.au>
2023-11-24 14:55:11 +1100

    Improve lock icon position in workbench.

--- lib/ReactViews/PrivateIndicator/PrivateIndicator.jsx
+++ lib/ReactViews/PrivateIndicator/PrivateIndicator.jsx
@@ -19,14 +19,6 @@ export default function PrivateIndicator(props) {
       inWorkbench={props.inWorkbench}
       css={`
         margin-top: -1px;
-        ${(p) =>
-          p.inWorkbench &&
-          `
-          position: absolute;
-          margin-right: 2px;
-          right: 20px;
-        `}
-
         svg {
           width: 15px;
           height: 15px;
