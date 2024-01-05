95ad7200cdac3553d2d7507cc6e73d7d8f89a2ff
sixlighthouses <brendon.ward@sixlighthouses.com>
2024-01-05 15:00:00 +1000

    refactor the DateTimeParameterEditor to be a functional component

--- lib/ReactViews/Analytics/DateTimeParameterEditor.jsx
+++ lib/ReactViews/Analytics/DateTimeParameterEditor.jsx
@@ -1,91 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import defined from "terriajs-cesium/Source/Core/defined";
-import createReactClass from "create-react-class";
-import PropTypes from "prop-types";
-
 import Styles from "./parameter-editors.scss";
 import CommonStrata from "../../Models/Definition/CommonStrata";
 
-const DateTimeParameterEditor = createReactClass({
-  displayName: "DateTimeParameterEditor",
+const DateTimeParameterEditor = (props) => {
+  const [dateValue, setDateValue] = useState(
+    defined(props.parameter) && defined(props.parameter.value)
+      ? props.parameter.value
+      : ""
+  );
+  const [timeValue, setTimeValue] = useState("00:00");
 
-  propTypes: {
-    previewed: PropTypes.object,
-    parameter: PropTypes.object
-  },
+  const style =
+    defined(props.parameter) && defined(props.parameter.value)
+      ? Styles.field
+      : Styles.fieldDatePlaceholder;
 
-  getInitialState() {
-    return this.getDateTime();
-  },
-
-  getDateTime() {
-    const dateTimeBreakOut = {};
-    const timeDate = this.props.parameter.value;
-    if (timeDate !== undefined) {
-      const splits = timeDate.split("T");
-      dateTimeBreakOut.date = splits[0];
-      if (splits[1].length === 0) {
-        dateTimeBreakOut.time = "00:00";
-      } else {
-        dateTimeBreakOut.time = splits[1];
-      }
-    } else {
-      dateTimeBreakOut.date = "";
-      dateTimeBreakOut.time = "00:00";
-    }
-
-    this.setDateTime(dateTimeBreakOut);
-
-    return dateTimeBreakOut;
-  },
-
-  setDateTime(dateTime) {
-    let value;
-    if (dateTime.date && dateTime.time) {
-      value = dateTime.date + "T" + dateTime.time;
-    }
-    this.props.parameter.setValue(CommonStrata.user, value);
-  },
-
-  onChangeDate(e) {
-    const dateTimeBreakOut = this.getDateTime();
-    dateTimeBreakOut.date = e.target.value;
-    this.setDateTime(dateTimeBreakOut);
-    this.setState(dateTimeBreakOut);
-  },
-
-  onChangeTime(e) {
-    const dateTimeBreakOut = this.getDateTime();
-    dateTimeBreakOut.time = e.target.value;
-    this.setDateTime(dateTimeBreakOut);
-    this.setState(dateTimeBreakOut);
-  },
-
-  render() {
-    const style =
-      defined(this.props.parameter) && defined(this.props.parameter.value)
-        ? Styles.field
-        : Styles.fieldDatePlaceholder;
-
-    return (
+  return (
+    <div>
       <div>
         <input
           className={style}
           type="date"
           placeholder="YYYY-MM-DD"
-          onChange={this.onChangeDate}
-          value={this.state.date}
+          value={dateValue}
+          onChange={(e) => {
+            setDateValue(e.target.value);
+            if (defined(props.parameter)) {
+              props.parameter.setValue(
+                CommonStrata.user,
+                `${e.target.value}T${timeValue}`
+              );
+            }
+          }}
         />
         <input
           className={style}
           type="time"
           placeholder="HH:mm:ss.sss"
-          onChange={this.onChangeTime}
-          value={this.state.time}
+          value={timeValue}
+          onChange={(e) => {
+            setTimeValue(e.target.value);
+            if (defined(props.parameter)) {
+              props.parameter.setValue(
+                CommonStrata.user,
+                `${dateValue}T${e.target.value}`
+              );
+            }
+          }}
         />
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
-module.exports = DateTimeParameterEditor;
+export default DateTimeParameterEditor;
