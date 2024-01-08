015d63a9c524bfbd0689b2031fdd479a3710aa37
sixlighthouses <brendon.ward@sixlighthouses.com>
2024-01-08 11:58:57 +1000

    format and minor code tidy

--- lib/ReactViews/Analytics/DateTimeParameterEditor.jsx
+++ lib/ReactViews/Analytics/DateTimeParameterEditor.jsx
@@ -4,53 +4,48 @@ import Styles from "./parameter-editors.scss";
 import CommonStrata from "../../Models/Definition/CommonStrata";
 import PropTypes from "prop-types";
 
-const DateTimeParameterEditor = (props) => {
+const DateTimeParameterEditor = ({ parameter, previewed }) => {
   const [dateValue, setDateValue] = useState(
-    defined(props.parameter) && defined(props.parameter.value)
-      ? props.parameter.value
-      : ""
+    defined(parameter) && defined(parameter.value) ? parameter.value : ""
   );
   const [timeValue, setTimeValue] = useState("00:00");
 
   const style =
-    defined(props.parameter) && defined(props.parameter.value)
+    defined(parameter) && defined(parameter.value)
       ? Styles.field
       : Styles.fieldDatePlaceholder;
-
   return (
     <div>
-      <div>
-        <input
-          className={style}
-          type="date"
-          placeholder="YYYY-MM-DD"
-          value={dateValue}
-          onChange={(e) => {
-            setDateValue(e.target.value);
-            if (defined(props.parameter)) {
-              props.parameter.setValue(
-                CommonStrata.user,
-                `${e.target.value}T${timeValue}`
-              );
-            }
-          }}
-        />
-        <input
-          className={style}
-          type="time"
-          placeholder="HH:mm:ss.sss"
-          value={timeValue}
-          onChange={(e) => {
-            setTimeValue(e.target.value);
-            if (defined(props.parameter)) {
-              props.parameter.setValue(
-                CommonStrata.user,
-                `${dateValue}T${e.target.value}`
-              );
-            }
-          }}
-        />
-      </div>
+      <input
+        className={style}
+        type="date"
+        placeholder="YYYY-MM-DD"
+        value={dateValue}
+        onChange={(e) => {
+          setDateValue(e.target.value);
+          if (defined(parameter)) {
+            parameter.setValue(
+              CommonStrata.user,
+              `${e.target.value}T${timeValue}`
+            );
+          }
+        }}
+      />
+      <input
+        className={style}
+        type="time"
+        placeholder="HH:mm:ss.sss"
+        value={timeValue}
+        onChange={(e) => {
+          setTimeValue(e.target.value);
+          if (defined(parameter)) {
+            parameter.setValue(
+              CommonStrata.user,
+              `${dateValue}T${e.target.value}`
+            );
+          }
+        }}
+      />
     </div>
   );
 };
