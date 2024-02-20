caa58ae6c4a82b3fcf60de901b248f1549195014
Mike Wu <wu057@plane-bm.nexus.csiro.au>
2024-02-20 11:54:15 +1100

    Add wfs example.

--- lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
@@ -4,7 +4,20 @@ import GroupTraits from "./GroupTraits";
 import mixTraits from "../mixTraits";
 import UrlTraits from "./UrlTraits";
 import LegendOwnerTraits from "./LegendOwnerTraits";
+import { traitClass } from "../Trait";
 
+@traitClass({
+  description: `Creates a single item in the catalog from url that points to WFS service.
+
+  <strong>Note:</strong> <i>Must specify property <b>typeNames</b>.</i>`,
+  example: {
+    "type": "wfs",
+    "name": "wfs example",
+    "url": "https://warehouse.ausseabed.gov.au/geoserver/ows",
+    "typeNames": "ausseabed:AHO_Reference_Surface__Broome__2023_0_5m_L0_Coverage",
+    "id": "some unique id for wfs example"
+  }
+})
 export default class WebFeatureServiceCatalogGroupTraits extends mixTraits(
   GetCapabilitiesTraits,
   GroupTraits,
