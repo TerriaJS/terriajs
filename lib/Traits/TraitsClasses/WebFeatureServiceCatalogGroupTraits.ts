72ad0730a6b485e129fca6daedc977b07e05cd84
Mike Wu <41275384+mwu2018@users.noreply.github.com>
2024-03-18 17:29:10 +1100

    Update lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>

--- lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
@@ -7,7 +7,7 @@ import LegendOwnerTraits from "./LegendOwnerTraits";
 import { traitClass } from "../Trait";
 
 @traitClass({
-  description: `Creates a group of all layers in the catalog from url that points to WFS service.`,
+  description: `Creates a group of all layers in the catalog from a url that points to a WFS service.`,
   example: {
     type: "wfs-group",
     name: "wfs-group example",
