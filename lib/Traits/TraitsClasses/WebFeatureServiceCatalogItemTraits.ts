0d05c68d29e90b6e952eac12b3381283c11bfb62
Mike Wu <41275384+mwu2018@users.noreply.github.com>
2024-03-18 17:29:23 +1100

    Update lib/Traits/TraitsClasses/WebFeatureServiceCatalogItemTraits.ts
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>

--- lib/Traits/TraitsClasses/WebFeatureServiceCatalogItemTraits.ts
+++ lib/Traits/TraitsClasses/WebFeatureServiceCatalogItemTraits.ts
@@ -27,7 +27,7 @@ export const SUPPORTED_CRS_3857 = [
 ];
 
 @traitClass({
-  description: `Creates a single item in the catalog from url that points to WFS service.
+  description: `Creates a single item in the catalog from url that points to a WFS service.
 
   <strong>Note:</strong> <i>Must specify property <b>typeNames</b>.</i>`,
   example: {
