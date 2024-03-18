eff3e6960746d0fca1d7ff71765df606496db158
Mike Wu <41275384+mwu2018@users.noreply.github.com>
2024-03-18 17:35:49 +1100

    Issue 4937 b (#7077)
    * Fix doc errors.
    
    * Add a wfs-grouup example.
    
    * Add a wms-group example.
    
    * Add a wmts example.
    
    * Add wmts-group example and fix an incorrect trait name.
    
    * Bug fix
    
    * Update lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
    
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>
    
    * Update lib/Traits/TraitsClasses/WebFeatureServiceCatalogItemTraits.ts
    
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>
    
    * Update lib/Traits/TraitsClasses/WebMapTileServiceCatalogGroupTraits.ts
    
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>
    
    * Update lib/Traits/TraitsClasses/WebMapTileServiceCatalogItemTraits.ts
    
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>
    
    ---------
    
    Co-authored-by: Lawrence Owen <lawrence.owen@csiro.au>

--- lib/Traits/TraitsClasses/WebMapTileServiceCatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/WebMapTileServiceCatalogGroupTraits.ts
@@ -5,7 +5,18 @@ import mixTraits from "../mixTraits";
 import primitiveTrait from "../Decorators/primitiveTrait";
 import UrlTraits from "./UrlTraits";
 import LegendOwnerTraits from "./LegendOwnerTraits";
+import { traitClass } from "../Trait";
 
+@traitClass({
+  description: `Creates a wmts group in the catalog from a url that points to a wmts-group service.`,
+  example: {
+    type: "wmts-group",
+    id: "a unique id for wmts-group example",
+    name: "wmts-group example",
+    url: "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/WMTS/1.0.0/WMTSCapabilities.xml",
+    opacity: 1
+  }
+})
 export default class WebMapTileServiceCatalogGroupTraits extends mixTraits(
   GetCapabilitiesTraits,
   GroupTraits,
