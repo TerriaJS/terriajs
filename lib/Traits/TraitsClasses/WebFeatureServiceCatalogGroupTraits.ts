bc01aeced5cd036f4b131dfd82a33789158e0614
Mike Wu <41275384+mwu2018@users.noreply.github.com>
2024-03-19 14:37:16 +1100

    Add a missing import. (#7080)

--- lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
+++ lib/Traits/TraitsClasses/WebFeatureServiceCatalogGroupTraits.ts
@@ -4,6 +4,7 @@ import GroupTraits from "./GroupTraits";
 import mixTraits from "../mixTraits";
 import UrlTraits from "./UrlTraits";
 import LegendOwnerTraits from "./LegendOwnerTraits";
+import { traitClass } from "../Trait";
 
 @traitClass({
   description: `Creates a group of all layers in the catalog from a url that points to a WFS service.`,
