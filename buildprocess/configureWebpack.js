5af708ad407294ef295bb515a2ff17cbc632a56a
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-05-04 14:22:39 +0200

    Use non-proposal babel plugins
    This removes a bunch of warnings
    when running "yarn upgrade".

--- buildprocess/configureWebpack.js
+++ buildprocess/configureWebpack.js
@@ -153,10 +153,10 @@ function configureWebpack(
       plugins: [
         "@babel/plugin-transform-modules-commonjs",
         ["@babel/plugin-proposal-decorators", { legacy: true }],
-        "@babel/plugin-proposal-class-properties",
-        "@babel/proposal-object-rest-spread",
-        "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-proposal-nullish-coalescing-operator",
+        "@babel/plugin-transform-class-properties",
+        "@babel/plugin-transform-object-rest-spread",
+        "@babel/plugin-transform-optional-chaining",
+        "@babel/plugin-transform-nullish-coalescing-operator",
         "babel-plugin-styled-components",
         require.resolve("@babel/plugin-syntax-dynamic-import"),
         "babel-plugin-lodash"
