6f74154c1bdba8b4dbdd370619604f6a02d26506
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-05-11 17:36:18 +0200

    karma: stop listening on "::"
    Karma-runner 6.3.20 and later defaults
    to IPv4, so this removes the warning:
    
    WARN [config]: ListenAddress was set to :: but hostname was left as the default: localhost. If your browsers fail to connect, consider changing the hostname option.

--- buildprocess/createKarmaBaseConfig.js
+++ buildprocess/createKarmaBaseConfig.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ["jasmine"],
-    listenAddress: "::",
 
     // list of files / patterns to load in the browser
     files: [
