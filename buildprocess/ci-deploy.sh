1eed359e966b9a2e0a203e0276c2300759b386ac
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-10-26 12:51:24 +0200

    CI: sync versions of dependencies
    The ^ on the request version was lost
    in #6986, so put it back so we get
    a slightly less ancient version that
    is what terriajs-server also uses.
    
    Also remove the addition of moment,
    that is already installed because
    of the dependencies of terriajs.

--- buildprocess/ci-deploy.sh
+++ buildprocess/ci-deploy.sh
@@ -19,7 +19,7 @@ gh api /repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA} -f state=pending -f co
 # Install some tools we need from npm
 npm install -g https://github.com/terriajs/sync-dependencies
 npm install -g yarn@^1.19.0
-yarn add -W request@2.83.0
+yarn add -W request@^2.88.2
 
 # Clone and build TerriaMap, using this version of TerriaJS
 TERRIAJS_COMMIT_HASH=$(git rev-parse HEAD)
@@ -34,7 +34,6 @@ git commit -a -m 'temporary commit' # so the version doesn't indicate local modi
 git tag -a "TerriaMap-$TERRIAMAP_COMMIT_HASH--TerriaJS-$TERRIAJS_COMMIT_HASH" -m 'temporary tag'
 rm yarn.lock # because TerriaMap's yarn.lock won't reflect terriajs dependencies
 yarn install
-yarn add -W moment@2.24.0
 yarn gulp build --baseHref="/${SAFE_BRANCH_NAME}/"
 
 pwd
