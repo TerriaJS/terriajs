c64b16a75bd46169a5578364adcb21e4e4a633c3
Nanda <nanda.sankaran@gmail.com>
2024-11-25 09:57:57 +1100

    Upgrade to TS 5.6.
    Mainly implements a workaround for error TS2855 that prevents accessing parent class
    fields as `super.field` [introduced in TS
    5.3](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-3.html#checks-for-super-property-accesses-on-instance-fields). In
    valid JS, super can only be used to access get/set accessors or methods from
    parent classes that are declared on the prototype, but not instance
    fields (declared as `x = "something"`). In our case though, traits are declared
    as getters and not fields, however because we use mapped types to mixin the
    traits into a class and mapped types does not allow specifying getter/setter
    variants, TS considers our traits to be fields and raises TS2855.
    
    The workaround in this PR defines these erroring traits as getters which seems
    to convince TS (at least for now) that they are valid accessors eventhough they
    still are typed using mapped.
    
    Hopefully in the future, we get this TS feature implementing [variant accessors
    for mapped types](https://github.com/microsoft/TypeScript/issues/43826), then
    we can get rid of this workaround.

--- lib/Traits/TraitsClasses/MappableTraits.ts
+++ lib/Traits/TraitsClasses/MappableTraits.ts
@@ -230,7 +230,9 @@ class MappableTraits extends mixTraits(AttributionTraits) {
     description:
       "Disables the zoom to (aka 'Ideal Zoom') button in the workbench."
   })
-  disableZoomTo: boolean = false;
+  get disableZoomTo(): boolean {
+    return false;
+  }
 
   @primitiveTrait({
     type: "boolean",
@@ -281,7 +283,7 @@ class MappableTraits extends mixTraits(AttributionTraits) {
   maximumShownFeatureInfos?: number;
 }
 
-/* eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging */
+// /* eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging */
 interface MappableTraits {
   // Add traits here that you want to override from some Mixin or Model class
   // without generating TS2611 type error.
