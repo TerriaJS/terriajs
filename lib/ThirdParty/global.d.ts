dd6fb6b22008859740ca0ef67e779f9e5eb844c6
Nanda <nanda.sankaran@gmail.com>
2024-12-04 15:48:55 +1100

    Add types for csv, dac and xml imports.

---
+++ lib/ThirdParty/global.d.ts
@@ -0,0 +1,14 @@
+declare module "*.DAC" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.csv" {
+  const csv: string;
+  export default csv;
+}
+
+declare module "*.xml" {
+  const xml: string;
+  export default xml;
+}
