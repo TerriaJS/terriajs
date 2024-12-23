7633b14893a5612c0477851c6e0c24e1d68a9ab5
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-12-23 13:28:37 +0100

    RegionPicker: pass id to catalog item constructors
    The constructors expect 2 or more arguments,
    where the first is an id, and the second
    is Terria. Pass in undefined for id
    so at least the second argument gets
    the expected value.

--- lib/ReactViews/Analytics/RegionPicker.jsx
+++ lib/ReactViews/Analytics/RegionPicker.jsx
@@ -133,6 +133,7 @@ const RegionPicker = createReactClass({
 
     if (defined(feature.data) && feature.data.type === "Feature") {
       this._selectedRegionCatalogItem = new GeoJsonCatalogItem(
+        undefined,
         this.props.previewed.terria
       );
       this._selectedRegionCatalogItem.name = "Selected Polygon";
@@ -176,6 +177,7 @@ const RegionPicker = createReactClass({
       }
 
       that._regionsCatalogItem = new WebMapServiceCatalogItem(
+        undefined,
         that.props.previewed.terria
       );
       that._regionsCatalogItem.name = "Available Regions";
@@ -219,6 +221,7 @@ const RegionPicker = createReactClass({
 
         if (defined(feature) && feature.type === "Feature") {
           that._selectedRegionCatalogItem = new GeoJsonCatalogItem(
+            undefined,
             that.props.previewed.terria
           );
           that._selectedRegionCatalogItem.name = t("analytics.selectedPolygon");
