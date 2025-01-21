a62e949ee1d2b8b6744650e14c6ebc4bb441b485
Lawrence Owen <lawrence.owen@terria.io>
2025-01-21 14:21:33 +1000

    Fix DateTimeSelectorSection test

--- test/ReactViews/Workbench/Controls/DateTimeSelectorSectionSpec.tsx
+++ test/ReactViews/Workbench/Controls/DateTimeSelectorSectionSpec.tsx
@@ -5,6 +5,7 @@ import WebMapServiceCatalogItem from "../../../../lib/Models/Catalog/Ows/WebMapS
 import Terria from "../../../../lib/Models/Terria";
 import { formatDateTime } from "../../../../lib/ReactViews/BottomDock/Timeline/DateFormats";
 import DateTimeSelectorSection from "../../../../lib/ReactViews/Workbench/Controls/DateTimeSelectorSection";
+import { terriaTheme } from "../../../../lib/ReactViews/StandardUserInterface";
 
 describe("DateTimeSelectorSection", function () {
   let terria: Terria;
@@ -24,7 +25,7 @@ describe("DateTimeSelectorSection", function () {
     await wmsItem.loadMapItems();
     act(() => {
       testRenderer = TestRenderer.create(
-        <DateTimeSelectorSection item={wmsItem} />
+        <DateTimeSelectorSection theme={terriaTheme} item={wmsItem} />
       );
     });
     buttons = testRenderer.root.findAllByType("button");
