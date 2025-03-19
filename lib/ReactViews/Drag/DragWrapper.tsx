c4fa4b0910c1ad9f0c5a5bff977a64729efa058a
Zoran Kokeza <zoran.kokeza@terria.io>
2025-03-19 23:52:16 +0100

    remove interactjs and implement custom dragable logic

---
+++ lib/ReactViews/Drag/DragWrapper.tsx
@@ -0,0 +1,30 @@
+import React, { FC, ReactNode } from "react";
+import { CSSProp } from "styled-components";
+import { useDraggable } from "./useDraggable";
+
+interface DragWrapperProps {
+  handleSelector?: string;
+  css?: CSSProp;
+  style?: React.CSSProperties;
+  children: ReactNode;
+}
+
+const DragWrapper: FC<DragWrapperProps> = ({
+  children,
+  handleSelector,
+  style,
+  css
+}) => {
+  console.log(handleSelector);
+  const [ref] = useDraggable({
+    handleSelector
+  });
+
+  return (
+    <div css={css} style={style} ref={ref}>
+      {children}
+    </div>
+  );
+};
+
+export default DragWrapper;
