TerriaJS/newterriajs

covert drag-drop-file to functional component

lib/ReactViews/DragDropFile.tsx
@@ -1,8 +1,8 @@
 import classNames from "classnames";
-import { action, runInAction, makeObservable } from "mobx";
+import { action, runInAction } from "mobx";
 import { observer } from "mobx-react";
-import { DragEvent, MouseEvent, Component } from "react";
-import { Trans, withTranslation, WithTranslation } from "react-i18next";
+import { DragEvent, MouseEvent, FC, useRef } from "react";
+import { Trans, useTranslation } from "react-i18next";
 import {
   Category,
   DataSourceAction
@@ -12,28 +12,19 @@ import Result from "../Core/Result";
 import CatalogMemberMixin, { getName } from "../ModelMixins/CatalogMemberMixin";
 import MappableMixin from "../ModelMixins/MappableMixin";
 import addUserFiles from "../Models/Catalog/addUserFiles";
-import { BaseModel } from "../Models/Definition/Model";
 import Styles from "./drag-drop-file.scss";
-import { WithViewState, withViewState } from "./Context";
+import { useViewState } from "./Context";
 import { raiseFileDragDropEvent } from "../ViewModels/FileDragDropListener";
+import type { BaseModel } from "../Models/Definition/Model";
 
-interface PropsType extends WithTranslation, WithViewState {}
+const DragDropFile: FC = observer(() => {
+  const viewState = useViewState();
+  const targetRef = useRef<EventTarget>();
 
-@observer
-class DragDropFile extends Component<PropsType> {
-  target: EventTarget | undefined;
-
-  constructor(props: PropsType) {
-    super(props);
-    makeObservable(this);
-  }
-
-  async handleDrop(e: DragEvent) {
+  const handleDrop = async (e: DragEvent) => {
     e.persist();
     e.preventDefault();
     e.stopPropagation();
-
-    const props = this.props;
 
     for (let i = 0; i < e.dataTransfer.files.length; i++) {
       // Log event to analytics for each file dropped (sometimes multiple files dropped in one DragEvent)
@@ -41,7 +32,7 @@ class DragDropFile extends Component<PropsType> {
         e.dataTransfer.files[i].type ||
         e.dataTransfer.files[i].name.split(".").pop(); // use file extension if type property is empty
 
-      this.props.viewState.terria.analytics?.logEvent(
+      viewState.terria.analytics?.logEvent(
         Category.dataSource,
         DataSourceAction.addFromDragAndDrop,
         `File Type: ${fileType}, File Size(B): ${e.dataTransfer.files[i].size}`
@@ -51,26 +42,23 @@ class DragDropFile extends Component<PropsType> {
     try {
       const addedCatalogItems: BaseModel[] | undefined = await addUserFiles(
         e.dataTransfer.files,
-        props.viewState.terria,
-        props.viewState
+        viewState.terria,
+        viewState
       );
 
       if (isDefined(addedCatalogItems) && addedCatalogItems.length > 0) {
-        runInAction(() => (props.viewState.myDataIsUploadView = false));
-        if (props.viewState.explorerPanelIsVisible) {
+        runInAction(() => (viewState.myDataIsUploadView = false));
+        if (viewState.explorerPanelIsVisible) {
           (
-            await props.viewState.viewCatalogMember(addedCatalogItems[0])
+            await viewState.viewCatalogMember(addedCatalogItems[0])
           ).throwIfError(`Failed to view ${getName(addedCatalogItems[0])}`);
-          props.viewState.openUserData();
+          viewState.openUserData();
         } else {
           // update last batch of uploaded files
           runInAction(
             () =>
-              (props.viewState.lastUploadedFiles = addedCatalogItems.map(
-                (item) =>
-                  CatalogMemberMixin.isMixedInto(item)
-                    ? item.name
-                    : item.uniqueId
+              (viewState.lastUploadedFiles = addedCatalogItems.map((item) =>
+                CatalogMemberMixin.isMixedInto(item) ? item.name : item.uniqueId
               ))
           );
         }
@@ -83,7 +71,7 @@ class DragDropFile extends Component<PropsType> {
         Result.combine(
           await Promise.all(mappableItems.map((f) => f.loadMapItems())),
           "Failed to load uploaded files"
-        ).raiseError(props.viewState.terria);
+        ).raiseError(viewState.terria);
 
         raiseFileDragDropEvent({
           addedItems: mappableItems,
@@ -97,72 +85,65 @@ class DragDropFile extends Component<PropsType> {
 
         if (isDefined(firstZoomableItem)) {
           runInAction(() =>
-            props.viewState.terria.currentViewer.zoomTo(firstZoomableItem, 1)
+            viewState.terria.currentViewer.zoomTo(firstZoomableItem, 1)
           );
         }
       }
 
-      runInAction(() => (props.viewState.isDraggingDroppingFile = false));
+      runInAction(() => (viewState.isDraggingDroppingFile = false));
     } catch (e) {
-      props.viewState.terria.raiseErrorToUser(e, "Failed to upload files");
+      viewState.terria.raiseErrorToUser(e, "Failed to upload files");
     }
-  }
+  };
 
-  @action
-  handleDragEnter(e: DragEvent) {
+  const handleDragEnter = action((e: DragEvent) => {
     e.preventDefault();
     e.stopPropagation();
     e.dataTransfer.dropEffect = "copy";
-    this.target = e.target;
-  }
+    targetRef.current = e.target;
+  });
 
-  handleDragOver(e: DragEvent) {
+  const handleDragOver = (e: DragEvent) => {
     e.preventDefault();
-  }
+  };
 
-  @action
-  handleDragLeave(e: MouseEvent) {
+  const handleDragLeave = action((e: MouseEvent) => {
     e.preventDefault();
     if (e.screenX === 0 && e.screenY === 0) {
-      this.props.viewState.isDraggingDroppingFile = false;
+      viewState.isDraggingDroppingFile = false;
     }
-    if (e.target === document || e.target === this.target) {
-      this.props.viewState.isDraggingDroppingFile = false;
+    if (e.target === document || e.target === targetRef.current) {
+      viewState.isDraggingDroppingFile = false;
     }
-  }
+  });
 
-  @action
-  handleMouseLeave() {
-    this.props.viewState.isDraggingDroppingFile = false;
-  }
+  const handleMouseLeave = action(() => {
+    viewState.isDraggingDroppingFile = false;
+  });
 
-  render() {
-    return (
-      <div
-        onDrop={this.handleDrop.bind(this)}
-        onDragEnter={this.handleDragEnter.bind(this)}
-        onDragOver={this.handleDragOver.bind(this)}
-        onDragLeave={this.handleDragLeave.bind(this)}
-        onMouseLeave={this.handleMouseLeave.bind(this)}
-        className={classNames(Styles.dropZone, {
-          [Styles.isActive]: this.props.viewState.isDraggingDroppingFile
-        })}
-      >
-        {this.props.viewState.isDraggingDroppingFile ? (
-          <div className={Styles.inner}>
-            <Trans i18nKey="dragDrop.text">
-              <h3 className={Styles.heading}>Drag & Drop</h3>
-              <div className={Styles.caption}>
-                Your data anywhere to view on the map
-              </div>
-            </Trans>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onMouseLeave={handleMouseLeave}
+      className={classNames(Styles.dropZone, {
+        [Styles.isActive]: viewState.isDraggingDroppingFile
+      })}
+    >
+      {viewState.isDraggingDroppingFile ? (
+        <div className={Styles.inner}>
+          <Trans i18nKey="dragDrop.text">
+            <h3 className={Styles.heading}>Drag & Drop</h3>
+            <div className={Styles.caption}>
+              Your data anywhere to view on the map
+            </div>
+          </Trans>
+        </div>
+      ) : null}
+    </div>
+  );
+});
 
-export default withTranslation()(withViewState(DragDropFile));
+export default DragDropFile;

TerriaJS/terriajs

lib/ReactViews/DragDropFile.tsx
@@ -1,8 +1,8 @@
 import classNames from "classnames";
-import { action, runInAction, makeObservable } from "mobx";
+import { action, runInAction } from "mobx";
 import { observer } from "mobx-react";
-import { DragEvent, MouseEvent, Component } from "react";
-import { Trans, withTranslation, WithTranslation } from "react-i18next";
+import { DragEvent, FC, MouseEvent, useRef } from "react";
+import { Trans } from "react-i18next";
 import {
   Category,
   DataSourceAction
@@ -12,28 +12,19 @@ import Result from "../Core/Result";
 import CatalogMemberMixin, { getName } from "../ModelMixins/CatalogMemberMixin";
 import MappableMixin from "../ModelMixins/MappableMixin";
 import addUserFiles from "../Models/Catalog/addUserFiles";
-import { BaseModel } from "../Models/Definition/Model";
-import Styles from "./drag-drop-file.scss";
-import { WithViewState, withViewState } from "./Context";
+import type { BaseModel } from "../Models/Definition/Model";
 import { raiseFileDragDropEvent } from "../ViewModels/FileDragDropListener";
+import { useViewState } from "./Context";
+import Styles from "./drag-drop-file.scss";
 
-interface PropsType extends WithTranslation, WithViewState {}
+const DragDropFile: FC = observer(() => {
+  const viewState = useViewState();
+  const targetRef = useRef<EventTarget>();
 
-@observer
-class DragDropFile extends Component<PropsType> {
-  target: EventTarget | undefined;
-
-  constructor(props: PropsType) {
-    super(props);
-    makeObservable(this);
-  }
-
-  async handleDrop(e: DragEvent) {
+  const handleDrop = async (e: DragEvent) => {
     e.persist();
     e.preventDefault();
     e.stopPropagation();
-
-    const props = this.props;
 
     for (let i = 0; i < e.dataTransfer.files.length; i++) {
       // Log event to analytics for each file dropped (sometimes multiple files dropped in one DragEvent)
@@ -41,7 +32,7 @@ class DragDropFile extends Component<PropsType> {
         e.dataTransfer.files[i].type ||
         e.dataTransfer.files[i].name.split(".").pop(); // use file extension if type property is empty
 
-      this.props.viewState.terria.analytics?.logEvent(
+      viewState.terria.analytics?.logEvent(
         Category.dataSource,
         DataSourceAction.addFromDragAndDrop,
         `File Type: ${fileType}, File Size(B): ${e.dataTransfer.files[i].size}`
@@ -51,26 +42,23 @@ class DragDropFile extends Component<PropsType> {
     try {
       const addedCatalogItems: BaseModel[] | undefined = await addUserFiles(
         e.dataTransfer.files,
-        props.viewState.terria,
-        props.viewState
+        viewState.terria,
+        viewState
       );
 
       if (isDefined(addedCatalogItems) && addedCatalogItems.length > 0) {
-        runInAction(() => (props.viewState.myDataIsUploadView = false));
-        if (props.viewState.explorerPanelIsVisible) {
+        runInAction(() => (viewState.myDataIsUploadView = false));
+        if (viewState.explorerPanelIsVisible) {
           (
-            await props.viewState.viewCatalogMember(addedCatalogItems[0])
+            await viewState.viewCatalogMember(addedCatalogItems[0])
           ).throwIfError(`Failed to view ${getName(addedCatalogItems[0])}`);
-          props.viewState.openUserData();
+          viewState.openUserData();
         } else {
           // update last batch of uploaded files
           runInAction(
             () =>
-              (props.viewState.lastUploadedFiles = addedCatalogItems.map(
-                (item) =>
-                  CatalogMemberMixin.isMixedInto(item)
-                    ? item.name
-                    : item.uniqueId
+              (viewState.lastUploadedFiles = addedCatalogItems.map((item) =>
+                CatalogMemberMixin.isMixedInto(item) ? item.name : item.uniqueId
               ))
           );
         }
@@ -83,7 +71,7 @@ class DragDropFile extends Component<PropsType> {
         Result.combine(
           await Promise.all(mappableItems.map((f) => f.loadMapItems())),
           "Failed to load uploaded files"
-        ).raiseError(props.viewState.terria);
+        ).raiseError(viewState.terria);
 
         raiseFileDragDropEvent({
           addedItems: mappableItems,
@@ -97,72 +85,65 @@ class DragDropFile extends Component<PropsType> {
 
         if (isDefined(firstZoomableItem)) {
           runInAction(() =>
-            props.viewState.terria.currentViewer.zoomTo(firstZoomableItem, 1)
+            viewState.terria.currentViewer.zoomTo(firstZoomableItem, 1)
           );
         }
       }
 
-      runInAction(() => (props.viewState.isDraggingDroppingFile = false));
+      runInAction(() => (viewState.isDraggingDroppingFile = false));
     } catch (e) {
-      props.viewState.terria.raiseErrorToUser(e, "Failed to upload files");
+      viewState.terria.raiseErrorToUser(e, "Failed to upload files");
     }
-  }
+  };
 
-  @action
-  handleDragEnter(e: DragEvent) {
+  const handleDragEnter = action((e: DragEvent) => {
     e.preventDefault();
     e.stopPropagation();
     e.dataTransfer.dropEffect = "copy";
-    this.target = e.target;
-  }
+    targetRef.current = e.target;
+  });
 
-  handleDragOver(e: DragEvent) {
+  const handleDragOver = (e: DragEvent) => {
     e.preventDefault();
-  }
+  };
 
-  @action
-  handleDragLeave(e: MouseEvent) {
+  const handleDragLeave = action((e: MouseEvent) => {
     e.preventDefault();
     if (e.screenX === 0 && e.screenY === 0) {
-      this.props.viewState.isDraggingDroppingFile = false;
+      viewState.isDraggingDroppingFile = false;
     }
-    if (e.target === document || e.target === this.target) {
-      this.props.viewState.isDraggingDroppingFile = false;
+    if (e.target === document || e.target === targetRef.current) {
+      viewState.isDraggingDroppingFile = false;
     }
-  }
+  });
 
-  @action
-  handleMouseLeave() {
-    this.props.viewState.isDraggingDroppingFile = false;
-  }
+  const handleMouseLeave = action(() => {
+    viewState.isDraggingDroppingFile = false;
+  });
 
-  render() {
-    return (
-      <div
-        onDrop={this.handleDrop.bind(this)}
-        onDragEnter={this.handleDragEnter.bind(this)}
-        onDragOver={this.handleDragOver.bind(this)}
-        onDragLeave={this.handleDragLeave.bind(this)}
-        onMouseLeave={this.handleMouseLeave.bind(this)}
-        className={classNames(Styles.dropZone, {
-          [Styles.isActive]: this.props.viewState.isDraggingDroppingFile
-        })}
-      >
-        {this.props.viewState.isDraggingDroppingFile ? (
-          <div className={Styles.inner}>
-            <Trans i18nKey="dragDrop.text">
-              <h3 className={Styles.heading}>Drag & Drop</h3>
-              <div className={Styles.caption}>
-                Your data anywhere to view on the map
-              </div>
-            </Trans>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onMouseLeave={handleMouseLeave}
+      className={classNames(Styles.dropZone, {
+        [Styles.isActive]: viewState.isDraggingDroppingFile
+      })}
+    >
+      {viewState.isDraggingDroppingFile ? (
+        <div className={Styles.inner}>
+          <Trans i18nKey="dragDrop.text">
+            <h3 className={Styles.heading}>Drag & Drop</h3>
+            <div className={Styles.caption}>
+              Your data anywhere to view on the map
+            </div>
+          </Trans>
+        </div>
+      ) : null}
+    </div>
+  );
+});
 
-export default withTranslation()(withViewState(DragDropFile));
+export default DragDropFile;