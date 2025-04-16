a4f0ff094ba00345b6671608ec089861a0079cb0
Zoran Kokeza <zoran.kokeza@terria.io>
2025-04-16 20:42:37 +0200

    update tests to use react testing library

--- test/ReactViews/Custom/Chart/FeatureInfoPanelChartSpec.tsx
+++ test/ReactViews/Custom/Chart/FeatureInfoPanelChartSpec.tsx
@@ -1,6 +1,9 @@
-import { ReactTestRenderer, act, create } from "react-test-renderer";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
-import runLater from "../../../../lib/Core/runLater";
 import CsvCatalogItem from "../../../../lib/Models/Catalog/CatalogItems/CsvCatalogItem";
 import CommonStrata from "../../../../lib/Models/Definition/CommonStrata";
 import TerriaFeature from "../../../../lib/Models/Feature/Feature";
@@ -59,79 +62,62 @@ describe("FeatureInfoPanelChart", function () {
   describe("yColumn prop", function () {
     describe("when specified", function () {
       it("renders the correct y-column", async function () {
-        const renderer = renderChart(
+        renderChart(
           `<chart src="test/csv_nongeo/x_height.csv" y-column="plant height"></chart>`,
           context
         );
-        await runLater(() => {});
+        await waitForElementToBeRemoved(() =>
+          screen.queryByText("chart.noData")
+        );
 
-        // Find elements by their props instead of component type
-        const labels = renderer?.root.findAllByProps({
-          children: "Plant Height x x"
-        });
-        expect(labels?.length).toBeGreaterThan(0);
+        expect(screen.getByText("Plant Height x x")).toBeVisible();
       });
 
       it("renders nothing if the specified y-column does not exist", async function () {
-        const renderer = renderChart(
+        renderChart(
           `<chart src="test/csv_nongeo/x_height.csv" y-column="foo-does-not-exist"></chart>`,
           context
         );
-        await runLater(() => {});
 
-        const noDataText = renderer?.root.findAllByProps({
-          children: "chart.noData"
-        });
-        expect(noDataText?.length).toBeGreaterThan(0);
+        expect(screen.queryByText("chart.noData")).toBeVisible();
       });
     });
 
     describe("otherwise", function () {
       it("renders the first line type chart item", async function () {
-        const renderer = renderChart(
+        renderChart(
           `<chart src="test/csv_nongeo/x_height.csv"></chart>`,
           context
         );
-        await runLater(() => {});
 
-        const labels = renderer?.root.findAllByProps({
-          children: "Z x x"
-        });
-        expect(labels?.length).toBeGreaterThan(0);
+        await waitForElementToBeRemoved(() =>
+          screen.queryByText("chart.noData")
+        );
+
+        expect(screen.getByText("Z x x")).toBeVisible();
       });
     });
   });
 
   it("can show a custom X axis label", async function () {
-    const renderer = renderChart(
+    renderChart(
       `<chart src="test/csv_nongeo/x_height.csv" preview-x-label="life-time"></chart>`,
       context
     );
-    await runLater(() => {});
 
-    const labels = renderer?.root.findAllByProps({
-      children: "life-time"
-    });
-    expect(labels?.length).toBeGreaterThan(0);
+    await waitForElementToBeRemoved(() => screen.queryByText("chart.noData"));
+
+    expect(screen.getAllByText("life-time")[1]).toBeVisible();
   });
 });
 
 /**
  * Render the given chart markup and return the test renderer.
  */
-function renderChart(
-  chartMarkup: string,
-  context: ProcessNodeContext
-): ReactTestRenderer | undefined {
+function renderChart(chartMarkup: string, context: ProcessNodeContext): void {
   const chartElements = parseCustomHtmlToReact(chartMarkup, context, false, {
     ADD_TAGS: ["chart"],
     ADD_ATTR: ChartAttributes
   });
-  let renderer;
-  act(() => {
-    renderer = create(
-      <ThemeProvider theme={terriaTheme}>{chartElements}</ThemeProvider>
-    );
-  });
-  return renderer;
+  render(<ThemeProvider theme={terriaTheme}>{chartElements}</ThemeProvider>);
 }
