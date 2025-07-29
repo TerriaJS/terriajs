bf564586a4197db1f573eb719725813ef3bfdb1e
Zoran Kokeza <zoran.kokeza@terria.io>
2025-07-29 23:52:26 +0200

    Nominatim search update (#7656)
    * disable autocomplete for nominatim search provider
    * show attributions for search providers
    * update doc

--- lib/ReactViews/Map/BottomBar/Credits/MapCredits.tsx
+++ lib/ReactViews/Map/BottomBar/Credits/MapCredits.tsx
@@ -3,24 +3,33 @@ import { observer } from "mobx-react";
 import { FC, useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import GlobeOrMap from "../../../../Models/GlobeOrMap";
+import { SearchBarModel } from "../../../../Models/SearchProviders/SearchBarModel";
 import { ICredit } from "./Credit.type";
 import { Credits } from "./Credits";
 import { CreditsContainer } from "./CreditsContainer";
 import { DataAttributionModal } from "./DataAttribution/DataAttributionModal";
+import { MapCreditLogo } from "./MapCreditLogo";
 import { Spacer } from "./Spacer";
 import { TerriaLogo } from "./TerriaLogo";
-import { MapCreditLogo } from "./MapCreditLogo";
 
 interface IMapCreditsProps {
   hideTerriaLogo: boolean;
   credits?: ICredit[];
   currentViewer: GlobeOrMap;
+  searchBarModel: SearchBarModel;
 }
 
 export const MapCredits: FC<IMapCreditsProps> = observer(
-  ({ currentViewer, hideTerriaLogo, credits }) => {
+  ({ currentViewer, hideTerriaLogo, credits, searchBarModel }) => {
     const { t } = useTranslation();
     const [dataAttributionVisible, setDataAttributionVisible] = useState(false);
+
+    const searchAttributions = searchBarModel.locationSearchProvidersArray
+      .map((provider) => {
+        return provider.attributions?.flat();
+      })
+      .flat()
+      .filter((attribution) => !!attribution);
 
     const showDataAttribution = useCallback(() => {
       setDataAttributionVisible(true);
@@ -55,15 +64,18 @@ export const MapCredits: FC<IMapCreditsProps> = observer(
         <MapCreditLogo currentViewer={currentViewer} />
         <Credits credits={credits} />
         <Spacer />
-        {currentViewer.attributions && currentViewer.attributions.length > 0 ? (
+        {(currentViewer.attributions &&
+          currentViewer.attributions.length > 0) ||
+        currentViewer.attributions.length > 0 ? (
           <a onClick={showDataAttribution}>
-            {t("map.extraCreditLinks.basemap")}
+            {t("map.extraCreditLinks.credits")}
           </a>
         ) : null}
         {dataAttributionVisible ? (
           <DataAttributionModal
             closeModal={hideDataAttribution}
             attributions={currentViewer.attributions}
+            searchAttributions={searchAttributions}
           />
         ) : null}
       </CreditsContainer>
--- test/ReactViews/Search/SearchBoxSpec.tsx
+++ test/ReactViews/Search/SearchBoxSpec.tsx
@@ -1,15 +1,13 @@
-import { create } from "react-test-renderer";
-import { act } from "react-dom/test-utils";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
 import Terria from "../../../lib/Models/Terria";
 import ViewState from "../../../lib/ReactViewModels/ViewState";
 import { SearchBox } from "../../../lib/ReactViews/Search/SearchBox";
-import { terriaTheme } from "../../../lib/ReactViews/StandardUserInterface";
+import { renderWithContexts } from "../withContext";
 
 describe("SearchBox", function () {
   let terria: Terria;
   let viewState: ViewState;
-
-  let testRenderer: any;
 
   beforeEach(function () {
     terria = new Terria({
@@ -21,49 +19,43 @@ describe("SearchBox", function () {
     });
   });
 
-  describe("with basic props", function () {
-    const searchText = "mochi";
-    let searchBoxValue = "something";
+  describe("with basic props", () => {
+    it("renders", () => {
+      renderWithContexts(
+        <SearchBox
+          onSearchTextChanged={jasmine.createSpy()}
+          onDoSearch={() => {}}
+          onFocus={() => {}}
+          searchText={"mochi"}
+          placeholder="placeholder"
+        />,
+        viewState
+      );
 
-    beforeEach(function () {
-      searchBoxValue = "something";
+      const searchBoxInput = screen.getByRole("textbox");
+
+      expect(searchBoxInput).toBeVisible();
+      expect(searchBoxInput).toHaveValue("mochi");
     });
-    const searchBoxWithProps = (
-      <SearchBox
-        t={() => {}}
-        terria={terria}
-        viewState={viewState}
-        onSearchTextChanged={(newVal: any) => {
-          searchBoxValue = newVal;
-        }}
-        onDoSearch={() => {}}
-        onFocus={() => {}}
-        searchText={searchText}
-        placeholder="placeholder"
-        theme={terriaTheme}
-      />
-    );
 
-    it("renders", function () {
-      act(() => {
-        testRenderer = create(searchBoxWithProps);
-      });
+    it("renders and clearSearch triggers onSearchTextChanged callback", async () => {
+      const onSeachTextChangedSpy = jasmine.createSpy();
+      renderWithContexts(
+        <SearchBox
+          onSearchTextChanged={onSeachTextChangedSpy}
+          onDoSearch={() => {}}
+          onFocus={() => {}}
+          searchText={"mochi"}
+          placeholder="placeholder"
+        />,
+        viewState
+      );
 
-      const searchBoxInput = testRenderer.root.findByType("input");
-      expect(searchBoxInput).toBeDefined();
-      expect(searchBoxInput.props.value).toEqual(searchText);
-    });
-    it("renders and clearSearch triggers onSearchTextChanged callback", function () {
-      act(() => {
-        testRenderer = create(searchBoxWithProps);
-      });
+      await userEvent.click(
+        screen.getByRole("button", { name: "Clear search" })
+      );
 
-      const searchBoxInstance = testRenderer.root.instance;
-      expect(searchBoxValue).toEqual("something");
-      // attempt clear search method
-      searchBoxInstance.clearSearch();
-      // Now it should be reset
-      expect(searchBoxValue).toEqual("");
+      expect(onSeachTextChangedSpy).toHaveBeenCalledWith("");
     });
   });
 });
