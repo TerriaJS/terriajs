3d03581625a2a035a54251b585ba36fe1daf767d
Nanda <nanda.sankaran@gmail.com>
2025-08-26 22:01:40 +1000

    Add button to open help center page in help panel.

---
+++ lib/ReactViews/Map/Panels/HelpPanel/HelpCentreButton.tsx
@@ -0,0 +1,49 @@
+import { runInAction } from "mobx";
+import { StyledIcon, useViewState } from "terriajs-plugin-api";
+import {
+  Category,
+  HelpAction
+} from "../../../../Core/AnalyticEvents/analyticEvents";
+import Button from "../../../../Styled/Button";
+import Icon from "../../../../Styled/Icon";
+
+const HELP_CENTER_URL = "https://terriajs.gitbook.io/help-center";
+
+/**
+ * SaaS only button to open help center
+ *
+ * Ideally there is a better way to customize help panel content that doesn't
+ * require code change.
+ */
+const HelpCentreButton = () => {
+  const viewState = useViewState();
+  return (
+    <Button
+      primary
+      rounded
+      styledMinWidth={"240px"}
+      onClick={() => {
+        viewState.terria.analytics?.logEvent(
+          Category.help,
+          HelpAction.helpCenterOpened
+        );
+        runInAction(() => {
+          viewState.hideHelpPanel();
+          window.open(HELP_CENTER_URL);
+        });
+      }}
+      renderIcon={() => (
+        <StyledIcon light styledWidth={"18px"} glyph={Icon.GLYPHS.info} />
+      )}
+      textProps={{
+        large: true
+      }}
+      css={`
+        ${(p) => p.theme.addTerriaPrimaryBtnStyles(p)}
+      `}
+    >
+      Help Centre
+    </Button>
+  );
+};
+export default HelpCentreButton;
