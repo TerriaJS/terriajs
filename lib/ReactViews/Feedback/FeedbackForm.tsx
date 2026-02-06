59ddf8d1c007acf62e30b286dad8550a79980f3e
Giovanni Lughi <giovanni.lughi@gmail.com>
2026-02-06 18:55:21 +0100

    If feedbackUrl starts with "mailto:" opens the email client

--- lib/ReactViews/Feedback/FeedbackForm.tsx
+++ lib/ReactViews/Feedback/FeedbackForm.tsx
@@ -166,6 +166,16 @@ class FeedbackForm extends React.Component<IProps, IState> {
 
   render() {
     const { t, i18n, viewState, theme } = this.props;
+
+    if (viewState.terria.configParameters?.feedbackUrl?.startsWith("mailto:")) {
+      window.open(
+        `${viewState.terria.configParameters?.feedbackUrl}?subject=${viewState.terria.configParameters?.feedbackPreamble}`,
+        "_blank"
+      );
+      this.onDismiss();
+      return null;
+    }
+
     const preamble = parseCustomMarkdownToReact(
       applyTranslationIfExists(
         viewState.terria.configParameters.feedbackPreamble ||
