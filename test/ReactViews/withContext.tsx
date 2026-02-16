eda950b87be80a8a8fa6b84d0fb7671ba93d100a
Zoran Kokeza <zoran.kokeza@terria.io>
2026-02-16 20:19:52 +0100

    remove remaining usage of react test renderer and shallow testutils

--- test/ReactViews/withContext.tsx
+++ test/ReactViews/withContext.tsx
@@ -1,6 +1,5 @@
 import { render, RenderOptions } from "@testing-library/react";
 import { ReactElement, ReactNode } from "react";
-import { create, TestRendererOptions } from "react-test-renderer";
 import { ThemeProvider } from "styled-components";
 import ViewState from "../../lib/ReactViewModels/ViewState";
 import { ViewStateProvider } from "../../lib/ReactViews/Context/ViewStateContext";
@@ -8,20 +7,6 @@ import { terriaTheme } from "../../lib/ReactViews/StandardUserInterface";
 
 export function withThemeContext(node: ReactNode) {
   return <ThemeProvider theme={terriaTheme}>{node}</ThemeProvider>;
-}
-
-/** Wrap react node in viewState and theme provider */
-export function createWithContexts(
-  viewState: ViewState,
-  node: ReactNode,
-  testRendererOptions?: TestRendererOptions
-) {
-  return create(
-    <ViewStateProvider viewState={viewState}>
-      <ThemeProvider theme={terriaTheme}>{node}</ThemeProvider>
-    </ViewStateProvider>,
-    testRendererOptions
-  );
 }
 
 export function renderWithContexts(
