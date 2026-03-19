afb24ed236db1e3ba7a4dbf6ec6bd725372b2b61
Zoran Kokeza <zoran.kokeza@terria.io>
2026-03-19 18:06:18 +0100

    fix lint--no-veri

--- lib/ReactViews/Custom/Chart/ChartPanelDownloadButton.tsx
+++ lib/ReactViews/Custom/Chart/ChartPanelDownloadButton.tsx
@@ -1,8 +1,8 @@
 // import FileSaver from "file-saver";
-import { runInAction, toJS } from "mobx";
+// import { runInAction, toJS } from "mobx";
 import { observer } from "mobx-react";
 import FeatureDetection from "terriajs-cesium/Source/Core/FeatureDetection";
-import isDefined from "../../../Core/isDefined";
+// import isDefined from "../../../Core/isDefined";
 import Result from "../../../Core/Result";
 import ChartableMixin from "../../../ModelMixins/ChartableMixin";
 import TableMixin from "../../../ModelMixins/TableMixin";
@@ -14,44 +14,44 @@ import Styles from "./chart-panel-download-button.scss";
 /**
  * Extracts column names and row data from TableMixin item for CSV download.
  */
-function synthesizeNameAndValueArrays(items: TableMixin.Instance[]) {
-  return runInAction(() => {
-    const valueArrays: (readonly string[])[][] = [];
-    const names: string[] = []; // We will add the catalog item name back into the csv column name.
+// function synthesizeNameAndValueArrays(items: TableMixin.Instance[]) {
+//   return runInAction(() => {
+//     const valueArrays: (readonly string[])[][] = [];
+//     const names: string[] = []; // We will add the catalog item name back into the csv column name.
 
-    for (let i = items.length - 1; i >= 0; i--) {
-      const item = items[i];
+//     for (let i = items.length - 1; i >= 0; i--) {
+//       const item = items[i];
 
-      const xColumn = item.xColumn;
-      if (!xColumn || !item.showInChartPanel) {
-        continue;
-      }
-      if (!names.length) {
-        names.push(xColumn.name);
-      }
+//       const xColumn = item.xColumn;
+//       if (!xColumn || !item.showInChartPanel) {
+//         continue;
+//       }
+//       if (!names.length) {
+//         names.push(xColumn.name);
+//       }
 
-      let columns = [xColumn];
-      const lineTraits = item.activeTableStyle.chartTraits.lines ?? [];
-      // Only add yColumns if `TableChartLineStyleTraits.isSelectedInWorkbench` is true
-      // i.e. the columns which are actually showing in chart
-      const yColumns = lineTraits
-        .filter((line) => line.isSelectedInWorkbench)
-        .map((line) => item.findColumnByName(line.yAxisColumn))
-        .filter(isDefined);
+//       let columns = [xColumn];
+//       const lineTraits = item.activeTableStyle.chartTraits.lines ?? [];
+//       // Only add yColumns if `TableChartLineStyleTraits.isSelectedInWorkbench` is true
+//       // i.e. the columns which are actually showing in chart
+//       const yColumns = lineTraits
+//         .filter((line) => line.isSelectedInWorkbench)
+//         .map((line) => item.findColumnByName(line.yAxisColumn))
+//         .filter(isDefined);
 
-      if (yColumns.length > 0) {
-        columns = columns.concat(yColumns);
-        // Use typed array if possible so we can pass by pointer to the web worker.
-        // Create a new array otherwise because if values are a knockout observable, they cannot be serialised for the web worker.
-        valueArrays.push(columns.map((column) => toJS(column.values)));
-        yColumns.forEach((column) => {
-          names.push(item.name + " " + column.name);
-        });
-      }
-    }
-    return { values: valueArrays, names: names };
-  });
-}
+//       if (yColumns.length > 0) {
+//         columns = columns.concat(yColumns);
+//         // Use typed array if possible so we can pass by pointer to the web worker.
+//         // Create a new array otherwise because if values are a knockout observable, they cannot be serialised for the web worker.
+//         valueArrays.push(columns.map((column) => toJS(column.values)));
+//         yColumns.forEach((column) => {
+//           names.push(item.name + " " + column.name);
+//         });
+//       }
+//     }
+//     return { values: valueArrays, names: names };
+//   });
+// }
 
 async function download(items: TableMixin.Instance[]) {
   if (items.length === 0) return;
