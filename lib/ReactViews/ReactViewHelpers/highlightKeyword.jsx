6074ae7eac83e4a22c64f8b2694dfe119d9a7687
Giovanni Lughi <giovanni.lughi@gmail.com>
2026-04-10 14:48:29 +0200

    Fix unbalanced parenthesis search and issue 7823.

--- lib/ReactViews/ReactViewHelpers/highlightKeyword.jsx
+++ lib/ReactViews/ReactViewHelpers/highlightKeyword.jsx
@@ -5,21 +5,28 @@ import React from "react";
 // cons: ???
 export default function highlightKeyword(searchResult, keywordToHighlight) {
   if (!keywordToHighlight) return searchResult;
-  const parts = searchResult.split(new RegExp(`(${keywordToHighlight})`, "gi"));
-  return (
-    <>
-      {parts.map((part, i) => (
-        <span
-          key={i}
-          style={
-            part.toLowerCase() === keywordToHighlight.toLowerCase()
-              ? { fontWeight: "bold" }
-              : {}
-          }
-        >
-          {part}
-        </span>
-      ))}
-    </>
-  );
+  try {
+    const parts = searchResult.split(
+      new RegExp(`(${keywordToHighlight})`, "gi")
+    );
+    return (
+      <>
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === keywordToHighlight.toLowerCase()
+                ? { fontWeight: "bold" }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}
+      </>
+    );
+  } catch (e) {
+    console.log(e);
+    return searchResult;
+  }
 }
